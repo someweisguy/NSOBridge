2f04d31353f1502f963f7ddec4b14b1e51f943a0
Mitch Weisbrod <mitch@theweisbrods.com>
2024-03-17 09:57:23 -0700

    basic client sync

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PyQt6.QtWidgets import QApplication, QWidget
 from flask import Flask
+import flask
 from flask_socketio import SocketIO
 from engineio.async_drivers import gevent  # noqa: F401 - Required for pyinstaller bundle
 from threading import Thread
 import sys
+import time
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -12,12 +14,19 @@ gui = QApplication(sys.argv)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return flask.render_template('index.html')
+
+@socketio.on("sync")
+def client_sync(*_):
+    epoch = round(time.time_ns() / 1_000_000)
+    print(epoch)
+    return epoch
 
 
 if __name__ == "__main__":
-    Thread(target=socketio.run, args=(app,), daemon=True).start()
+    kwargs = dict()
+    Thread(target=socketio.run, args=(app, "0.0.0.0", 5000), kwargs=kwargs, daemon=True).start()
     window = QWidget()
-    window.show()
+    # window.show()
 
     gui.exec()
