0e00d1093cf37ea5dea4f3c0bb201a8eea34d48c
Mitch Weisbrod <mitch@theweisbrods.com>
2024-03-17 16:44:54 -0700

    add context

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QWidget
-from flask import Flask
-import flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 from engineio.async_drivers import gevent  # noqa: F401 - Required for pyinstaller bundle
 from threading import Thread
@@ -16,7 +15,8 @@ gui = QApplication(sys.argv)
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
+    context = {"sync_iterations": 10}
+    return render_template("index.html", **context)
 
 
 @socketio.on("sync")
