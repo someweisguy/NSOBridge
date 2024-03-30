387da0711846a571d1baf9482caa7a68b1dd8cd1
Mitch Weisbrod <mitch@theweisbrods.com>
2024-03-29 19:07:05 -0700

    remove import

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from engineio.async_drivers import gevent  # noqa: F401 - Required for pyinstall
 from threading import Thread
 import sys
 import time
-from dataclasses import dataclass
 
 DEBUG_FLASK = True
 
@@ -24,11 +23,12 @@ def index():
 def sync():
     return Timer.server_time()
 
+
 class Timer:
     def __init__(self, value: int = 120_000):
         self._value = value
         self._start = None
-    
+
     def __str__(self) -> str:
         val = self.value()
         minutes = val / 60_000
@@ -40,7 +40,7 @@ class Timer:
         """Gets the server time in milliseconds using a monotonic clock."""
         now = time.monotonic_ns()
         return now // 1_000_000
-    
+
     def start(self, start_time: int | None = None) -> bool:
         if self._start is not None:
             return False
@@ -57,70 +57,16 @@ class Timer:
         self._value -= stop_time - self._start
         self._start = None
         return True
-    
+
     def value(self, time: int | None = None) -> int:
         if self._start is None:
             return self._value
         if time is None:
             time = Timer.server_time()
-        return (self._value - (time - self._start))
+        return self._value - (time - self._start)
 
-@dataclass(frozen=True)
-class Skater:
-    number: str
-    name: str
 
-    def __str__(self) -> str:
-        return f"{self.number}: {self.name}"
-
-    def __hash__(self) -> int:
-        return hash(self.number)
-
-    def __eq__(self, other) -> bool:
-        if isinstance(other, Skater):
-            return self.number == other.number
-        elif isinstance(other, str):
-            return self.number == other
-        else:
-            return False
-
-class Team:
-    def __init__(self, league_name: str, team_name: str) -> None:
-        self.league_name: str = league_name
-        self.name: str = team_name
-        self._roster: list[Skater] = list()
-
-    def __getitem__(self, key: str) -> Skater:
-        if isinstance(key, str) and key in self._roster:
-            return self._roster[self._roster.index(key)]
-        else:
-            raise ValueError(f"Team indices must be string, not {type(key).__name__}")
-    
-    def __iter__(self) -> Skater:
-        return self._roster.__iter__()
-    
-    def __next__(self) -> Skater:
-        return self._roster.__next__()
-    
-    def __str__(self) -> str:
-        return f"{self.league_name}, {self.name}"
-
-    def __len__(self) -> int:
-        return len(self._roster)
-    
-    def add(self, skater: Skater) -> bool:
-        if not isinstance(skater, Skater) or skater in self._roster:
-            return False
-        self._roster.append(skater)
-        return True
-    
-    def remove(self, skater: Skater | str) -> bool:
-        if skater not in self._roster:
-            return False
-        self._roster.remove(skater)
-        return True
-    
-if __name__ == "__main__":  
+if __name__ == "__main__":
     if DEBUG_FLASK:
         # Debug the Flask application without the Qt GUI
         socketio.run(app, "0.0.0.0", 5000, debug=True)
