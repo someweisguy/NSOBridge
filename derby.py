feb4103131152f20edbafabd9e4e5d6cc647fb31
Mitch Weisbrod <mitch@theweisbrods.com>
2024-03-29 19:06:27 -0700

    add game structure

--- derby.py
+++ derby.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from typing import Any, Iterable, SupportsIndex
+
 
 @dataclass()
 class Skater:
     """Represents a skater and their number. Skaters can be uniquely identified by their number."""
+
     number: str
     name: str
 
@@ -29,46 +32,92 @@ class Skater:
             return self.number == other
         else:
             return False
-    
+
     def is_sanctionable(self) -> bool:
+        """Returns True if the skater number is valid for a sanctioned WFTDA game. Valid numbers contain at least one numeral but no more than four numerals."""
         return self.number.isnumeric() and len(self.number) <= 4
 
 
-class Team:
-    def __init__(self, league_name: str, team_name: str, color = None) -> None:
-        self.league_name: str = league_name
-        self.name: str = team_name
-        self._color = color  # TODO
-        self._roster: list[Skater] = list()
+class Roster(list):
+    def __init__(self, *, limit: None | int = None) -> None:
+        if not isinstance(limit, int) and limit is not None:
+            raise TypeError(
+                f"limit must be None or int not type {type(limit).__name__}"
+            )
+        if isinstance(limit, int) and limit < 0:
+            raise ValueError("limit must be greater than 0")
+        self._limit: None | int = limit
 
-    def __getitem__(self, key: str) -> Skater:
-        if not isinstance(key, str):
-            raise ValueError(f"Team indices must be string, not {type(key).__name__}")
-        elif key not in self._roster:
-            return None
-        else:
-            return self._roster[self._roster.index(key)]
-    
-    def __iter__(self) -> Skater:
-        return self._roster.__iter__()
-    
-    def __next__(self) -> Skater:
-        return self._roster.__next__()
-    
-    def __str__(self) -> str:
-        return f"{self.league_name}, {self.name}"
+    @property
+    def limit(self) -> None | int:
+        return self._limit
 
-    def __len__(self) -> int:
-        return len(self._roster)
-    
-    def add(self, skater: Skater) -> bool:
-        if not isinstance(skater, Skater) or skater in self._roster:
-            return False
-        self._roster.append(skater)
-        return True
-    
-    def remove(self, skater: Skater | str) -> bool:
-        if skater not in self._roster:
-            return False
-        self._roster.remove(skater)
-        return True
+    def append(self, __object: Any) -> None:
+        if __object in self:
+            raise ValueError("Roster must not contain duplicates")
+        if self._limit is not None and len(self) + 1 > self._limit:
+            raise RuntimeError()  # TODO
+        return super().append(__object)
+
+    def extend(self, __iterable: Iterable) -> None:
+        for item in __iterable:
+            if item in self:
+                raise ValueError("Roster must not contain duplicates")
+        if self._limit is not None and len(self) + len(__iterable) > self._limit:
+            raise RuntimeError()  # TODO
+        return super().extend(__iterable)
+
+    def insert(self, __index: SupportsIndex, __object: Any) -> None:
+        if __object in self:
+            raise ValueError("Roster must not contain duplicates")
+        if self._limit is not None and len(self) + 1 > self._limit:
+            raise RuntimeError()  # TODO
+        return super().insert(__index, __object)
+
+
+class Teams:
+    class TeamData:
+        def __init__(self) -> None:
+            self._league: str = ""
+            self._name: str = ""
+            self._skaters: Roster[Skater] = Roster()
+
+        @property
+        def league(self) -> str:
+            return self._league
+
+        @league.setter
+        def league(self, name: str) -> None:
+            self._league = name
+
+        @property
+        def name(self) -> str:
+            return self._name
+
+        @name.setter
+        def name(self, name: str) -> None:
+            self._name = name
+
+        @property
+        def skaters(self) -> set[Skater]:
+            return self._skaters
+
+    def __init__(self) -> None:
+        self._home = Teams.TeamData()
+        self._away = Teams.TeamData()
+
+    @property
+    def home(self) -> TeamData:
+        return self._home
+
+    @property
+    def away(self) -> TeamData:
+        return self._away
+
+
+""" TODO
+- game.roster.home -- done
+- game.jams.home
+- game.lineups.home
+- game.penalties.home
+"""
