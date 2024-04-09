d05c0bd370eae97c246887dbaf8406ac2ee5c321
Mitch Weisbrod <mitch@theweisbrods.com>
2024-04-09 15:32:00 -0700

    formatting

--- static/core.js
+++ static/core.js
@@ -1,7 +1,10 @@
+const socket = io()
+var epsilon = 0;
+
 async function getEpsilon(iterations) {
     const socket = io();
     var epsilon = 0;
-    var successes = 0;
+    var successes = iterations;
     for (var i = 0; i < iterations; ++i) {
         try {
             const start = Date.now();
@@ -9,20 +12,25 @@ async function getEpsilon(iterations) {
             const end = Date.now();
             const delay = (end - start) / 2;
             epsilon += start - (response - delay);
-            ++successes;
         } catch {
+            --successes;
             continue;
         }
+    }
+    if (successes < iterations) {
+        console.log("Sync iteration failed " + (iterations - successes) + "out of " + iterations + " times");
     }
     return Math.round(epsilon / successes);
 }
 
-var epsilon = null;
-const socket = io();
 socket.on("connect", async () => {
     console.log("Connected to game server");
     const iterations = 10;
     console.log("Syncing time with server using " + iterations + " iterations");
     epsilon = await getEpsilon(iterations);
     console.log("Time sync epsilon is " + epsilon);
+
+    const event = new Event("serverSync");
+    document.dispatchEvent(event)
+
 });
