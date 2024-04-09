ce6b6d92619ac8b3b20645e2b56c1a09c237bb2c
Mitch Weisbrod <mitch@theweisbrods.com>
2024-04-09 14:16:25 -0700

    use reloader

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
             server,
             host="0.0.0.0",
             port=5000,
-            use_reloader=False,
+            use_reloader=True,
             log_output=True,
             debug=True,
         )
