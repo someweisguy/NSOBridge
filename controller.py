0588b6888a56cb2ce05627058c05ddfe49dd5b2c
Mitch Weisbrod <mitch@theweisbrods.com>
2024-04-16 18:40:42 -0700

    log errors

--- controller.py
+++ controller.py
@@ -41,9 +41,7 @@ class Controller(QRunnable):
         if self.is_running:
             return  # only allow one instance to run at a time
         wsgi = socketio.WSGIApp(Controller.socket, Controller.flask)
-        self._server = WSGIServer(
-            ("0.0.0.0", self.port), wsgi, log=None, error_log=None
-        )
+        self._server = WSGIServer(("0.0.0.0", self.port), wsgi, log=None)
         try:
             self._server.serve_forever()
         except OSError:
