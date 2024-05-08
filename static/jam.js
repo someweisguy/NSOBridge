a0b9dea2d4c15c1c47f5f4bee06d7b8c5f3a07f5
Mitch Weisbrod <mitch@theweisbrods.com>
2024-05-07 19:40:29 -0700

    widget formatting

--- static/jam.js
+++ static/jam.js
@@ -9,20 +9,30 @@ class JamElement extends HTMLElement {
         // Create the main span that will wrap the element
         const jamWrapper = document.createElement("span");
         jamWrapper.setAttribute("class", "jamWrapper");
+        jamWrapper.style.display = "inline-block";
+        if (this.hasAttribute("width")) {
+            jamWrapper.style.width = this.getAttribute(width);
+        } else {
+            jamWrapper.style.width = "300px";
+        }
+        jamWrapper.style.textAlign = "center";
         
         // Create the label for the jam score
         const jamScoreHeader = document.createElement("div");
         jamScoreHeader.appendChild(document.createTextNode("Jam Score"));
+        jamScoreHeader.setAttribute("class", "label");
         jamWrapper.appendChild(jamScoreHeader);
 
         // Create the element to display the jam score
         const jamScoreValue = document.createElement("div");
         jamScoreValue.appendChild(document.createTextNode("88"));
+        jamScoreValue.style.fontSize = "24pt";
         jamWrapper.appendChild(jamScoreValue);
         
         // Create the label for the trip point buttons
         const tripPointButtonHeader = document.createElement("div");
         tripPointButtonHeader.appendChild(document.createTextNode("Trip Points"));
+        tripPointButtonHeader.setAttribute("class", "label");
         jamWrapper.appendChild(tripPointButtonHeader);
 
         // Create the trip points
@@ -37,10 +47,16 @@ class JamElement extends HTMLElement {
         // Trip points can be a custom number depending on the game rules
         for (let i = 0; i <= maxPoints; i++) {
             let button = document.createElement("button");
-            button.setAttribute("id", "button" + i);
+            button.setAttribute("id", "pointButton" + i);
             button.appendChild(document.createTextNode(i.toString()));
+            button.style.width = "30px";
+            button.style.height = "20px";
+            button.style.marginLeft = "10px";
+            button.style.marginRight = "10px";
             tripPointButtons.appendChild(button);
         }
+        tripPointButtons.style.display = "block";
+        tripPointButtons.style.marginBottom = "10px";
         jamWrapper.appendChild(tripPointButtons);
 
         // Create the trip viewer
@@ -48,15 +64,16 @@ class JamElement extends HTMLElement {
         // Create example data for the trip viewer for debug purposes
         for (let i = 1; i < 11; i++) {
             let tripView = document.createElement("button");
-            tripView.appendChild(document.createTextNode("Trip " + i.toString()));
+            tripView.setAttribute("id", "tripButton" + i);
+            tripView.setAttribute("value", i);
+            tripView.appendChild(document.createTextNode("Trip " + i));
             tripView.appendChild(document.createElement("br"));
             tripView.appendChild(document.createTextNode("0"))
             tripViewer.appendChild(tripView);
         }
+        tripViewer.style.whiteSpace = "nowrap";
+        tripViewer.style.overflowX = "scroll";
         jamWrapper.appendChild(tripViewer);
-
-        
-
 
 
         shadow.appendChild(jamWrapper);
