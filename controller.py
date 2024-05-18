629c45d0e0f52bceeed7155109f4ec93fea2cc91
Mitch Weisbrod <mitch@theweisbrods.com>
2024-05-17 23:49:36 -0700

    logging

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-from roller_derby import Bout
+from roller_derby import BoutManager, ClientException
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import FileResponse
@@ -20,8 +20,6 @@ logging.basicConfig(
     style="{",
     level=None,
 )
-log: logging.Logger = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
 
 
 def getTick():
@@ -34,6 +32,9 @@ async def serve(port: int = 8000, *, debug: bool = False) -> None:
         raise TypeError(f"port must be int, not {type(port).__name__}")
     if not 1 <= port <= 65535:
         raise ValueError("port number is invalid")
+    if debug:
+        log.setLevel(logging.DEBUG)
+    app.debug = debug
     log.info(f"Starting NSO Bridge on port {port}")
     config: uvicorn.Config = uvicorn.Config(
         app, host="0.0.0.0", port=port, log_level="critical"
@@ -87,38 +88,44 @@ async def _sync(sessionId: str, *args, **kwargs) -> dict:
     return {"data": None, "tick": tick}
 
 
-async def _handleEvent(command: str, sessionId: str, *args, **kwargs) -> dict:
-    log.debug(f"Handling event '{command}' with args {args}.")
+async def _handleEvent(command: str, sessionId: str, *args) -> dict:
+    log.debug(f"Handling event '{command}' with args: {args}.")
     response: dict = {"data": None}
     try:
         if command not in _commandTable:
-            log.debug(f"'{command}' does not exist.")
-            raise NotImplementedError(f"Unknown command '{command}'.")
+            log.debug(f"The '{command}' handler does not exist.")
+            raise ClientException(f"Unknown command '{command}'.")
         func = _commandTable[command]
-        response["data"] = func(None, *args, **kwargs)  # FIXME: None to Bout
-    # TODO: add ClientException to return a warning message, not a traceback
-    except Exception as e:
+        response["data"] = func(*args)
+    except (Exception, ClientException) as e:
+        response["tick"] = getTick()
         response["error"] = {
             "name": type(e).__name__,
             "message": str(e),
         }
         traceback: None | TracebackType = e.__traceback__
-        if isinstance(traceback, TracebackType):
-            response["error"] |= {
-                "file": os.path.split(traceback.tb_frame.f_code.co_filename)[-1],
-                "lineNumber": traceback.tb_lineno,
-            }
-    response["tick"] = getTick()
+        if not isinstance(e, ClientException) and traceback is not None:
+            fileName: str = os.path.split(traceback.tb_frame.f_code.co_filename)[-1]
+            lineNumber: int = traceback.tb_lineno
+            log.error(f"{type(e).__name__}: {str(e)} ({fileName}, {lineNumber})")
     return response
 
 
-_commandTable: dict[str, Callable] = dict()
+_jinja = Jinja2Templates(directory="templates")
+
 _socket: socketio.AsyncServer = socketio.AsyncServer(
     cors_allowed_origins="*", async_mode="asgi"
 )
-_jinja = Jinja2Templates(directory="templates")
+_socket.on("connect", _handleConnect)
+_socket.on("disconnect", _handleDisconnect)
+_socket.on("sync", _sync)
+_socket.on("*", _handleEvent)
+
+_commandTable: dict[str, Callable] = dict()
+
+log: logging.Logger = logging.getLogger(__name__)
+game: BoutManager = BoutManager()
 app: Starlette = Starlette(
-    debug=True,
     routes=[
         Route("/", _renderTemplate),
         Mount("/static", app=StaticFiles(directory="static"), name="static"),
@@ -126,10 +133,6 @@ app: Starlette = Starlette(
         Route("/{file:str}", _renderTemplate),
     ],
 )
-_socket.on("connect", _handleConnect)
-_socket.on("disconnect", _handleDisconnect)
-_socket.on("sync", _sync)
-_socket.on("*", _handleEvent)
 
 
 if __name__ == "__main__":
