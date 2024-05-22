a14367c261b632ae3f6e2858ed04645c51e98fe3
Mitch Weisbrod <mitch@theweisbrods.com>
2024-05-21 19:59:37 -0700

    migrate functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { io } from 'socket.io-client';
+
+var userId = localStorage.getItem("userId");
+const socket = io(window.location.host, { auth: { token: userId } });
+var epsilon = 0;
+
+export function getTick() {
+  return Math.round(window.performance.now()) - epsilon;
+}
+
+export async function getEpsilon(iterations) {
+  let sum = 0;
+  let oldServerTime = 0;
+  for (var i = 0; i < iterations; ++i) {
+    const start = window.performance.now();
+    oldServerTime = await socket.emitWithAck("sync");
+    oldServerTime = oldServerTime.tick;  // TODO: streamline this 
+    sum += (window.performance.now() - start);
+  }
+  const currentServerTime = oldServerTime + Math.round((sum / 2) / iterations);
+  return Math.round(window.performance.now()) - currentServerTime;
+}
+
 function App() {
+  socket.once("userId", (newUserId) => {
+    console.log("Got User ID: " + newUserId);
+    localStorage.setItem("userId", newUserId);
+    userId = newUserId;
+  });
+  socket.on("connect", async () => {
+    await getEpsilon(10);
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
