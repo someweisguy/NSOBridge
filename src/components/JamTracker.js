0819395fb7f74e48fb9adccd07895e3bce284de9
Mitch Weisbrod <mitch@theweisbrods.com>
2024-05-22 17:11:50 -0700

    check prop

--- src/components/JamTracker.js
+++ src/components/JamTracker.js
@@ -5,7 +5,7 @@ export function JamTracker({ team }) {
   const [trips, setTrips] = useState([]);
   useEffect(() => {
     socket.on("jamUpdate", (payload) => {
-      console.log(payload.data[team].trips);
+      console.log(payload);
       let newTrips = payload.data[team].trips.map((tripData) => tripData.points);
       setTrips(newTrips);
     });
@@ -13,20 +13,24 @@ export function JamTracker({ team }) {
       socket.removeListener("jamUpdate");
     };
   }, []);
-
-
+  
   function handleClick() {
     socket.emitWithAck("addTrip", team, 0, 0);
     let newTrips = [...trips];
     newTrips.push(0);
     console.log(newTrips);
-
+    
     setTrips(newTrips);
+  }
+
+  // The `team` prop must be either "home" or "away".
+  if (team != "home" && team != "away") {
+    throw Error();
   }
 
   // Render each trip as a button
   let tripButtons = trips.map((trip, i) =>
-    <button>
+    <button key={i}>
       <small>Trip {i + 1}</small>
       <br />{trip}
     </button>
