18213f8f031384d848280e0f27dc96de20b8bff0
Mitch Weisbrod <mitch@theweisbrods.com>
2024-05-26 17:38:03 -0700

    scrolling buttons

--- src/components/JamComponent.js
+++ src/components/JamComponent.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { socket, getTick } from "../App";
 import "./JamComponent.css"
 
@@ -6,18 +6,8 @@ import "./JamComponent.css"
 function TripComponent({ team, periodIndex, jamIndex }) {
   const [trips, setTrips] = useState([]);
   const [activeTrip, setActiveTrip] = useState(0);
+  const scrollBar = useRef(null);
 
-  function updateTrips(data) {
-    console.log(data);
-    if (data.team != team) {
-      return;
-    }
-    let newTrips = data.trips.map((trip) => trip.points);
-    if (activeTrip == trips.length) {
-      setActiveTrip(newTrips.length);
-    }
-    setTrips(newTrips)
-  }
 
   useEffect(async () => {
     const apiCall = "getJamTrip";
@@ -30,6 +20,17 @@ function TripComponent({ team, periodIndex, jamIndex }) {
     return () => { socket.removeListener(apiCall); };
   }, []);
 
+  function updateTrips(data) {
+    if (data.team !== team) {
+      return;
+    }
+    let newTrips = data.trips.map((trip) => trip.points);
+    if (activeTrip === trips.length) {
+      setActiveTrip(newTrips.length);
+    }
+    setTrips(newTrips)
+  }
+
   async function setPoints(points) {
     const tick = getTick();
     const response = await socket.emitWithAck("setJamTrip",
@@ -39,9 +40,26 @@ function TripComponent({ team, periodIndex, jamIndex }) {
     }
   }
 
+  async function deleteTrip() {
+    const tick = getTick()
+    const response = await socket.emitWithAck("deleteJamTrip", 
+      { team: team, tripIndex: activeTrip, tick: tick });
+    if (response && !response.error) {
+      let newTrips = trips.filter((_, tripIndex) => tripIndex !== activeTrip);
+      setTrips(newTrips);
+      if (activeTrip > 0) {
+        setActiveTrip(activeTrip - 1);
+      }
+    }
+  }
+
+  function scroll(amount) {
+    scrollBar.current.scrollLeft += amount;
+  }
+
   // Render the points buttons
   const pointButtons = [];
-  if (activeTrip == 0) {
+  if (activeTrip === 0) {
     // Render No-Pass/No-Penalty and Initial Pass buttons
     const className = "initial";
     pointButtons.push(
@@ -59,9 +77,10 @@ function TripComponent({ team, periodIndex, jamIndex }) {
     const className = "points";
     const disableIndex = activeTrip < trips.length ? trips[activeTrip] : -1;
     for (let i = 0; i <= 4; i++) {
-      const disabled = i == disableIndex;
+      const disabled = i === disableIndex;
       pointButtons.push(
-        <button key={i} className={className} disabled={disabled} onClick={() => setPoints(i)}>
+        <button key={i} className={className} disabled={disabled} 
+          onClick={() => setPoints(i)}>
           {i}
         </button>
       );
@@ -82,25 +101,13 @@ function TripComponent({ team, periodIndex, jamIndex }) {
 
   // Determine if the "delete trip" button is visible
   let visibility = "hidden";
-  if (activeTrip < trips.length && (activeTrip > 0 || trips.length == 1)) {
+  if (activeTrip < trips.length && (activeTrip > 0 || trips.length === 1)) {
     visibility = "visible";
-  }
-
-  async function deleteTrip() {
-    const tick = getTick()
-    const response = await socket.emitWithAck("deleteJamTrip", { team: team, tripIndex: activeTrip, tick: tick })
-    if (response && !response.error) {
-      let newTrips = trips.filter((points, tripIndex) => tripIndex != activeTrip);
-      setTrips(newTrips);
-      if (activeTrip > 0) {
-        setActiveTrip(activeTrip - 1);
-      }
-    }
   }
 
   return (
     <div className="tripComponent">
-      
+
       <div className="tripInput">
         {pointButtons}
       </div>
@@ -111,13 +118,13 @@ function TripComponent({ team, periodIndex, jamIndex }) {
       </div>
 
       <div className="scrollBar">
-        <button>&lt;</button>
-        <div className="trips">
+        <button onClick={() => scroll(-50)}>&lt;</button>
+        <div ref={scrollBar} className="trips">
           {tripViewButtons}
         </div>
-        <button>&gt;</button>
+        <button onClick={() => scroll(50)}>&gt;</button>
       </div>
-      
+
     </div>
   );
 }
@@ -125,7 +132,7 @@ function TripComponent({ team, periodIndex, jamIndex }) {
 function LeadComponent({ periodIndex, jamIndex, team }) {
 
   return (
-    <div style={{border: "1px solid black"}}>
+    <div style={{ border: "1px solid black" }}>
       Hello world!
     </div>
   );
@@ -133,14 +140,14 @@ function LeadComponent({ periodIndex, jamIndex, team }) {
 
 
 export function TeamJamComponent({ periodIndex, jamIndex, team }) {
-  const [callReason, setCallReason] = useState(null);
+  // const [callReason, setCallReason] = useState(null); // TODO
 
 
   // TODO: Add lead, lost, starPass, noPivot
 
 
   return (
-    <div style={{width: "300px"}}>
+    <div style={{ width: "400px" }}>
       <TripComponent team={team} />
       <LeadComponent team={team} />
     </div>
