8eca2b30564b816f0e8469e8db5feb75de4e4038
Mitch Weisbrod <mitch@theweisbrods.com>
2024-06-02 10:22:16 -0700

    add starPass api

--- src/test.py
+++ src/test.py
@@ -84,6 +84,31 @@ async def jamLost(payload: dict[str, Any]) -> None | dict[str, Any]:
 
     # Broadcast the updates
     await server.emit("jamTrips", teamJam.encode())
+        
+@server.register
+async def jamStarPass(payload: dict[str, Any]) -> None | dict[str, Any]:
+    kwargs: dict[str, Any] = payload["kwargs"]
+
+    # Get the desired Bout
+    bout: Bout = server.bouts.currentBout
+
+    # Get the desired Jam and Jam Team
+    jam: Jam = bout.currentJam  # TODO: periodIndex, jamIndex
+    teamJam: Jam.Team = jam[kwargs["team"]]
+
+    # Handle the method
+    match payload["method"]:
+        case "get":
+            # Just ack with the Jam Team
+            return teamJam.encode()
+        case "set":
+            # Set the Jam starPass variable appropriately
+            teamJam.starPass = kwargs["tripIndex"]
+        case _ as default:
+            raise ClientException(f"Unknown method '{default}'.")
+
+    # Broadcast the updates
+    await server.emit("jamTrips", teamJam.encode())
 
 
 if __name__ == "__main__":
