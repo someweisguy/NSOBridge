a2adf398864c7238aab8cf9bb7de9951d9ccfc2c
Mitch Weisbrod <mitch@theweisbrods.com>
2024-06-06 19:52:08 -0700

    rough start tracking

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -4,28 +4,13 @@ import { useInterface, getLatency } from "../App.js"
 export default function TimerComponent({ }) {
     const [timerElapsed, setTimerElapsed] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
+    const accumulatedMilliseconds = useRef(0);
     const runningSince = useRef(Date.now());
-    const intervalId = useRef(null);
-
-    // const timerCallback = useCallback(({alarm, elapsed, running}) => {
-    //     if (intervalId.current !== null) {
-    //         clearInterval(intervalId.current);
-    //         intervalId.current = null;
-    //     }
-    //     if (running) {
-    //         runningSince.current = Date.now() - getLatency();
-    //         intervalId.current = setInterval(() => {
-    //             const elapsedSinceLast = Date.now() - runningSince.current
-    //             setTimerElapsed(elapsedSinceLast + elapsed);
-    //         }, 50);
-    //     }
-    //     setTimerElapsed(elapsed + getLatency());
-    // }, [timerElapsed]);
 
     useEffect(() => {
         if (isRunning) {
             const intervalId = setInterval(() => {
-                setTimerElapsed(Date.now() - runningSince.current)
+                setTimerElapsed(Date.now() - runningSince.current + accumulatedMilliseconds.current)
             }, 50);
             return () => clearInterval(intervalId);
         }
@@ -33,14 +18,16 @@ export default function TimerComponent({ }) {
 
 
     function cb({ alarm, elapsed, running }) {
+        console.log(elapsed)
         runningSince.current = Date.now() - getLatency();
-        setTimerElapsed(elapsed);
+        accumulatedMilliseconds.current = elapsed;
+        setTimerElapsed(elapsed + (running ? getLatency() : 0));
         setIsRunning(running);
     }
     useInterface("getJamTimer", cb);
 
 
     return (
-        <h1>{timerElapsed}</h1>
+        <h1>{((timerElapsed + accumulatedMilliseconds.current) / 1000.0).toFixed(1)}</h1>
     );
 }
