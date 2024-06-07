76b42b5756f1f5832ba1a87c4a0ca35792c5d87e
Mitch Weisbrod <mitch@theweisbrods.com>
2024-06-06 22:04:00 -0700

    code cleanup

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -1,34 +1,8 @@
 import { useState, useRef, useCallback, useEffect } from "react";
 import { useInterface, getLatency } from "../App.js"
 
-export default function TimerComponent({ }) {
-    const [runTime, setRunTime] = useState(0);
-    const [maxRunTime, setMaxRunTime] = useState(null);
-    const [isRunning, setIsRunning] = useState(false);
-    const accumulated = useRef(0);
-
-    function timerCallback({ alarm, elapsed, running }) {
-        setRunTime(elapsed + (running ? getLatency() : 0));
-        accumulated.current = elapsed;
-        setMaxRunTime(alarm);
-        setIsRunning(running);
-    }
-    useInterface("getJamTimer", timerCallback);
-
-    useEffect(() => {
-        if (isRunning) {
-            const runningSince = Date.now() - getLatency();
-            const intervalId = setInterval(() => {
-                setRunTime(Date.now() - runningSince)
-            }, 50);
-            return () => clearInterval(intervalId);
-        }
-    }, [isRunning]);
-
+function formatTimeString(millisRemaining) {
     let timeString = "";
-
-    // Format the time string for a count-down timer
-    const millisRemaining = maxRunTime - (runTime + accumulated.current);
     if (millisRemaining > 0) {
         let hours = Math.floor((millisRemaining / (1000 * 60 * 60)) % 24);
         if (hours > 0) {
@@ -54,11 +28,49 @@ export default function TimerComponent({ }) {
         }
     } else {
         timeString = "0.0";
+    }
+    return timeString;
+}
+
+
+export default function TimerComponent({ }) {
+    const [runTime, setRunTime] = useState(0);
+    const [maxRunTime, setMaxRunTime] = useState(null);
+    const [isRunning, setIsRunning] = useState(false);
+    const accumulated = useRef(0);
+
+    function timerCallback({ alarm, elapsed, running }) {
+        setRunTime(elapsed + (running ? getLatency() : 0));
+        accumulated.current = elapsed;
+        setMaxRunTime(alarm);
+        setIsRunning(running);
+    }
+    useInterface("getJamTimer", timerCallback);
+
+    useEffect(() => {
         if (isRunning) {
-            // Stop the interval from running to reduce CPU load
+            const runningSince = Date.now() - getLatency();
+            const intervalId = setInterval(() => {
+                setRunTime(Date.now() - runningSince)
+            }, 50);
+            return () => clearInterval(intervalId);
+        }
+    }, [isRunning]);
+
+
+    let timerMillis = 0;
+    if (maxRunTime !== null) {
+        // Format the time string for a count-down timer
+        timerMillis = maxRunTime - (runTime + accumulated.current);
+        if (isRunning && timerMillis <= 0) {
+            // Stop interval to reduce CPU load
             setIsRunning(false);
         }
+    } else {
+        timerMillis = runTime + accumulated.current;
     }
+
+    const timeString = formatTimeString(timerMillis);
 
     return (
         <h1>{timeString}</h1>
