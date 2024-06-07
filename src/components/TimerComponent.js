c2a16ccd02460d14ca0ad31380e420bb3a5d94b3
Mitch Weisbrod <mitch@theweisbrods.com>
2024-06-06 18:22:39 -0700

    simplify timer

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -1,25 +1,43 @@
 import { useState, useRef, useCallback, useEffect } from "react";
 import { useInterface, getLatency } from "../App.js"
 
-export default function TimerComponent({}) {
+export default function TimerComponent({ }) {
     const [timerElapsed, setTimerElapsed] = useState(0);
-    const runningSince = useRef(null);
+    const [isRunning, setIsRunning] = useState(false);
+    const runningSince = useRef(Date.now());
     const intervalId = useRef(null);
 
-    const timerCallback = useCallback(({alarm, elapsed, running}) => {
-        if (intervalId.current !== null) {
-            clearInterval(intervalId.current);
-            intervalId.current = null;
-        }
-        if (running) {
-            runningSince.current = Date.now() - getLatency();
-            intervalId.current = setInterval(() => {
-                setTimerElapsed(Date.now() - runningSince.current + elapsed);
+    // const timerCallback = useCallback(({alarm, elapsed, running}) => {
+    //     if (intervalId.current !== null) {
+    //         clearInterval(intervalId.current);
+    //         intervalId.current = null;
+    //     }
+    //     if (running) {
+    //         runningSince.current = Date.now() - getLatency();
+    //         intervalId.current = setInterval(() => {
+    //             const elapsedSinceLast = Date.now() - runningSince.current
+    //             setTimerElapsed(elapsedSinceLast + elapsed);
+    //         }, 50);
+    //     }
+    //     setTimerElapsed(elapsed + getLatency());
+    // }, [timerElapsed]);
+
+    useEffect(() => {
+        if (isRunning) {
+            const intervalId = setInterval(() => {
+                setTimerElapsed(Date.now() - runningSince.current)
             }, 50);
+            return () => clearInterval(intervalId);
         }
-        setTimerElapsed(elapsed + getLatency());
-    }, [timerElapsed]);
-    useInterface("getJamTimer", timerCallback);
+    }, [isRunning]);
+
+
+    function cb({ alarm, elapsed, running }) {
+        runningSince.current = Date.now() - getLatency();
+        setTimerElapsed(elapsed);
+        setIsRunning(running);
+    }
+    useInterface("getJamTimer", cb);
 
 
     return (
