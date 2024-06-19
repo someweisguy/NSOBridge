426b183414e704b9195b41b80a428d348d280faa
Mitch Weisbrod <mitch@theweisbrods.com>
2024-06-18 21:33:55 -0700

    shrink API

--- src/scoreApi.py
+++ src/scoreApi.py
@@ -5,18 +5,6 @@ import server
 
 
 @server.register
-async def getJamTrips(team: str) -> dict[str, Any]:
-    # Get the desired Bout
-    bout: Bout = server.bouts.currentBout
-
-    # Get the desired Jam and Jam Team
-    jam: Jam = bout.currentJam  # TODO: periodIndex, jamIndex
-    teamJam: Jam.Team = jam[team]
-
-    return {"team": team, "trips": [trip.encode() for trip in teamJam.trips]}
-
-
-@server.register
 async def setJamTrips(
     team: str, tripIndex: int, tripPoints: int, timestamp: datetime
 ) -> None:
@@ -28,7 +16,8 @@ async def setJamTrips(
     teamJam: Jam.Team = jam[team]
 
     teamJam.setTrip(tripIndex, tripPoints, timestamp)
-    await server.emit("getJamTrips", await getJamTrips(team))
+
+    await server.emit("jam", jam.encode())
 
 
 @server.register
@@ -41,7 +30,8 @@ async def delJamTrips(team: str, tripIndex: int) -> None:
     teamJam: Jam.Team = jam[team]
 
     teamJam.deleteTrip(tripIndex)
-    await server.emit("getJamTrips", await getJamTrips(team))
+
+    await server.emit("jam", jam.encode())
 
 
 @server.register
@@ -66,20 +56,7 @@ async def setJamInitial(team: str, timestamp: datetime) -> None:
     points: int = 0  # TODO: if is overtime, set to 4 points
     teamJam.setTrip(0, points, timestamp)
 
-    await server.emit("getJamTrips", await getJamTrips(team))
-    await server.emit("getJamLead", await getJamLead(team))
-
-
-@server.register
-async def getJamLead(team: str) -> dict[str, Any]:
-    # Get the desired Bout
-    bout: Bout = server.bouts.currentBout
-
-    # Get the desired Jam and Jam Team
-    jam: Jam = bout.currentJam  # TODO: periodIndex, jamIndex
-    teamJam: Jam.Team = jam[team]
-
-    return {"team": team, "lead": teamJam.lead}
+    await server.emit("jam", jam.encode())
 
 
 @server.register
@@ -93,19 +70,7 @@ async def setJamLead(team: str, lead: bool) -> None:
     teamJam.lead = lead
 
     # Broadcast the updates
-    await server.emit("getJamLead", await getJamLead(team))
-
-
-@server.register
-async def getJamLost(team: str) -> dict[str, Any]:
-    # Get the desired Bout
-    bout: Bout = server.bouts.currentBout
-
-    # Get the desired Jam and Jam Team
-    jam: Jam = bout.currentJam  # TODO: periodIndex, jamIndex
-    teamJam: Jam.Team = jam[team]
-
-    return {"team": team, "lost": teamJam.lost}
+    await server.emit("jam", jam.encode())
 
 
 @server.register
@@ -118,19 +83,7 @@ async def setJamLost(team: str, lost: bool) -> None:
     teamJam: Jam.Team = jam[team]
     teamJam.lost = lost
 
-    await server.emit("getJamLost", await getJamLost(team))
-
-
-@server.register
-async def getJamStarPass(team: str) -> dict[str, Any]:
-    # Get the desired Bout
-    bout: Bout = server.bouts.currentBout
-
-    # Get the desired Jam and Jam Team
-    jam: Jam = bout.currentJam  # TODO: periodIndex, jamIndex
-    teamJam: Jam.Team = jam[team]
-
-    return {"team": team, "starPass": teamJam.starPass}
+    await server.emit("jam", jam.encode())
 
 
 @server.register
@@ -142,7 +95,7 @@ async def setJamStarPass(team: str, tripIndex: None | int) -> None:
     jam: Jam = bout.currentJam  # TODO: periodIndex, jamIndex
     teamJam: Jam.Team = jam[team]
     teamJam.starPass = tripIndex
-    teamJam.lost = True
+    if tripIndex is not False:
+        teamJam.lost = True
 
-    await server.emit("getJamStarPass", await getJamStarPass(team))
-    await server.emit("getJamLost", await getJamLost(team))
+    await server.emit("jam", jam.encode())
