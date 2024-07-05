f15e7dfa64bf269a5e7b1a2c3345d9cf4c7b0f65
Mitch Weisbrod <mitch@theweisbrods.com>
2024-07-05 10:29:00 -0700

    rough-in ABC

--- src/roller_derby/timer.py
+++ src/roller_derby/timer.py
@@ -1,14 +1,104 @@
 from datetime import datetime, timedelta
 from .encodable import Encodable
-from typing import Any, Callable
+from typing import Any, Callable, Protocol
+import server
 import asyncio
+
+"""
+Period:
+    start time to derby
+    
+
+Jam:
+    start lineup
+    
+    
+
+expect
+start
+stop
+
+isRunning
+isExpected
+
+getElapsed
+setElapsed
+getRemaining
+
+setTimeToStart
+getTimeToStart
+
+
+    
+
+
+"""
+
+
+class Expectable(Encodable):
+    def __init__(self, expect: timedelta, event: timedelta) -> None:
+        self._expectTimer: Timer = Timer(expect)
+        self._eventTimer: Timer = Timer(event)
+
+    def expect(self, timestamp: datetime) -> None:
+        if self._expectTimer.isRunning():
+            raise RuntimeError(f"this {type(self).__name__} is already expected")
+        self._expectTimer.start(timestamp, lambda _: server.update(self))
+
+    def start(self, timestamp: datetime) -> None:
+        if self._eventTimer.isRunning():
+            raise RuntimeError(f"this {type(self).__name__} is already running")
+        if self._expectTimer.isRunning():
+            self._expectTimer.stop(timestamp)
+        self._eventTimer.start(timestamp, lambda _: server.update(self))
+
+    def stop(self, timestamp: datetime) -> None:
+        if not self._eventTimer.isRunning():
+            raise RuntimeError(f"this {type(self).__name__} is not running")
+        if self._expectTimer.isRunning():
+            self._expectTimer.stop(timestamp)
+        self._eventTimer.stop(timestamp)
+
+    def isExpected(self) -> bool:
+        return self._expectTimer.isRunning()
+
+    def isRunning(self) -> bool:
+        return self._eventTimer.isRunning()
+
+    def getElapsed(self) -> timedelta:
+        return self._eventTimer.getElapsed()
+
+    def setElapsed(self, hours: int = 0, minutes: int = 0, seconds: int = 0) -> None:
+        self._eventTimer.setElapsed(hours, minutes, seconds)
+
+    def getRemaining(self) -> timedelta:
+        return self._eventTimer.getRemaining()
+
+    def getTimeToStart(self) -> timedelta:
+        return self._expectTimer.getRemaining()
+
+    def setTimeToStart(
+        self, hours: int = 0, minutes: int = 0, seconds: int = 0
+    ) -> None:
+        pass  # TODO
 
 
 class Timer(Encodable):
-    def __init__(self, *, hours: int = 0, minutes: int = 0, seconds: int = 0) -> None:
+    def __init__(
+        self,
+        time: None | timedelta = None,
+        *,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+    ) -> None:
         # Set the Timer alarm value if necessary
-        alarm: timedelta = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-        self._alarm: None | timedelta = alarm if alarm.total_seconds() > 0 else None
+        self._alarm: None | timedelta = None
+        if time is not None and time.total_seconds() > 0:
+            self._alarm = time
+        time = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        if time.total_seconds() > 0:
+            self._alarm = time
 
         self._task: None | asyncio.Task = None
         self._startTime: None | datetime = None
