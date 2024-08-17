48884bb5090f53c38ffb071e1a689a08d5910013
Mitch Weisbrod <mitch@theweisbrods.com>
2024-08-16 18:58:19 -0700

    change quotes

--- backend/src/server.py
+++ backend/src/server.py
@@ -22,13 +22,13 @@ import uvicorn
 
 
 logging.basicConfig(
-    format="{levelname}: {message}",
-    datefmt="%m/%d/%Y %H:%M:%S",
-    style="{",
+    format='{levelname}: {message}',
+    datefmt='%m/%d/%Y %H:%M:%S',
+    style='{',
     level=logging.INFO,
 )
 
-"""Type alias for the types of values that can be serialized into JSON. """
+'''Type alias for the types of values that can be serialized into JSON. '''
 API: TypeAlias = (
     None
     | dict[str, None | int | float | str | bool | dict | list]
@@ -60,7 +60,7 @@ class Encodable(ABC):
 
     @abstractmethod
     def encode(self) -> dict[str, Encodable.PRIMITIVE]:
-        """Encodes the Encodable into a dictionary which can then be sent to a
+        '''Encodes the Encodable into a dictionary which can then be sent to a
         client. This method is called recursively so that each sub-class is
         encoded into a sub-dictionary. All private members of the Encodable
         (i.e. members prefixed with an underscore) are converted to public
@@ -68,13 +68,13 @@ class Encodable(ABC):
 
         Returns:
             dict: A dictionary representing the Encodable.
-        """
+        '''
         raise NotImplementedError()
 
     @staticmethod
     # @abstractmethod # TODO
     def decode(json: Encodable.PRIMITIVE) -> Encodable:
-        """Creates a new Encodable object from a dictionary.
+        '''Creates a new Encodable object from a dictionary.
 
         Args:
             json (dict): A dictionary object created from a JSON object.
@@ -82,12 +82,12 @@ class Encodable(ABC):
         Returns:
             Encodable: A new Encodable with the same attributes as the JSON
             object.
-        """
+        '''
         raise NotImplementedError()
 
 
 async def serve(port: int, *, debug: bool = False) -> None:
-    """Start and serve the scoreboard app on the specified port.
+    '''Start and serve the scoreboard app on the specified port.
 
     Args:
         port (int): The port number to serve the scoreboard.
@@ -97,32 +97,32 @@ async def serve(port: int, *, debug: bool = False) -> None:
         TypeError: if the port number is is not an int or the debug arg is not
         a bool.
         ValueError: if the port number is not between 1 and 65535 (inclusive).
-    """
+    '''
     if not isinstance(port, int):
-        raise TypeError(f"port must be int, not {type(port).__name__}")
+        raise TypeError(f'port must be int, not {type(port).__name__}')
     if not 1 <= port <= 65535:
-        raise ValueError("port number is invalid")
+        raise ValueError('port number is invalid')
     if not isinstance(debug, bool):
-        raise TypeError(f"debug must be bool, not {type(port).__name__}")
+        raise TypeError(f'debug must be bool, not {type(port).__name__}')
     if debug:
         log.setLevel(logging.DEBUG)
     _app.debug = debug
-    log.info(f"Starting NSO Bridge on port {port}")
+    log.info(f'Starting NSO Bridge on port {port}')
     config: uvicorn.Config = uvicorn.Config(
-        _app, host="0.0.0.0", port=port, log_level="critical"
+        _app, host='0.0.0.0', port=port, log_level='critical'
     )
     server: uvicorn.Server = uvicorn.Server(config)
     await server.serve()
-    log.info("NSO Bridge was successfully shut down.")
+    log.info('NSO Bridge was successfully shut down.')
 
 
 def register(
     command: None | Callable = None,
     *,
-    name: str = "",
+    name: str = '',
     overwrite: bool = False,
 ) -> Callable:
-    """A decorator to register server command methods. Server command methods
+    '''A decorator to register server command methods. Server command methods
     must not be asynchronous functions.
 
     Args:
@@ -137,17 +137,17 @@ def register(
 
     Returns:
         Callable: The original method.
-    """
+    '''
 
     def decorator(
         command: Callable[[dict[str, Any]], Any],
     ) -> Callable[[dict[str, Any]], Any]:
-        commandName: str = name if name != "" else command.__name__
+        commandName: str = name if name != '' else command.__name__
         if overwriting := (commandName in _commandTable) and not overwrite:
             raise LookupError(
-                f"The command '{commandName}' is already registered.")
-        gerund: str = "Adding" if not overwriting else "Overwriting"
-        log.debug(f"{gerund} '{commandName}' command")
+                f'The command \'{commandName}\' is already registered.')
+        gerund: str = 'Adding' if not overwriting else 'Overwriting'
+        log.debug(f'{gerund} \'{commandName}\' command')
         _commandTable[commandName] = command
         return command
 
@@ -157,8 +157,8 @@ def register(
 def update(encodable: Encodable) -> None:
     # TODO: documentation
     eventName: str = type(encodable).__name__
-    if hasattr(encodable, "API_NAME"):
-        eventName = getattr(encodable, "API_NAME")
+    if hasattr(encodable, 'API_NAME'):
+        eventName = getattr(encodable, 'API_NAME')
     loop = asyncio.get_running_loop()
     loop.create_task(emit(eventName, encodable.encode()))
 
@@ -166,7 +166,7 @@ def update(encodable: Encodable) -> None:
 async def emit(event: str, data: dict[str, Any], to: None | str = None,
                room: None | str = None, skip: None | str = None,
                namespace: None | str = None) -> None:
-    """Sends a Socket.IO message with the desired event name and data.
+    '''Sends a Socket.IO message with the desired event name and data.
 
     Args:
         event (str): The name of the event to send.
@@ -182,15 +182,15 @@ async def emit(event: str, data: dict[str, Any], to: None | str = None,
         send the message. Defaults to None.
         timestamp (None | int, optional): The server epoch at which the action
         originated. Defaults to None.
-    """
-    log.debug(f"Emit: '{event}' {data}")
+    '''
+    log.debug(f'Emit: \'{event}\' {data}')
     await _socket.emit(
         event, data, to=to, room=room, skip_sid=skip, namespace=namespace
     )
 
 
 async def _renderTemplate(request: Request) -> HTMLResponse:
-    """Renders the HTML response using the Jinja2 templating engine. All HTML
+    '''Renders the HTML response using the Jinja2 templating engine. All HTML
     templates must be found in the `web/templates/` directory.
 
     Args:
@@ -199,43 +199,43 @@ async def _renderTemplate(request: Request) -> HTMLResponse:
     Returns:
         HTMLResponse: An HTML response rendered from the Jinja2 templating
         engine.
-    """
-    file: str = "index.html"
-    if "file" in request.path_params:
-        file = f"{request.path_params["file"]}"
-    log.debug(f"Handling request for '{file}'.")
+    '''
+    file: str = 'index.html'
+    if 'file' in request.path_params:
+        file = f'{request.path_params['file']}'
+    log.debug(f'Handling request for \'{file}\'.')
     return _jinja.TemplateResponse(request, file)
 
 
 async def _handleConnect(sessionId: str, environ: dict[str, Any],
                          auth: dict[str, Any]) -> None:
-    """Handles a socket.io connection event.
+    '''Handles a socket.io connection event.
 
     Args:
         sessionId (str): The session ID of the corresponding connection.
         environ (dict): The web browser environment of the connection.
         auth (dict): The auth dictionary from the connection.
-    """
-    userId: None | str = None if auth is None else auth.get("token", None)
+    '''
+    userId: None | str = None if auth is None else auth.get('token', None)
     if userId is None:
         environStr: bytes = str(environ.items()).encode()
         userId = hashlib.md5(environStr, usedforsecurity=False).hexdigest()
-        await _socket.emit("userId", userId, to=sessionId)
+        await _socket.emit('userId', userId, to=sessionId)
     async with _socket.session(sessionId) as session:
-        session["userId"] = userId
+        session['userId'] = userId
 
 
 async def _dummyHandler(*_, **__) -> None:
-    """A dummy function to handle miscellaneous Socket.IO API. This is needed
+    '''A dummy function to handle miscellaneous Socket.IO API. This is needed
     to ensure that there aren't any argument exceptions with catch-all
     Socket.IO event handlers.
-    """
+    '''
     pass
 
 
 async def _handleEvent(command: str, sessionId: str,
                        json: dict[str, Any]) -> dict[str, Any]:
-    """Handles all socket.io events except for connection, disconnection, and
+    '''Handles all socket.io events except for connection, disconnection, and
     sync. This handler looks up the received command in a command table and
     calls the appropriate function, if it exists.
 
@@ -249,35 +249,35 @@ async def _handleEvent(command: str, sessionId: str,
 
     Returns:
         dict: A dictionary of the command response.
-    """
+    '''
     NOW: datetime = datetime.now()
-    log.debug(f"Handling event '{command}' with args: {json}.")
+    log.debug(f'Handling event \'{command}\' with args: {json}.')
     response: dict[str, Any] = dict()
     try:
         # Validate the request payload has all the required JSON keys
-        requiredKeys: tuple[str, ...] = ("latency",)
+        requiredKeys: tuple[str, ...] = ('latency',)
         if not all(key in json for key in requiredKeys):
-            raise ClientException("Invalid request payload.")
+            raise ClientException('Invalid request payload.')
 
         # Validate the latency value is between 0 and 5 seconds
-        if not isinstance(json["latency"], int) or 0 > json["latency"] > 5000:
-            raise ClientException("Client latency is invalid")
+        if not isinstance(json['latency'], int) or 0 > json['latency'] > 5000:
+            raise ClientException('Client latency is invalid')
 
         # Validate the command exists
         if command not in _commandTable:
-            log.debug(f"The '{command}' handler does not exist.")
-            raise ClientException(f"Unknown command '{command}'.")
+            log.debug(f'The \'{command}\' handler does not exist.')
+            raise ClientException(f'Unknown command \'{command}\'.')
 
         # Add commonly used arguments
-        json["timestamp"] = NOW - timedelta(milliseconds=json["latency"])
-        json["session"] = sessionId
-        if "uri" in json.keys() and isinstance(json["uri"], dict):
-            rawURI: dict[str, Any] = json["uri"]
-            if "bout" not in rawURI:
-                raise ClientException("bout must be specified")
-            period: int = rawURI["period"] if "period" in rawURI else -1
-            jam: int = rawURI["jam"] if "jam" in rawURI else -1
-            json["uri"] = URI(rawURI["bout"], period, jam)
+        json['timestamp'] = NOW - timedelta(milliseconds=json['latency'])
+        json['session'] = sessionId
+        if 'uri' in json.keys() and isinstance(json['uri'], dict):
+            rawURI: dict[str, Any] = json['uri']
+            if 'bout' not in rawURI:
+                raise ClientException('bout must be specified')
+            period: int = rawURI['period'] if 'period' in rawURI else -1
+            jam: int = rawURI['jam'] if 'jam' in rawURI else -1
+            json['uri'] = URI(rawURI['bout'], period, jam)
 
         # Get the function and call it with only the required arguments
         func: Callable[..., Awaitable[None | Collection]
@@ -290,14 +290,14 @@ async def _handleEvent(command: str, sessionId: str,
         data: None | Collection = await func(**json)
 
         # Build the response payload
-        response["status"] = "ok"
-        response["data"] = data
+        response['status'] = 'ok'
+        response['data'] = data
     except (Exception, ClientException) as e:
         # Return the exception and exception message
-        response["status"] = "error"
-        response["error"] = {
-            "name": type(e).__name__,
-            "message": str(e),
+        response['status'] = 'error'
+        response['error'] = {
+            'name': type(e).__name__,
+            'message': str(e),
         }
 
         # If the exception was not caused by the client, log a traceback
@@ -306,10 +306,10 @@ async def _handleEvent(command: str, sessionId: str,
             fileName: str = os.path.split(
                 traceback.tb_frame.f_code.co_filename)[-1]
             lineNumber: int = traceback.tb_lineno
-            log.error(f"{type(e).__name__}: {
-                      str(e)} ({fileName}, {lineNumber})")
+            log.error(f'{type(e).__name__}: {
+                      str(e)} ({fileName}, {lineNumber})')
     finally:
-        log.debug(f"Ack: {str(response)}")
+        log.debug(f'Ack: {str(response)}')
         # Return the current timestamp
         return response
 
@@ -321,23 +321,23 @@ log: logging.Logger = logging.getLogger(__name__)
 # bouts: bout.Series = bout.Series()
 
 _commandTable: dict[str, Callable[..., Awaitable[None | Collection]]] = dict()
-_socket: socketio.AsyncServer = socketio.AsyncServer(cors_allowed_origins="*",
-                                                     async_mode="asgi")
-_socket.on("connect", _handleConnect)
-_socket.on("disconnect", _dummyHandler)
-_socket.on("ping", _dummyHandler)
-_socket.on("*", _handleEvent)
+_socket: socketio.AsyncServer = socketio.AsyncServer(cors_allowed_origins='*',
+                                                     async_mode='asgi')
+_socket.on('connect', _handleConnect)
+_socket.on('disconnect', _dummyHandler)
+_socket.on('ping', _dummyHandler)
+_socket.on('*', _handleEvent)
 
-_webDir: Path = Path(__file__).parent.parent.parent / "frontend" / "build"
+_webDir: Path = Path(__file__).parent.parent.parent / 'frontend' / 'build'
 _jinja: Jinja2Templates = Jinja2Templates(directory=_webDir)
 _app: Starlette = Starlette(
     routes=[
-        Route("/", _renderTemplate),
-        # Mount("/static", app=StaticFiles(directory=f"{_webDir}/static"),
-        # name="static"),
-        Mount("/assets", app=StaticFiles(directory=_webDir / "assets"),
-              name="assets"),
-        Mount("/socket.io", app=socketio.ASGIApp(_socket)),
-        Route("/{file:str}", _renderTemplate),
+        Route('/', _renderTemplate),
+        # Mount('/static', app=StaticFiles(directory=f'{_webDir}/static'),
+        # name='static'),
+        Mount('/assets', app=StaticFiles(directory=_webDir / 'assets'),
+              name='assets'),
+        Mount('/socket.io', app=socketio.ASGIApp(_socket)),
+        Route('/{file:str}', _renderTemplate),
     ],
 )
