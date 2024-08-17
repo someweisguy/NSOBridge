016fd7fa1c185637deea836e0b9e51b04c585dad
Mitch Weisbrod <mitch@theweisbrods.com>
2024-08-16 21:01:02 -0700

    simplify main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,93 +1,32 @@
 import './App.css';
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, Suspense} from "react";
 import { ScoreboardEditor } from './components/JamComponent.jsx';
-import { onEvent, sendRequest } from './client.js';
+import { useSeries, useBout } from './customHooks.jsx';
+
 
 function App() {
-  const [isConnected, setIsConnected] = useState(false);
+  const series = useSeries();
   const [boutUuid, setBoutUuid] = useState(null);
+  const bout = useBout(boutUuid);
 
-  // Add connect and disconnect listeners
   useEffect(() => {
-    const unsubscribeConnect = onEvent("connect",
-      () => setIsConnected(true)
-    );
-    const unsubscribeDisconnect = onEvent("disconnect",
-      () => setIsConnected(false)
-    );
+    // TODO: verify UUID is in series
 
-    return () => {
-      unsubscribeConnect();
-      unsubscribeDisconnect();
-    };
-  }, []);
-
-  // Request server data on connection
-  useEffect(() => {
-    if (!isConnected) {
-      return;
+    if (series.length == 1) {
+      setBoutUuid(series[0])
+    } else if (series.length > 1) {
+      // TODO: show Bout selection page
+    } else {
+      // TODO: error - no Bouts available
     }
-
-    // Calculate connection latency then get server information
-    let ignore = false;
-    calculateLatency(10).then(() => {
-      sendRequest("getBouts").then((series) => {
-        if (ignore) {
-          return;  // Ignore old requests
-        }
-
-        // If the client already has a Bout UUID, ensure it is valid
-        if (boutUuid !== null) {
-          let hasValidBout = false;
-          for (let bout of series) {
-            if (boutUuid === bout.uuid) {
-              hasValidBout = true;
-              break;
-            }
-          }
-
-          // Refresh the Bout UUID so data may be reloaded
-          if (hasValidBout) {
-            let refreshedUuid = boutUuid;
-            setBoutUuid(refreshedUuid);
-            return;
-          }
-          setBoutUuid(null);  // Discard invalid Bout UUID
-        }
-
-        // Choose an action based on the Bouts available
-        if (series.length === 0) {
-          // TODO: Go to "Create new Bout"
-        } else if (series.length === 1) {
-          // Select the only created Bout
-          const bout = series[0];
-          setBoutUuid(bout.uuid);
-        } else {
-          // TODO: Go to "Select a Bout"
-        }
-      });
-    });
-
-    // Periodically recalculate the client-server latency
-    const intervalId = setInterval(() => calculateLatency(5), 25000);
-
-    return () => {
-      clearInterval(intervalId);
-      ignore = true;
-    };
-  }, [isConnected]);
-
-  // Show a loading icon if a Bout has not been selected
-  if (boutUuid === null) {
-    // TODO
-    return (
-      <></>
-    );
-  }
+  }, [series]);
 
   return (
     <div className="App">
-      <ScoreboardEditor boutUuid={boutUuid} />
+      <Suspense>
+        <p>{bout.uuid}</p>
+        <ScoreboardEditor bout={bout} />
+      </Suspense>
     </div>
   );
 }
