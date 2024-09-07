2901047ee9c01833fcf7207f750a4004b1c15208
Mitch Weisbrod <mitch@theweisbrods.com>
2024-09-07 11:52:20 -0700

    add uuid package

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,9 +1,11 @@
+import { v4 as uuid4 } from 'uuid';
+
 const socket = new WebSocket('ws://' + window.location.host + '/ws');
 const ackResolutions = new Map();
 
 export async function send(action, args = undefined) {
   const clientTimestamp = new Date();
-  const ackId = crypto.randomUUID();
+  const ackId = uuid4();
   const request = { action, args, clientTimestamp, ackId };
   socket.send(JSON.stringify(request));
 
