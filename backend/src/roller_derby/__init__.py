27bd33c7c2f7f959a626089d28b91cf6a28a9a8d
Mitch Weisbrod <mitch@theweisbrods.com>
2024-09-17 19:31:06 -0700

    formatting

--- backend/src/roller_derby/__init__.py
+++ backend/src/roller_derby/__init__.py
@@ -1,8 +1,8 @@
 # flake8: noqa
 
-from roller_derby.series import Series
-from roller_derby.bout import Bout
-from roller_derby.jam import Jam
+from .series import Series
+from .bout import Bout
+from .jam import Jam
 from uuid import UUID as BoutId
 
 bouts: Series = Series()
