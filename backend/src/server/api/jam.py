b388199c08dee8fafbd7b08b07eec295986f9b9a
Mitch Weisbrod <mitch@theweisbrods.com>
2024-09-25 20:35:01 -0700

    file consolidation

--- backend/src/server/api/jam.py
+++ backend/src/server/api/jam.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from roller_derby import BoutId, bouts, Jam
 from typing import Any
 import server
@@ -30,3 +31,65 @@ def getJam(boutId: BoutId, periodId: int, jamId: int) -> dict[str, Any]:
         'home': encode_team(jam.home),
         'away': encode_team(jam.away)
     }
+
+
+@server.register
+def setTrip(boutId: BoutId, periodId: int, jamId: int, team: str,
+            tripNum: int, points: int, timestamp: datetime,
+            validPass: bool = True) -> dict[str, Any]:
+    ...
+    # # Get the desired Bout and Jam
+    # bout: Bout = series.currentBout
+    # jam: Jam = bout[uri.period][uri.jam]
+
+    # # Attempt to set the lead jammer
+    # if jam.score[team].isLeadEligible() and validPass:
+    #     jam.score[team].lead = True
+
+    # jam.score[team].setTrip(tripNum, points, timestamp)
+
+
+@server.register
+def deleteTrip(boutId: BoutId, periodId: int, jamId: int, team: str,
+               tripNum: int) -> dict[str, Any]:
+    ...
+    # # Get the desired Bout and Jam
+    # bout: Bout = series.currentBout
+    # jam: Jam = bout[uri.period][uri.jam]
+
+    # jam.score[team].deleteTrip(tripNum)
+
+
+@server.register
+def setLead(boutId: BoutId, periodId: int, jamId: int, team: str,
+            lead: bool) -> dict[str, Any]:
+    ...
+    # # Get the desired Bout and Jam
+    # bout: Bout = series.currentBout
+    # jam: Jam = bout[uri.period][uri.jam]
+
+    # jam.score[team].lead = lead
+
+
+@server.register
+def setLost(boutId: BoutId, periodId: int, jamId: int, team: str,
+            lost: bool) -> dict[str, Any]:
+    ...
+    # # Get the desired Bout and Jam
+    # bout: Bout = series.currentBout
+    # jam: Jam = bout[uri.period][uri.jam]
+
+    # jam.score[team].lost = lost
+
+
+@server.register
+def setStarPass(boutId: BoutId, periodId: int, jamId: int, team: str,
+                tripNum: int) -> dict[str, Any]:
+    ...
+    # # Get the desired Bout and Jam
+    # bout: Bout = series.currentBout
+    # jam: Jam = bout[uri.period][uri.jam]
+
+    # jam.score[team].starPass = tripNum
+    # if tripNum is not None:
+    #     jam.score[team].lost = True
