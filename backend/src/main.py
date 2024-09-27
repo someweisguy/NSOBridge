5e4705efb302e4634a5466ae330d0925e3e2137e
Mitch Weisbrod <mitch@theweisbrods.com>
2024-09-26 21:09:51 -0700

    formatting

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,12 @@
 import asyncio
 import server
 import roller_derby
-import api
+
 
 if __name__ == '__main__':
     roller_derby.bouts.add()
+
+    import api
     server.set_adapter(api.adapter)
+
     asyncio.run(server.serve())
