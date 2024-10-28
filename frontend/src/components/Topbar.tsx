39f94572afba2f02b94cf681368cd5974db44b15
Mitch Weisbrod <mitch@theweisbrods.com>
2024-10-27 21:36:10 -0700

    add logo

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -2,8 +2,11 @@ import { ReactNode } from "react";
 
 export default function Topbar(): ReactNode {
   return (
-    <div className="sticky top-0 flex-row p-4 bg-slate-400">
-      <strong>NSO Bridge</strong>
+    <div className="container sticky top-0 flex flex-row items-center p-4 shadow-md place-content-start bg-slate-400 max-h-16">
+      <img src="skate.svg" className="flex-initial h-full pl-2"></img>
+      <div className="flex-none h-full px-6 place-content-center">
+        <strong>NSO Bridge</strong>
+      </div>
     </div>
   );
 }
