73abe967b3ad6ade93469055aa52bc9649d8e8fd
Mitch Weisbrod <mitch@theweisbrods.com>
2024-10-27 20:55:38 -0700

    rename component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import Header from "./components/Header.tsx";
+import Topbar from "./components/Topbar.tsx";
 import Sidebar from "./components/Sidebar.tsx";
 
 export default function App() {
   return (
     <div className="container flex flex-col w-full h-svh max-h-svh">
-      <Header />
+      <Topbar />
 
       <Sidebar>
         First <br /> hello <br /> hello <br /> hello <br />
--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 
-export default function Header(): ReactNode {
+export default function Topbar(): ReactNode {
   return (
     <div className="sticky top-0 flex-row p-4 bg-slate-400">
       <strong>NSO Bridge</strong>
     </div>
   );
 }
+
+
