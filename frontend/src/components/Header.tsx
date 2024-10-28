a463a3f7125ab966a3c1d7b75a33e304479d9ce5
Mitch Weisbrod <mitch@theweisbrods.com>
2024-10-27 20:49:15 -0700

    header update

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react";
 
 export default function Header(): ReactNode {
-  return <div className="sticky top-0 flex-row p-4 bg-slate-400">Header!</div>;
+  return (
+    <div className="sticky top-0 flex-row p-4 bg-slate-400">
+      <strong>NSO Bridge</strong>
+    </div>
+  );
 }
