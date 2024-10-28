d895f8365bf9fe46171c3e6c152c03b5a15be55c
Mitch Weisbrod <mitch@theweisbrods.com>
2024-10-27 18:40:34 -0700

    add header and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,49 @@
-import { useOnlineState, useLatency } from "./client";
-import { useSeries } from "./api/series";
-import { Suspense } from "react";
-import "./App.css";
+import { Suspense, PropsWithChildren } from "react";
 
-function App() {
-  const latency: number = useLatency();
-  const isConnected: boolean = useOnlineState();
+function NavBar() {
+  return <div className="sticky top-0 p-4 bg-slate-400">NavBar!</div>;
+}
 
+function SideBar({ children }: PropsWithChildren) {
   return (
-    <>
-      Connected: {isConnected.toString()}
-      <br />
-      Latency: {latency}ms
-      <br />
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <Body />
-      </Suspense>
-    </>
+    <div className="flex flex-row">
+      <div className="flex flex-row w-1/6 h-full bg-green-500">
+        <nav className="container fixed flex-col p-4">
+          <p>Link 1</p>
+          <p>Link 2</p>
+          <p>Link 3</p>
+          <p>Link 4</p>
+        </nav>
+      </div>
+
+      <div className="flex-auto p-4 bg-white">
+        <Suspense>{children}</Suspense>
+      </div>
+    </div>
   );
 }
 
+function App() {
+  return (
+    <div className="container flex flex-col w-full h-svh max-h-svh">
+      <NavBar />
 
-
-function Body() {
-  const series: Map<string, object> = useSeries();
-
-  if (series.size > 1) {
-    // TODO: display bout selection screen
-  } else if (series.size == 1) {
-    // Automatically select the only bout
-    const boutId: string = Array.from(series.keys())[0];
-    const bout: object | undefined = series.get(boutId)
-    return (
-    <>
-      {boutId}: {JSON.stringify(bout)}
-    </>
-    );
-  } else {
-    // TODO: go to the bout creation page
-  }
-
-  return <body>{JSON.stringify(series)}</body>;
+      <SideBar>
+        First <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+        Hello <br /> hello <br /> hello <br /> hello <br />
+      </SideBar>
+    </div>
+  );
 }
 
 export default App;
