7abf6765ec8dfb0d528d56cadd6a0bb5893b2793
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-03 09:19:17 -0800

    formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,24 +6,23 @@ export const SeriesContext = createContext(new Map<string, object>());
 
 export default function App() {
   return (
-
-      <MainContainer>
-        <Sidebar>
-          <Suspense>
-            First <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-            Hello <br /> hello <br /> hello <br /> hello <br />
-          </Suspense>
-        </Sidebar>
-      </MainContainer>
+    <MainContainer>
+      <Sidebar>
+        <Suspense>
+          First <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+          Hello <br /> hello <br /> hello <br /> hello <br />
+        </Suspense>
+      </Sidebar>
+    </MainContainer>
   );
 }
