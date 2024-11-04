12840889104780852d555b331248545dbeaf7417
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-03 20:29:37 -0800

    add TODO

--- frontend/src/hooks/series.ts
+++ frontend/src/hooks/series.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useGetter } from "./client";
 
+// TODO: add BoutAbstractType
 
 export function useSeries(): Map<string, object> {
   const series: object = useGetter("series");
