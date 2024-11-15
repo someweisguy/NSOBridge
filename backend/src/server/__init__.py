5867066af928a4d0dcc013915fd911f3f0d4cc36
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-14 19:33:55 -0800

    rename view

--- backend/src/server/__init__.py
+++ backend/src/server/__init__.py
@@ -1,15 +1,15 @@
 from server.controller import Controller
-from .model import Queryable
 
-controller = Controller()
+
+controller: Controller = Controller()
 
 
 async def serve(host: str = '0.0.0.0', port: int = 8000) -> None:
-    from server.view import view
+    from server.view import app
     import uvicorn
 
     # Configure and start the server
-    config: uvicorn.Config = uvicorn.Config(view, host=host, port=port,
+    config: uvicorn.Config = uvicorn.Config(app, host=host, port=port,
                                             log_config=None, access_log=False,
                                             log_level='warning')
     server: uvicorn.Server = uvicorn.Server(config)
@@ -18,15 +18,3 @@ async def serve(host: str = '0.0.0.0', port: int = 8000) -> None:
 
 def get_interfaces() -> list:
     raise NotImplementedError  # TODO: get list of interfaces available
-
-
-'''
-main -> view -> controller -> model -> series -> 
-
-
-
-Data -> Model -> Controller -> View
-
-
-
-'''
--- backend/src/server/view.py
+++ backend/src/server/view.py
@@ -1,29 +1,28 @@
-from typing import Any
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.routing import Mount
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-from .controller import Controller
+from typing import Any
 from . import controller
 import os
 
 
-frontend: Path = Path(os.getcwd()) / 'frontend' / 'dist'
-view: FastAPI = FastAPI(debug=True, routes=[
-    Mount('/assets', StaticFiles(directory=frontend / 'assets'))],
-    templates=Jinja2Templates(frontend),
+build_dir: Path = Path(os.getcwd()) / 'frontend' / 'dist'
+app: FastAPI = FastAPI(debug=True, routes=[
+    Mount('/assets', StaticFiles(directory=build_dir / 'assets'))],
+    templates=Jinja2Templates(build_dir),
 )
 
 
-@view.get("/")
+@app.get("/")
 async def index(request: Request):
-    templates: Jinja2Templates = view.extra['templates']
+    templates: Jinja2Templates = app.extra['templates']
     data: dict[str | float | int, Any] = controller.model.data.get_data()
     return templates.TemplateResponse("index.html", {'request': request,
                                                      'series': data})
 
 
-@view.websocket('/ws')
+@app.websocket('/ws')
 async def ws(websocket: WebSocket):
     await controller.handle_websocket(websocket)
