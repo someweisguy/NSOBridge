3f78d9237b0dd837730c3f1a9052a54462aa4989
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-16 14:39:32 -0800

    readability pass

--- frontend/src/components/TripSetter.tsx
+++ frontend/src/components/TripSetter.tsx
@@ -41,81 +41,50 @@ export default function TripSetter({
     [boutId, jamId, team, teamScore, selectedTrip]
   );
 
-  const buttonRow: ReactNode =
-    teamScore.trips.length == 0 && useInitial ? (
-      <div className="flex flex-row justify-center flex-1 max-w-lg pb-4 space-x-12">
-        <button
-          onClick={() => addTripCallback(0)}
-          className="flex-none bg-slate-200"
-        >
-          NP/NP
-        </button>
-        <button
-          onClick={() => addTripCallback(0)}
-          className="flex-none bg-slate-200"
-        >
-          Initial
-        </button>
-      </div>
-    ) : (
-      <div className="flex flex-row justify-between flex-1 max-w-lg px-8 pb-4 space-x-8">
-        <button
-          onClick={() => addTripCallback(0)}
-          className="flex-none bg-slate-200"
-        >
-          0
-        </button>
-        <button
-          onClick={() => addTripCallback(1)}
-          className="flex-none bg-slate-200"
-        >
-          1
-        </button>
-        <button
-          onClick={() => addTripCallback(2)}
-          className="flex-none bg-slate-200"
-        >
-          2
-        </button>
-        <button
-          onClick={() => addTripCallback(3)}
-          className="flex-none bg-slate-200"
-        >
-          3
-        </button>
-        <button
-          onClick={() => addTripCallback(4)}
-          className="flex-none bg-slate-200"
-        >
-          4
-        </button>
-      </div>
-    );
-
-  const tripCarousel: ReactNode[] = [];
-  for (const i in teamScore.trips) {
-    tripCarousel.push(
-      <TripSquare
-        tripIndex={i}
-        points={teamScore.trips[i].points}
-        tripState={[selectedTrip, setSelectedTrip]}
-      />
+  // Create the buttons which allow the user to add Trips
+  const pointButtons: ReactNode[] = [];
+  const buttonEntries =
+    teamScore.trips.length == 0 && useInitial
+      ? { "NP/NP": 0, Initial: 0 }
+      : { "0": 0, "1": 1, "2": 2, "3": 3, "4": 4 };
+  for (const [text, points] of Object.entries(buttonEntries)) {
+    pointButtons.push(
+      <button
+        onClick={() => addTripCallback(points)}
+        className="flex-none bg-slate-200"
+      >
+        {text}
+      </button>
     );
   }
-  tripCarousel.push(
-    <TripSquare
-      tripIndex={tripCarousel.length}
-      tripState={[selectedTrip, setSelectedTrip]}
-    />
-  );
+
+  // Create the buttons which represents the Jammer Trips so far
+  const tripButtons: ReactNode[] = [];
+  for (let i = 0; i <= teamScore.trips.length; ++i) {
+    const points =
+      i < teamScore.trips.length ? teamScore.trips[i].points : <>&nbsp;</>;
+    tripButtons.push(
+      <button
+        aria-selected={i == selectedTrip}
+        onClick={() => setSelectedTrip(i)}
+        className="aria-selected:bg-red-400 hover:bg-red-700 even: bg-slate-400 odd:bg-inherit text-center flex-none w-[50px] h-[50px] justify-center align-middle"
+      >
+        <i>Trip {i + 1}</i>
+        <br />
+        {points}
+      </button>
+    );
+  }
 
   return (
     <span className="flex flex-col justify-center max-w-[350px] flex-1 p-4 bg-green-400">
-      {buttonRow}
-      <div className="flex flex-row flex-auto overflow-hidden rounded-2xl">
+      <div className="flex flex-row justify-center flex-1 max-w-lg px-4 pb-4 space-x-8 align-middle place-content-between">
+        {pointButtons}
+      </div>
+      <div className="flex flex-row flex-auto overflow-hidden rounded-2xl bg-white">
         <button
           onClick={() => scrollCarousel(-1)}
-          className="h-full text-center bg-white w-7"
+          className="h-full text-center w-7"
         >
           &lt;
         </button>
@@ -123,41 +92,15 @@ export default function TripSetter({
           ref={carousel}
           className="scroll-smooth flex flex-auto flex-row min-w-[100px] w-[150px] max-w-full overflow-x-scroll no-scrollbar bg-slate-100"
         >
-          {tripCarousel}
+          {tripButtons}
         </div>
         <button
           onClick={() => scrollCarousel(1)}
-          className="h-full text-center align-middle bg-white w-7"
+          className="h-full text-center align-middle w-7"
         >
           &gt;
         </button>
       </div>
     </span>
   );
-}
-
-function TripSquare({
-  tripIndex,
-  points = null,
-  tripState,
-}: {
-  tripIndex: number | string;
-  points?: number | null;
-  tripState: [number, (trip: number) => void];
-}) {
-  const [selectedTrip, setSelectedTrip] = tripState;
-  tripIndex = Number(tripIndex);
-
-  const button: ReactNode = (
-    <button
-      className="aria-selected:bg-red-400 hover:bg-red-700 even: bg-slate-400 odd:bg-inherit text-center flex-none w-[50px] h-[50px] justify-center align-middle"
-      aria-selected={tripIndex == selectedTrip}
-      onClick={() => setSelectedTrip(tripIndex)}
-    >
-      <i>Trip {tripIndex + 1}</i>
-      <br />
-      {points == null ? <>&nbsp;</> : points}
-    </button>
-  );
-  return button;
 }
