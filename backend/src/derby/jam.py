172d575480703cad9924386ac84543cea4f567ae
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-15 19:55:35 -0800

    add team attributes

--- backend/src/derby/jam.py
+++ backend/src/derby/jam.py
@@ -1,18 +1,85 @@
-from typing import Any, Hashable
-from uuid import UUID
+from dataclasses import dataclass
+from datetime import datetime
 from server import Queryable
+from typing import Any, Hashable, Sequence
+from uuid import UUID
+
+
+class TeamAttribute[T: Queryable]:
+    __slots__ = '_home', '_away'
+    
+    def __init__(self, home: T, away: T) -> None:
+        for team, name in zip((home, away), ('home', 'away')):
+            if isinstance(team._id, tuple):
+                team._id += (name, )
+            else:
+                team._id = (team._id, name)
+        self._home: T = home
+        self._away: T = away
+    
+    @property
+    def home(self) -> T:
+        return self._home
+    
+    @property
+    def away(self) -> T:
+        return self._away
+
+
+class Score(Queryable):
+    @dataclass(slots=True)
+    class Trip():
+        timestamp: datetime
+        points: int
+
+    def __init__(self, bout_id: UUID, id: tuple) -> None:
+        super().__init__((bout_id, id))
+        self._lead: bool = False
+        self._lost: bool = False
+        self._star_pass: int | None = None
+        self._trips: list[Score.Trip] = []
+    
+    def total_points(self) -> int:
+        return sum(trip.points for trip in self._trips)
+    
+    def get(self) -> dict[str | float | int, Any]:
+        return {
+            'lead': self._lead,
+            'lost': self._lost,
+            'starPass': self._star_pass,
+            'trips': [{
+                'timestamp': str(trip.timestamp),
+                'points': trip.points
+            } for trip in self._trips],
+        }
+
 
 
 class Jam(Queryable):
-    def __init__(self, bout_id: UUID, key: Hashable) -> None:
+    def __init__(self, bout_id: UUID, id: tuple[int, int]) -> None:
+        super().__init__((bout_id, id))
         self._bout_id: UUID = bout_id
-        super().__init__((bout_id, key))
+        self._start_timestamp: datetime | None = None
+        self._stop_timestamp: datetime | None = None
+        self._stop_reason: int | None = None
+        
+        # Initialize the Scores
+        starting_scores = (Score(bout_id, id), Score(bout_id, id))
+        self._score: TeamAttribute = TeamAttribute(*starting_scores)
 
     @property
     def bout_id(self) -> UUID:
         return self._bout_id
+    
+    @property
+    def score(self) -> TeamAttribute[Score]:
+        return self._score
 
     def get(self) -> dict[str | float | int, Any]:
         return {
-            # TODO
+            'start': (str(self._start_timestamp) if self._start_timestamp
+                      else None),
+            'stop': (str(self._stop_timestamp) if self._stop_timestamp
+                     else None),
+            'stop_reason': self._stop_reason,
         }
