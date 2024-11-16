6fc349b30b07793c98d9c666bbc6560d6aea2f03
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-15 23:25:21 -0800

    print bout ID

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import { BoutContext } from "../App";
+
 export default function TestPage() {
+  const boutId: string = useContext(BoutContext);
+
   return (
     <>
+      {boutId} <br />
       First <br /> hello <br /> hello <br /> hello <br />
       Hello <br /> hello <br /> hello <br /> hello <br />
       Hello <br /> hello <br /> hello <br /> hello <br />
