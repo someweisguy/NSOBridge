95834fc502b0470c7e9c536e75077cff7848529d
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-15 23:12:04 -0800

    properly get model seed

--- frontend/src/hooks/client.ts
+++ frontend/src/hooks/client.ts
@@ -53,7 +53,7 @@ socket.onmessage = (event: MessageEvent<string>) => {
 const rootNode: HTMLElement | null = document.getElementById('root');
 if (rootNode?.dataset?.model) {
   try {
-    client.setQueryData(['series'],
+    client.setQueryData(['series', {}],
       JSON.parse(rootNode.dataset.model));
   } catch {
     console.error("Could not parse data model seed.")
