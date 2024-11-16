365d13131e98828c16c01bd67aeb209fe76c6c30
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-15 23:31:30 -0800

    add getter

---
+++ backend/src/api/jam.py
@@ -0,0 +1,9 @@
+from derby.bout import Bout
+from derby.series import Series
+from server import controller
+from typing import Any
+
+
+def get(boutId: str, jamId: tuple[int, int]) -> dict[str | float | int, Any]:
+    series: Series = controller.data
+    return series.get_bout(boutId).get_jam(jamId).get()
