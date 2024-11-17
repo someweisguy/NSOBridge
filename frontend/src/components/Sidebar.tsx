d19b60a76abfb36a5b5ca1ca266b22878a3e7fcd
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-16 16:35:52 -0800

    adjust sidebar size

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -12,7 +12,7 @@ export default function Sidebar({
 }: SidebarProps): ReactNode {
   return (
     <div className="flex flex-row w-full h-full max-h-full min-h-full">
-      <div className="sticky left-0 flex flex-row flex-none h-full max-h-full min-h-full w-72 bg-slate-200">
+      <div className="sticky left-0 flex flex-row flex-none w-3/12 h-full max-h-full min-h-full min-w-20 bg-slate-200">
         <nav className="fixed flex-col p-4">
           <p>Link 1</p>
           <p>Link 2</p>
