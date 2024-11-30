9dde98f8726b463c1609eeae90e3a448ce198b89
Mitch Weisbrod <mitch@theweisbrods.com>
2024-11-29 22:21:01 -0800

    make more responsive

--- frontend/src/features/score/components/TripCard.tsx
+++ frontend/src/features/score/components/TripCard.tsx
@@ -17,7 +17,7 @@ export default function TripCard({
     <div
       aria-selected={selected}
       id={id}
-      className="transition duration-200 aria-selected:scale-105 aria-selected:shadow-lg hover:scale-105 hover:shadow-lg aspect-[7/8] flex-none h-full place-content-center rounded-sm bg-gray-200 text-center outline outline-1 outline-gray-300 shadow-md"
+      className="transition duration-200 aria-selected:scale-105 aria-selected:shadow-lg hover:scale-105 hover:shadow-lg aspect-[7/8] flex-none h-full place-content-center rounded-sm bg-gray-200 text-center outline outline-1 hover:duration-0 outline-gray-300 shadow-md"
     >
       <button onClick={onClick} className="size-full">
         <p className="text-sm italic font-light text-gray-800">
