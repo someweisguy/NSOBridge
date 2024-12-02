d54df96e43a0bc2a21bca976115e7cc33c96a376
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-01 20:37:38 -0800

    remove old api

--- backend/src/roller_derby/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-from .series import Series
-from .bout import Bout, Timer
-from .jam import Jam
-from uuid import UUID as BoutId
-
-bouts: Series = Series()
--- backend/src/server/api/info.py
+++
@@ -1,8 +0,0 @@
-def get() -> dict[str, str]:
-    return {
-        'version': '0.1.0'
-    }
-
-
-def log(message: str) -> None:
-    print(message)
