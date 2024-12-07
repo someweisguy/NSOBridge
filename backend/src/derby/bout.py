2856c49332481048a722b89e0f89a9333a273b77
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-07 10:33:55 -0800

    check timeout type

--- backend/src/derby/bout.py
+++ backend/src/derby/bout.py
@@ -73,8 +73,8 @@ class Bout(Queryable[UUID]):
         elif self._lineup_clock.is_running():
             return 'lineup'
         elif self._timeout_clock.is_running():
-            # TODO: check timeout type?
-            return 'timeout'
+            return ('officialReview' if self._timeouts[-1].is_official_review
+                    else 'timeout')
         else:
             return 'stopped'
 
