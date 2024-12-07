651bbbf9dc6f28e187f0fcbeb011a6e23b2615f9
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-07 10:41:31 -0800

    add timeout api

--- backend/src/api/bout.py
+++ backend/src/api/bout.py
@@ -22,13 +22,38 @@ def startJam(boutId: str, latency: int | timedelta) -> None:
     bout: Bout = series.get_bout(boutId)
     bout.start_jam(now)
 
+
 @controller.action
 def stopJam(boutId: str, latency: int | timedelta) -> None:
     now: datetime = datetime.now()
     if isinstance(latency, int):
         latency = timedelta(milliseconds=latency)
     now -= latency
-    
+
     series: Series = controller.model
     bout: Bout = series.get_bout(boutId)
     bout.stop_jam(now)
+
+
+@controller.action
+def callTimeout(boutId: str, latency: int | timedelta) -> None:
+    now: datetime = datetime.now()
+    if isinstance(latency, int):
+        latency = timedelta(milliseconds=latency)
+    now -= latency
+
+    series: Series = controller.model
+    bout: Bout = series.get_bout(boutId)
+    bout.call_timeout(now)
+
+
+@controller.action
+def endTimeout(boutId: str, latency: int | timedelta) -> None:
+    now: datetime = datetime.now()
+    if isinstance(latency, int):
+        latency = timedelta(milliseconds=latency)
+    now -= latency
+
+    series: Series = controller.model
+    bout: Bout = series.get_bout(boutId)
+    bout.end_timeout(now)
