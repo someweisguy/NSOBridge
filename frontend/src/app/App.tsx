b763fe03d54f44d48ecd71c1a7bc5dd5d210c105
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-12 21:03:17 -0800

    remove head banner

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,10 @@
 import Sidebar from "../components/Sidebar.tsx";
-import MainContainer from "../components/MainContainer.tsx";
 import ScoreboardOperator from "./pages/ScoreboardOperator.tsx";
 
 export default function App() {
   return (
-    <MainContainer>
       <Sidebar>
         <ScoreboardOperator />
       </Sidebar>
-    </MainContainer>
   );
 }
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
-import { ReactNode, Suspense } from "react";
+import { ReactNode, Suspense, useEffect, useState } from "react";
 import LoadingSpinner from "./LoadingSpinner";
+import useSeries from "../hooks/useSeries";
+import { BoutIdContext } from "../contexts/BoutIdContext";
 
 type SidebarProps = {
   children?: ReactNode;
@@ -10,19 +12,42 @@ export default function Sidebar({
   children,
   spinner = <LoadingSpinner />,
 }: SidebarProps): ReactNode {
-  return (
-    <div className="flex flex-row w-full h-full max-h-full min-h-full">
-      <div className="sticky left-0 flex flex-row flex-none w-3/12 h-full max-h-full min-h-full min-w-20 bg-slate-200">
-        <nav className="fixed flex-col p-4">
-          <p>Link 1</p>
-          <p>Link 2</p>
-          <p>Link 3</p>
-          <p>Link 4</p>
-        </nav>
-      </div>
+  const series: Map<string, object> = useSeries();
+  const [boutId, setBoutId] = useState<string>(
+    series.size > 0 ? series.keys().next().value! : ""
+  );
 
-      <div className="p-4 shadow-inner size-full">
-        <Suspense fallback={spinner}>{children}</Suspense>
+  // Automatically select a Bout with which to interact
+  useEffect(() => {
+    if (boutId && series.has(boutId)) {
+      return; // Do nothing
+    } else if (series.size > 0) {
+      if (boutId) {
+        // TODO: notify client that the Bout has been deleted
+      }
+      setBoutId(series.keys().next().value!);
+    } else {
+      // TODO: Go to Bout creation page
+    }
+  }, [boutId, series]);
+
+  return (
+    <div className="flex flex-row min-h-screen min-w-screen bg-raisin-50">
+      <div className="relative flex-initial min-h-full border-0 min-w-32 bg-raisin-100 border-e-1 border-raisin-300">
+        <div className="sticky top-0 left-0 flex flex-col items-start gap-2 p-4">
+          <div className="text-2xl font-bold">NSO Bridge</div>
+          <div>Sidebar 2</div>
+          <div>Sidebar 3</div>
+          <div>Sidebar 4</div>
+          <div>Sidebar 5</div>
+        </div>
+      </div>
+      <div className="flex-1 p-4 h-fit">
+        <Suspense fallback={spinner}>
+          <BoutIdContext.Provider value={boutId}>
+            {children}
+          </BoutIdContext.Provider>
+        </Suspense>
       </div>
     </div>
   );
