026655132c1d796b78324095ebb6fc127c896ba9
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-15 21:14:08 -0800

    simplify component

--- frontend/src/features/JamController/components/JamController.tsx
+++ frontend/src/features/JamController/components/JamController.tsx
@@ -1,60 +1,30 @@
 import { PropsWithChildren, ReactElement, useContext } from "react";
-import TraverseJamButton from "./TraverseJamButton";
-import CurrentJam from "./CurrentJam";
-import useJamNavigation from "../hooks/useJamNavigation";
 import { BoutIdContext } from "../../../contexts/BoutIdContext";
 import { BoutIdType } from "../../../types/BoutIdType";
-import StartJam from "./StartJam";
-import StopJam from "./StopJam";
-import { JamType } from "../../../types/JamType";
-import useJam from "../../../hooks/useJam";
-import { JamIdContext } from "../../../contexts/JamIdContext";
+import useJamIterator from "../hooks/useJamIterator";
+import Button from "../../../components/Button";
 
 export default function JamController({
   children,
 }: PropsWithChildren): ReactElement {
   const boutId: BoutIdType = useContext(BoutIdContext);
 
-  const {
-    currentJamId,
-    nextJamExists,
-    goToNextJam,
-    previousJamExists,
-    goToPreviousJam,
-    // goToCustomJam,  // TODO
-  } = useJamNavigation(boutId);
-
-  const jam: JamType = useJam(boutId, currentJamId);
-  const jamHasStarted: boolean = jam.start != null;
-  const jamHasFinished: boolean = jam.stop != null;
-
-  const [periodNum, jamNum] = currentJamId;
+  const [jamId, setJamId, nextJamId, previousJamId] = useJamIterator(boutId);
+  const [periodNum, jamNum] = jamId;
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row items-center">
-        <div className="basis-1/2">
-          {jamHasStarted ? jamHasFinished ? <></> : <StopJam /> : <StartJam />}
-        </div>
-
-        <div className="basis-1/2 place-items-end">
-          <div className="flex items-center p-4">
-            <TraverseJamButton
-              disabled={!previousJamExists}
-              onClick={goToPreviousJam}
-            >
-              &#10094;
-            </TraverseJamButton>
-            <CurrentJam periodNum={periodNum} jamNum={jamNum} />
-            <TraverseJamButton disabled={!nextJamExists} onClick={goToNextJam}>
-              Go to Next Jam &#10095;
-            </TraverseJamButton>
-          </div>
-        </div>
-      </div>
-      <JamIdContext.Provider value={currentJamId}>
-        {children}
-      </JamIdContext.Provider>
+    <div>
+      <Button
+        disabled={previousJamId == null}
+        onClick={() => setJamId(previousJamId!)}
+      >
+        &#10094;
+      </Button>
+      <Button color="blue">{`P${periodNum + 1} J${jamNum + 1}`}</Button>
+      <Button disabled={nextJamId == null} onClick={() => setJamId(nextJamId!)}>
+        Go to Next Jam &#10095;
+      </Button>
+      {children}
     </div>
   );
 }
