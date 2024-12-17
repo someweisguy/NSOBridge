de3d063723bc038ff11a85ccce41f23c75d7d450
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-16 20:37:44 -0800

    change context location

--- frontend/src/app/pages/ScoreboardOperator.tsx
+++ frontend/src/app/pages/ScoreboardOperator.tsx
@@ -1,25 +1,25 @@
 import ScoreKeeper from "../../features/ScoreKeeper/components/ScoreKeeper";
-import JamController from "../../features/JamController/components/JamController";
-import Clock from "../../components/Clock";
+import JamPaginator from "../../features/JamPaginator/components/JamPaginator";
+import Container from "../../components/Container";
 
 export default function ScoreboardOperator() {
-
-
   return (
     <>
-      <div className="flex flex-col items-center">
-        <JamController>
-          <Clock type="lineup" />
-          <div className="flex flex-row gap-4">
-            <div className="flex flex-col items-center">
-              <ScoreKeeper team="home" />
-            </div>
-            <div className="flex flex-col items-center">
-              <ScoreKeeper team="away" />
-            </div>
-          </div>
-        </JamController>
-      </div>
+      <JamPaginator>
+        {/* <Clock type="lineup" />
+          &nbsp;
+          <Clock type="period" />
+          &nbsp;
+          <Clock type="timeout" /> */}
+        <div className="flex flex-row gap-4">
+          <Container>
+            <ScoreKeeper team="home" />
+          </Container>
+          <Container>
+            <ScoreKeeper team="away" />
+          </Container>
+        </div>
+      </JamPaginator>
     </>
   );
 }
--- frontend/src/contexts/JamIdContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { JamIdType } from "../types/JamIdType";
-
-
-export const JamIdContext = createContext<JamIdType>([0, 0]);
