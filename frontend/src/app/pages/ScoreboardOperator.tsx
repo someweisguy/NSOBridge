50db34e9e5e99b8c9af7966b808c2f75e0cdc455
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-17 21:05:20 -0800

    add timeout controller

--- frontend/src/app/pages/ScoreboardOperator.tsx
+++ frontend/src/app/pages/ScoreboardOperator.tsx
@@ -2,16 +2,18 @@ import ScoreKeeper from "../../features/ScoreKeeper/components/ScoreKeeper";
 import JamPaginator from "../../features/JamPaginator/components/JamPaginator";
 import Container from "../../components/Container";
 import JamController from "../../features/JamController/components/JamController";
+import TimeoutController from "../../features/TimeoutController/components/TimeoutController";
+import Clock from "../../components/Clock";
 
 export default function ScoreboardOperator() {
   return (
     <>
-      <JamPaginator left={<JamController />}>
-        {/* <Clock type="lineup" />
+      <JamPaginator left={<JamController />} right={<TimeoutController />}>
+        <Clock type="lineup" />
           &nbsp;
           <Clock type="period" />
           &nbsp;
-          <Clock type="timeout" /> */}
+          <Clock type="timeout" />
         <div className="flex flex-row gap-4">
           <Container>
             <ScoreKeeper team="home" />
