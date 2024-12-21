ce4482770eccced97ec05c83f15187c12c9d2429
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-21 13:01:41 -0800

    feat: enhance intermission readiness logic by incorporating last period clock snapshot

--- frontend/src/features/IntermissionHandler/components/IntermissionHandler.tsx
+++ frontend/src/features/IntermissionHandler/components/IntermissionHandler.tsx
@@ -15,44 +15,59 @@ import Clock from "../../DynamicClock/components/Clock";
 import { GameStates } from "../../../types/GameStates";
 import useClock from "../../../hooks/useClock";
 
-
 export default function IntermissionHandler(): ReactElement {
   const boutId: BoutIdType = useContext(BoutIdContext);
 
-  // Get the Period and Intermission Clocks
+  // Get the required data to determine if the Intermission is ready to start
   const { latency } = useSocketState();
   const periodClockSnapshot = useClock(boutId, "period");
+  const [lastPeriodClockSnapshot, setLastPeriodClockSnapshot] = useState(
+    window.performance.now()
+  );
   const intermissionClockSnapshot = useClock(boutId, "intermission");
-
   const gameState = useBout<GameStates>(boutId, (bout) => bout.gameState);
+
+  // Determine if the Intermission is ready to start
   const [isReady, setIsReady] = useState<boolean>(
     ["pregame", "halftime"].includes(gameState) ||
       periodClockSnapshot.alarm! - periodClockSnapshot.elapsed < 5000
   );
 
-  const startIntermission = useCallback(() => {
-    const advanceGameState = !["pregame", "final"].includes(gameState);
-    dispatch("bout", "startIntermission", { boutId, latency, advanceGameState });
-  }, [boutId, latency, gameState]);
-
-  const stopIntermission = useCallback(() => {
-    dispatch("bout", "stopIntermission", { boutId, latency });
-  }, [boutId, latency]);
-
+  // Update the lastPeriodClockSnapshot when the periodClockSnapshot changes
   useEffect(() => {
-    const remaining = periodClockSnapshot.alarm! - periodClockSnapshot.elapsed;
-    if (isReady) {
-      return;
-    } else if (remaining < 5000) {
+    setLastPeriodClockSnapshot(window.performance.now());
+  }, [periodClockSnapshot]);
+
+  // Allow intermission to start when the period is almost over
+  useEffect(() => {
+    const remaining =
+      periodClockSnapshot.alarm! -
+      periodClockSnapshot.elapsed -
+      (window.performance.now() - lastPeriodClockSnapshot);
+    if (remaining < 5000 || ["pregame", "halftime"].includes(gameState)) {
       setIsReady(true);
       return;
     }
+    setIsReady(false);
 
     const timeoutId = setTimeout(() => {
       setIsReady(true);
     }, remaining - 5000);
     return () => clearTimeout(timeoutId);
-  }, [periodClockSnapshot, isReady]);
+  }, [periodClockSnapshot, gameState, lastPeriodClockSnapshot]);
+
+  const startIntermission = useCallback(() => {
+    const advanceGameState = !["pregame", "final"].includes(gameState);
+    dispatch("bout", "startIntermission", {
+      boutId,
+      latency,
+      advanceGameState,
+    });
+  }, [boutId, latency, gameState]);
+
+  const stopIntermission = useCallback(() => {
+    dispatch("bout", "stopIntermission", { boutId, latency });
+  }, [boutId, latency]);
 
   if (!intermissionClockSnapshot.isRunning) {
     return (
