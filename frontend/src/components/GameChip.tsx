7a1243b3d9407f9088642816997e19be59ab7bae
Mitch Weisbrod <mitch@theweisbrods.com>
2024-12-29 19:55:51 -0800

    feat: add GameChip component to display bout information with dynamic clock

---
+++ frontend/src/components/GameChip.tsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { BoutIdContext } from "../contexts/BoutIdContext";
+import { BoutIdType } from "../types/BoutIdType";
+import useBout from "../hooks/useBout";
+import Clock from "../features/DynamicClock/components/Clock";
+
+export default function GameChip({ boutId }: { boutId?: BoutIdType }) {
+  const boutIdContext = useContext<BoutIdType>(BoutIdContext);
+  if (boutId === undefined) {
+    boutId = boutIdContext;
+  }
+  if (!boutId) {
+    throw Error("No BoutId provided");
+  }
+
+  const periodNum = useBout<number>(boutId, (bout) =>
+    Number(bout.numJams[1] > 0)
+  );
+  const [jamNum, playState] = useBout<[number, string]>(boutId, (bout) => [
+    bout.numJams[periodNum] - 1,
+    bout.playState,
+  ]);
+
+  return (
+    <div className="flex flex-col bg-raisin-100 m-3 rounded-lg max-w-min text-lg overflow-hidden">
+      <div className="flex flex-row place-content-between bg-pink p-1 rounded-b-lg font-mono">
+        <div className="text-right flex-1 px-2 w-16 min-w-fit">
+          <Clock type="period" />
+        </div>
+        <div className="flex-none px-1 border-raisin-300 border-l w-fit">
+          P{periodNum + 1}
+        </div>
+
+        <div className="text-right flex-none px-1 border-r border-raisin-300">
+          {playState === "lineup" ? "L" : "J"}
+          {jamNum + 1}
+        </div>
+        <div className="text-right flex-1 px-1 w-16">
+          <Clock type={playState === "stopped" ? "jam" : playState} />
+        </div>
+      </div>
+      <div className="bg-blue-200 w-full font-semibold text-center text-sm">
+        Timeout
+      </div>
+    </div>
+  );
+}
