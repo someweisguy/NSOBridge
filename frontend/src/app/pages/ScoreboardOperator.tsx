588a4e71695e7548f96157916022580835fcaefe
Mitch Weisbrod <mitch@theweisbrods.com>
2025-01-02 12:24:31 -0800

    fix: remove unused components from ScoreboardOperator for cleaner code

--- frontend/src/app/pages/ScoreboardOperator.tsx
+++ frontend/src/app/pages/ScoreboardOperator.tsx
@@ -1,13 +1,9 @@
 import ScoreKeeper from "../../features/ScoreKeeper/components/ScoreKeeper";
 import JamPaginator from "../../features/JamPaginator/components/JamPaginator";
 import Container from "../../components/Container";
-import JamController from "../../features/JamController/components/JamController";
-import TimeoutController from "../../features/TimeoutController/components/TimeoutController";
 import JamEndReason from "../../features/JamEndReason/components/JamEndReason";
-import IntermissionHandler from "../../features/IntermissionHandler/components/IntermissionHandler";
 import GameChip from "../../components/GameChip";
 import GameController from "../../features/GameController/components/GameController";
-
 
 export default function ScoreboardOperator() {
   return (
@@ -15,15 +11,6 @@ export default function ScoreboardOperator() {
       <div className="flex flex-row items-center">
         <GameChip />
         <GameController />
-      </div>
-      <IntermissionHandler />
-      <div className="flex w-full spaced-between">
-        <div className="flex-1">
-          <JamController />
-        </div>
-        <div className="flex-1">
-          <TimeoutController />
-        </div>
       </div>
       <JamPaginator left={<JamEndReason />}>
         <div className="flex flex-row gap-4">
