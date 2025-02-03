9b8028f98eb1bd96f4fcc61964b87aa1a68f9291
Mitch Weisbrod <mitch@theweisbrods.com>
2025-02-02 20:15:09 -0800

    feat: comment out unused TripCarousel and useTripIndex in ScoreKeeper component

--- frontend/src/features/ScoreKeeper/components/ScoreKeeper.tsx
+++ frontend/src/features/ScoreKeeper/components/ScoreKeeper.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useContext } from "react";
-import TripCarousel from "./TripCarousel";
-import useTripIndex from "../hooks/useTripIndex";
+// import TripCarousel from "./TripCarousel";
+// import useTripIndex from "../hooks/useTripIndex";
 // import useInitialPass from "../hooks/useInitialPass";
 import { BoutIdContext } from "../../../contexts/BoutIdContext";
 import { JamIdContext } from "../../JamPaginator/components/JamPaginator";
@@ -20,7 +20,7 @@ export default function ScoreKeeper({
   const boutId: BoutIdType = useContext(BoutIdContext);
   const jamId: JamIdType = useContext(JamIdContext);
 
-  const [selectedTrip, setSelectedTrip] = useTripIndex(boutId, jamId, team);
+  // const [selectedTrip, setSelectedTrip] = useTripIndex(boutId, jamId, team);
   // const showInitial = useInitialPass(boutId, jamId, selectedTrip);
 
   // Get the Jammer state variables
@@ -36,12 +36,6 @@ export default function ScoreKeeper({
   return (
     <div className="flex flex-col items-center bg-gray-100 rounded-lg">
       <TripEditor team={team} />
-      <TripCarousel
-        boutId={boutId}
-        jamId={jamId}
-        team={team}
-        tripState={[selectedTrip, setSelectedTrip]}
-      />
       <div className="flex flex-row gap-2">
         <Checkbox
           disabled={!isLeadEligible}
