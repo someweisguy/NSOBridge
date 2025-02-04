873cef87281fdbea7cdda3f4935da7cdaab8d6ca
Mitch Weisbrod <mitch@theweisbrods.com>
2025-02-03 19:35:13 -0800

    feat: enhance TripEditor to set points and adjust trip display

--- frontend/src/features/sbo/trip-editor.tsx
+++ frontend/src/features/sbo/trip-editor.tsx
@@ -8,10 +8,11 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { BoutIdContext } from "@/contexts/BoutIdContext";
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { JamIdContext } from "../JamPaginator/components/JamPaginator";
 import useScore from "@/hooks/useScore";
 import { Button } from "@/components/ui/button";
+import setTrip from "../ScoreKeeper/api/setTrip";
 
 export default function TripEditor({ team }: { team: "home" | "away" }) {
   const boutId = useContext(BoutIdContext);
@@ -21,21 +22,34 @@ export default function TripEditor({ team }: { team: "home" | "away" }) {
   const [api, setApi] = useState<CarouselApi>();
 
   useEffect(() => {
-    api?.scrollTo(10, true);
+    api?.scrollTo(jamScore.trips.length, true);
   }, [api]);
+
+  const setPoints = useCallback(
+    (points: number, validPass: boolean = true) =>
+      setTrip(boutId, jamId, team, jamScore.trips.length, points, validPass),
+    [boutId, jamId, team, jamScore.trips.length]
+  );
 
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-row gap-2 m-2">
         {Array.from({ length: 4 + 1 }, (_, i) => (
-          <Button variant={i === 4 ? "default" : "secondary"}>{i}</Button>
+          <Button
+            variant={i === 4 ? "default" : "secondary"}
+            onClick={() => setPoints(i, true)}
+          >
+            {i}
+          </Button>
         ))}
       </div>
       <Carousel setApi={setApi}>
         <Card>
           <CarouselContent className="-ml-4">
             {Array.from({ length: jamScore.trips.length + 1 }, (_, i) => (
-              <CarouselItem className="pl-4 basis-1/5">Trip {i}</CarouselItem>
+              <CarouselItem className="pl-4 basis-1/5">
+                Trip {i + 1}
+              </CarouselItem>
             ))}
           </CarouselContent>
         </Card>
