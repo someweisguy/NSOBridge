4cf2b526fdf56a00443701633382a16140b0632b
Mitch Weisbrod <mitch@theweisbrods.com>
2025-02-07 23:32:10 -0800

    workaround for git case insensitivity

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,7 +2,7 @@ import { queryClient } from "../lib/client.ts";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import App from "./app.tsx";
+import App from "./pages/app.tsx";
 import "./index.css";
 import LoadingSpinner from "@/components/loading-spinner.tsx";
 
