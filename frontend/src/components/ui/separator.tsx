d04b77abe2b7aaccc59de20be1b69c9f463688d8
Mitch Weisbrod <mitch@theweisbrods.com>
2025-03-03 20:57:38 -0800

    refactor: update Separator component styles for improved responsiveness and consistency

--- frontend/src/components/ui/separator.tsx
+++ frontend/src/components/ui/separator.tsx
@@ -17,7 +17,7 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-neutral-200 shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px dark:bg-neutral-800",
+        "bg-neutral-200 shrink-0 data-[orientation=horizontal]:h-[1px] data-[orientation=horizontal]:min-w-full data-[orientation=vertical]:min-h-full data-[orientation=vertical]:w-[1px] dark:bg-neutral-800",
         className
       )}
       {...props}
