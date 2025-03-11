8c60c5f1c7137fdea8f8c25621cde679e32abc8a
Mitch Weisbrod <mitch@theweisbrods.com>
2025-03-10 21:37:57 -0700

    feat: add properties for timeouts, official reviews, and history in Stops model

--- backend/src/model/stops.py
+++ backend/src/model/stops.py
@@ -7,3 +7,15 @@ class Stops:
     _timeouts_remaining: TeamAttribute[int] = TeamAttribute(3, 3)
     _official_reviews_remaining: TeamAttribute[int] = TeamAttribute(1, 1)
     _history: list = field(default_factory=list)
+
+    @property
+    def timeouts(self) -> TeamAttribute[int]:
+        return self._timeouts_remaining
+
+    @property
+    def official_reviews(self) -> TeamAttribute[int]:
+        return self._official_reviews_remaining
+
+    @property
+    def history(self) -> list:
+        return self._history
