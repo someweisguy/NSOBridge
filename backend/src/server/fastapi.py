07c9b7a0c552b9cf955323e6eb4e682669a4f678
Mitch Weisbrod <mitch@theweisbrods.com>
2025-04-09 19:51:14 -0700

    refactor: enhance websocket handling and improve code readability in fastapi.py

--- backend/src/server/fastapi.py
+++ backend/src/server/fastapi.py
@@ -3,17 +3,25 @@ import os
 import traceback
 from json import JSONDecodeError
 from pathlib import Path
-from typing import Annotated, Any, Callable, Final
-from datetime import datetime
+from typing import Annotated, Any, Final
 
-from controller import Controller
-from fastapi import (Depends, FastAPI, Request, Response, WebSocket,
-                     WebSocketDisconnect)
+from fastapi import (
+    Depends,
+    FastAPI,
+    Request,
+    Response,
+    WebSocket,
+    WebSocketDisconnect,
+)
 from fastapi.responses import FileResponse
 from fastapi.routing import Mount
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import ValidationError
+
+from controller import Controller
+
+SOCKET_PROTOCOL: Final[str] = 'nso-bridge'
 
 FRONTEND: Final[Path] = Path(os.getcwd()) / 'frontend' / 'dist'
 TEMPLATES: Final[Jinja2Templates] = Jinja2Templates(FRONTEND)
@@ -21,14 +29,15 @@ TEMPLATES: Final[Jinja2Templates] = Jinja2Templates(FRONTEND)
 ControllerDepend = Annotated[Controller, Depends(Controller.get_instance)]
 
 sockets: set[WebSocket] = set()
-app: FastAPI = FastAPI(debug=True, mount=[
-    Mount('/', StaticFiles(directory=FRONTEND, html=True))
-])
+app: FastAPI = FastAPI(
+    debug=True, mount=[Mount('/', StaticFiles(directory=FRONTEND, html=True))]
+)
 
 
 @app.get('/{path}')
-async def render_generic(request: Request, path: str,
-                         controller: ControllerDepend) -> Response:
+async def render_generic(
+    request: Request, path: str, controller: ControllerDepend
+) -> Response:
     if not path.endswith('.html'):
         return FileResponse(FRONTEND / path)
     data: str = json.dumps(controller.model, separators=(',', ':'))
@@ -36,32 +45,29 @@ async def render_generic(request: Request, path: str,
 
 
 @app.get('/')
-async def render_index(request: Request,
-                       controller: ControllerDepend) -> Response:
-    return await render_generic(request, 'index.html', controller)        
+async def render_index(request: Request, controller: ControllerDepend) -> Response:
+    return await render_generic(request, 'index.html', controller)
 
 
-@app.websocket('/ws')
-async def handle_websocket(websocket: WebSocket,
-                           controller: ControllerDepend) -> None:
-    await websocket.accept()
+@app.websocket('/ws/')
+async def handle_websocket(websocket: WebSocket, controller: ControllerDepend) -> None:
+    await websocket.accept(subprotocol=SOCKET_PROTOCOL)
     sockets.add(websocket)
 
-    disconnect_code: int = 1006
-    disconnect_message: str = 'An unexpected error occurred.'
     while True:
         try:
             # Parse the JSON payload
             request: dict[str, Any] = await websocket.receive_json()
 
             # Ensure that the payload has the required keys
-            if not all(key in request.keys() for key in
-                       ('action', 'args', 'clientId', 'transactionId')):
+            if not all(
+                key in request.keys()
+                for key in ('action', 'args', 'clientId', 'transactionId')
+            ):
                 raise ValidationError('Missing payload key.')
-            
+
             # Handle the client request
-            response = controller.handle_client(request['action'],
-                                                request['args'])
+            response = controller.handle_client(request['action'], request['args'])
 
             # Construct and send the response payload
             response: dict[str, Any] = {
@@ -71,23 +77,19 @@ async def handle_websocket(websocket: WebSocket,
                 'result': 'ok',
             }
             await websocket.send_json(response)
-            
+
             # Broadcast updates to all clients
             for update in controller.get_updates():
                 pass  # TODO
             controller.clear_updates()
-            
         except (WebSocketDisconnect, JSONDecodeError, ValidationError) as e:
             match e:
                 case WebSocketDisconnect():
-                    disconnect_code = 1000
-                    disconnect_message = 'WebSocket disconnected.'
+                    pass  # Client already disconnected
                 case JSONDecodeError():
-                    disconnect_code = 1003
-                    disconnect_message = 'Invalid JSON payload.'
+                    await websocket.close(1003, 'Invalid JSON payload.')
                 case ValidationError():
-                    disconnect_code = 1008
-                    disconnect_message = 'Invalid payload schema.'
+                    await websocket.close(1008, 'Invalid payload schema.')
             break
         except Exception as e:
             tb = traceback.extract_tb(e.__traceback__)
@@ -96,11 +98,11 @@ async def handle_websocket(websocket: WebSocket,
                 'clientId': request['clientId'],  # type: ignore
                 'result': 'error',
                 'data': {
-                    'title': type(e).__name__, 'detail': str(e),
+                    'title': type(e).__name__,
+                    'detail': str(e),
                     'filename': os.path.basename(tb[-1].filename),
-                    'lineno': tb[-1].lineno
+                    'lineno': tb[-1].lineno,
                 },
             }
 
     sockets.remove(websocket)
-    await websocket.close(disconnect_code, disconnect_message)
