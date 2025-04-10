01443542141f715ae408b1eef4927aad0d8dafa1
Mitch Weisbrod <mitch@theweisbrods.com>
2025-04-09 20:56:08 -0700

    feat: update get_version function to return a dictionary instead of a string

--- backend/src/controller/__init__.py
+++ backend/src/controller/__init__.py
@@ -2,7 +2,7 @@ from .controller import Controller, register
 
 
 @register('getVersion')
-def get_version(controller: Controller) -> str:
+def get_version(controller: Controller) -> dict:
     return {
         'app': '0.0.1',
         'api': '0.0.1',
