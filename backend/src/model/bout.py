029b025bf079b022ed4ba67430b86c6e0dbc91c0
Mitch Weisbrod <mitch@theweisbrods.com>
2025-04-25 19:24:23 -0700

    refactor: add get_score method to calculate team score in Bout class

--- backend/src/model/bout.py
+++ backend/src/model/bout.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import Final
 
 from .jam import Jam, JamId
+from .protocols import TeamType
 from .timer import Timer
 
 
@@ -17,3 +18,7 @@ class Bout:
             return self.jams[period][jam]
         except KeyError:
             raise KeyError(f'Jam {jam_id} not found') from None
+
+    def get_score(self, team: TeamType) -> int:
+        all_jams: list[Jam] = [jam for period in self.jams for jam in period]
+        return sum(trip.points for jam in all_jams for trip in jam[team].score.trips)
