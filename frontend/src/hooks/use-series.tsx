f1bf471f53baaa3515dc25e4d3798ddfdb82718b
Mitch Weisbrod <mitch@theweisbrods.com>
2025-04-25 19:24:45 -0700

    refactor: replace dispatchRequest with genericRequest in useSeries hook

--- frontend/src/hooks/use-series.tsx
+++ frontend/src/hooks/use-series.tsx
@@ -1,12 +1,12 @@
-import dispatchRequest from "../lib/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { keyFactory } from "../utils/key-factory";
+import genericRequest, { APIResponse } from "../lib/client";
 
 export default function useSeries(): Map<string, object> {
-  const { data } = useSuspenseQuery<Map<string, object>>({
+  const { data } = useSuspenseQuery<APIResponse, unknown, Map<string, object>>({
     queryKey: keyFactory.series(),
-    queryFn: () => dispatchRequest("series", "get"),
-    select: (data: object) => new Map(Object.entries(data)),
+    queryFn: () => genericRequest<Map<string, object>>("/series", "GET"),
+    select: (response: APIResponse) => new Map(Object.entries(response.data)),
   });
 
   return data;
