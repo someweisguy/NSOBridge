a2cf741f47b4ae01f65c42ee2720f0cb11fa7b03
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-02 16:13:48 -0700

    refactor: update import path for queryClient and reorganize suspense component

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,4 +1,4 @@
-import { queryClient } from "@/lib/client.ts";
+import { queryClient } from "@/lib/cache";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -9,24 +9,22 @@ import LoadingSpinner from "@/components/loading-spinner.tsx";
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
-      <Suspense fallback={<LoadingSpinner />}>
-        Hello world!
-      </Suspense>
+      <Suspense fallback={<LoadingSpinner />}>Hello world!</Suspense>
     </StrictMode>
   </QueryClientProvider>
 );
 
-// // Get the Series data from the HTML root
-// const rootNode: HTMLElement | null = document.getElementById("root");
-// if (rootNode?.dataset?.model) {
-//   try {
-//     queryClient.setQueryData(
-//       ["series"], // TODO: use key factory
-//       JSON.parse(rootNode.dataset.model)
-//     );
-//   } catch {
-//     console.error("Could not parse data model seed.");
-//   }
-// } else {
-//   console.error("Data model not found in server response.");
-// }
+// Get the Series data from the HTML root
+const rootNode: HTMLElement | null = document.getElementById("root");
+if (rootNode?.dataset?.model) {
+  try {
+    queryClient.setQueryData(
+      ["series"], // TODO: use key factory
+      JSON.parse(rootNode.dataset.model)
+    );
+  } catch {
+    console.error("Could not parse data model seed.");
+  }
+} else {
+  console.error("Data model not found in server response.");
+}
