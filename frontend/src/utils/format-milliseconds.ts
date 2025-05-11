a763611861dbf4d35961ad278fd99722a26638af
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-11 00:00:50 -0700

    refactor: streamline output string assignment in formatMilliseconds function

--- frontend/src/utils/format-milliseconds.ts
+++ frontend/src/utils/format-milliseconds.ts
@@ -13,23 +13,28 @@ export default function formatMilliseconds(
   let m = String(Math.floor((millis % 3600000) / 60000));
   let s = String(Math.floor((millis / 1000) % 60));
 
-  let output: string = isNegative ? "-" : "";
+  let output: string;
   if (maxDisplayValue === "hours" || millis >= 3600000) {
     const h = Math.floor(millis / 3600000);
     m = String(m).padStart(2, "0");
     s = String(s).padStart(2, "0");
-    output += `${h}:${m}:${s}`;
+    output = `${h}:${m}:${s}`;
   } else if (maxDisplayValue === "minutes" || millis >= 60000) {
     s = String(s).padStart(2, "0");
-    output += `${m}:${s}`;
+    output = `${m}:${s}`;
   } else {
-    output += s;
+    output = s;
   }
 
   // Render tenths of a seconds
   if (displayMillis) {
     const ds = String(Math.floor((millis % 1000) / 100)).padStart(1, "0");
     output += `.${ds}`;
+  }
+
+  // Render a negative sign if the format string is not zero
+  if (isNegative && ((displayMillis && millis >= 100) || millis >= 1000)) {
+    output = "-" + output;
   }
 
   return output;
