75b581133251af4c7bbb9b5187bf3dafea7bcef9
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-10 23:49:02 -0700

    refactor: optimize output string construction in formatMilliseconds function

--- frontend/src/utils/format-milliseconds.ts
+++ frontend/src/utils/format-milliseconds.ts
@@ -8,21 +8,22 @@ export default function formatMilliseconds(
     maxDisplayValue?: "hours" | "minutes" | "seconds";
   } = {}
 ): string {
+  const isNegative = millis < 0;
   millis = Math.abs(millis);
   let m = String(Math.floor((millis % 3600000) / 60000));
   let s = String(Math.floor((millis / 1000) % 60));
 
-  let output: string;
+  let output: string = isNegative ? "-" : "";
   if (maxDisplayValue === "hours" || millis >= 3600000) {
     const h = Math.floor(millis / 3600000);
     m = String(m).padStart(2, "0");
     s = String(s).padStart(2, "0");
-    output = `${h}:${m}:${s}`;
+    output += `${h}:${m}:${s}`;
   } else if (maxDisplayValue === "minutes" || millis >= 60000) {
     s = String(s).padStart(2, "0");
-    output = `${m}:${s}`;
+    output += `${m}:${s}`;
   } else {
-    output = s;
+    output += s;
   }
 
   // Render tenths of a seconds
