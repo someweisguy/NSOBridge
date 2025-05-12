2a34b5f52c6b8a70d26b853bb86c5567cbce071d
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-11 20:02:05 -0700

    feat: add usePrefetchJam hook for prefetching jam data based on bout and jam IDs

---
+++ frontend/src/hooks/use-prefetch-jam.tsx
@@ -0,0 +1,19 @@
+import { queryClient } from "@/lib/cache";
+import { getJam } from "@/lib/client/api/jam";
+import { keyFactory } from "@/utils/key-factory";
+import { useEffect } from "react";
+
+export default function usePrefetchJam(
+  boutId: string,
+  jamId: [number, number] | null
+) {
+  useEffect(() => {
+    if (jamId !== null) {
+      const [periodNum, jamNum] = jamId;
+      void queryClient.prefetchQuery({
+        queryKey: keyFactory.jam(boutId, periodNum, jamNum),
+        queryFn: () => getJam(boutId, periodNum, jamNum),
+      });
+    }
+  }, [boutId, jamId]);
+}
