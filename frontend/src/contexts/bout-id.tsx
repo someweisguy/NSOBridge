b45a478b0b7802c677445ecd06b8ac8a1ee02e9b
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-17 20:48:39 -0700

    refactor: remove unused UI components and contexts from the project

--- frontend/src/contexts/bout-id.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { BoutIdType } from "../types/bout";
-
-export const BoutIdContext = createContext<BoutIdType>("");
--- frontend/src/contexts/jam-id.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { JamIdType } from "@/types/jam";
-
-export const JamIdContext = createContext<JamIdType>([0, 0]);
