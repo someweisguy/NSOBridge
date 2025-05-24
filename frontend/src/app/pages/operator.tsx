d7e8b42894f7f663f77b8ab8ecba64fb48d34b52
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-24 16:38:34 -0700

    refactor: reorganize imports and refactor ScoreboardOperator to use a map for rendering teams

--- frontend/src/app/pages/operator.tsx
+++ frontend/src/app/pages/operator.tsx
@@ -1,15 +1,17 @@
+import TeamName from "@/components/team-name";
+import TeamScore from "@/components/team-score";
+import TimeoutBar from "@/components/timeout-pips";
+import TripView from "@/components/trip-view";
 import useActiveJamId from "@/hooks/use-active-jam-id";
 import useLatestJamId from "@/hooks/use-latest-jam-id";
 import usePrefetchJam from "@/hooks/use-prefetch-jam";
-import { BoutIdContext } from "../provider";
-import TripView from "@/components/trip-view";
 import { startJam, stopJam } from "@/lib/client/api/bout.ts";
+import { TeamString } from "@/lib/client/api/jam";
 import { getSeries } from "@/lib/client/api/series.ts";
 import { useContext } from "react";
-import TeamScore from "@/components/team-score";
-import TimeoutBar from "@/components/timeout-pips";
-import TeamName from "@/components/team-name";
-// import Clock from "@/components/Clock";
+import { BoutIdContext } from "../provider";
+
+const TEAMS: [TeamString, TeamString] = ["home", "away"];
 
 export function ScoreboardOperator() {
   const [boutId] = useContext(BoutIdContext);
@@ -19,13 +21,22 @@ export function ScoreboardOperator() {
   usePrefetchJam(boutId, lastestJamId);
 
   return (
-    <div className="justify-center content-center grid grid-flow-row p-8">
-      <TeamName team="home" />
-      <div className="grid grid-cols-2">
-        <TimeoutBar team="home" />
-        <TeamScore team="home" />
-      </div>
-      <TripView periodNum={periodNum} jamNum={jamNum} team="home" />
+    <div className="justify-center content-center grid grid-flow-col">
+      {TEAMS.map((team: TeamString) => {
+        return (
+          <div
+            key={team}
+            className="justify-center content-center grid grid-flow-row p-8"
+          >
+            <TeamName team={team} />
+            <div className="grid grid-cols-2">
+              <TimeoutBar team={team} />
+              <TeamScore team={team} />
+            </div>
+            <TripView periodNum={periodNum} jamNum={jamNum} team={team} />
+          </div>
+        );
+      })}
     </div>
   );
 }
