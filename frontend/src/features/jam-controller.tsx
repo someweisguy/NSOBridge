bdde82501b6486aade259fabf6e6372b8a52627f
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-24 20:25:36 -0700

    feat: refactor JamController to manage game state buttons and integrate GameTimer component

--- frontend/src/features/jam-controller.tsx
+++ frontend/src/features/jam-controller.tsx
@@ -1,6 +1,9 @@
 import { BoutIdContext } from "@/app/provider";
+import Button from "@/components/button";
 import GameTimer from "@/components/game-timer";
-import { PropsWithChildren, useContext } from "react";
+import useBout from "@/hooks/use-bout";
+import { selectGameState, startJam, stopJam } from "@/lib/client/api/bout";
+import { PropsWithChildren, useContext, useMemo } from "react";
 
 interface JamControllerProps extends PropsWithChildren {
   boutId?: string;
@@ -13,14 +16,34 @@ export default function JamController({
   const [boutIdContext] = useContext(BoutIdContext);
   boutId ??= boutIdContext;
 
+  const gameState = useBout(boutId, selectGameState);
+  const buttonRow = useMemo(() => {
+    switch (gameState) {
+      case "intermission":
+      case "lineup":
+        return (
+          <>
+            <Button onClick={() => void startJam(boutId)}>Start Jam</Button>
+          </>
+        );
+      case "jam":
+        return (
+          <>
+            <Button onClick={() => void stopJam(boutId)}>End Jam</Button>
+          </>
+        );
+    }
+  }, [gameState, boutId]);
+
   return (
-    <div className="justify-items-center grid grid-flow-row w-2/3">
-      <div className="place-items-start grid grid-flow-col m-2 w-full align-middle">
+    <div className="justify-content-center grid grid-flow-row w-1/2">
+      <div className="justify-start items-center gap-4 grid grid-flow-col m-2 w-full">
         <GameTimer boutId={boutId} />
+        <div className="w-full">
+          {buttonRow}
+        </div>
       </div>
-      <div className="justify-center grid grid-flow-col">
-        {children}
-      </div>
+      <div className="justify-center grid grid-flow-col">{children}</div>
     </div>
   );
 }
