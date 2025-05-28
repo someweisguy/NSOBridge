941d3f9b4add8a7c0e1f795180c5466ab9615333
Mitch Weisbrod <mitch@theweisbrods.com>
2025-05-27 22:30:39 -0700

    refactor: improve timeout state initialization and enhance Switch component integration

--- frontend/src/components/timeout-controls.tsx
+++ frontend/src/components/timeout-controls.tsx
@@ -1,6 +1,7 @@
 import { BoutIdContext } from "@/app/provider";
+import useBout from "@/hooks/use-bout";
 import { editTimeout, TimeoutState } from "@/lib/client/api/bout";
-import { TeamString } from "@/lib/client/api/types";
+import { Bout, TeamString, Timeout } from "@/lib/client/api/types";
 import { Switch, ToggleGroup } from "radix-ui";
 import { useContext, useEffect, useRef, useState } from "react";
 
@@ -12,12 +13,17 @@ export default function TimeoutControls({ boutId }: TimeoutControlsProps) {
   const [boutIdContext] = useContext(BoutIdContext);
   boutId ??= boutIdContext;
 
-  const [timeoutState, setTimeoutState] = useState<TimeoutState>({
-    isReview: false,
-    team: null,
-    details: "",
-    result: "",
-    retained: false,
+  const bout: Bout = useBout(boutId);
+  const [timeoutState, setTimeoutState] = useState<TimeoutState>(() => {
+    const numTimeouts = bout.timer.timeouts.length;
+    const timeout: Timeout | undefined = bout.timer.timeouts[numTimeouts - 1];
+    return {
+      isReview: timeout.isReview ?? false,
+      team: timeout.team ?? null,
+      details: timeout.details ?? "",
+      result: timeout.result ?? "",
+      retained: timeout.retained ?? false,
+    };
   });
   const firstUpdate = useRef(true);
 
@@ -29,18 +35,21 @@ export default function TimeoutControls({ boutId }: TimeoutControlsProps) {
     void editTimeout(boutId, -1, timeoutState);
   }, [timeoutState, boutId]);
 
-  // TODO: type, retained
+  // TODO: make switch and toggle group its own component
   return (
     <>
-      <Switch.Root onCheckedChange={(checked: boolean) => {
-        setTimeoutState((timeout) => {
-          if (checked && timeout.team === "official") {
-            return { ...timeout, isReview: checked, team: null }
-          }
-          return { ...timeout, isReview: checked }
-        })
-      }}>
-        <Switch.Thumb />
+      <Switch.Root
+        defaultChecked={timeoutState.isReview}
+        onCheckedChange={(checked: boolean) => {
+          setTimeoutState((timeout) => {
+            if (checked && timeout.team === "official") {
+              return { ...timeout, isReview: checked, team: null };
+            }
+            return { ...timeout, isReview: checked };
+          });
+        }}
+      >
+        <Switch.Thumb className="block bg-black rounded-full size-[21px] transition-transform translate-x-0.5 data-[state=checked]:translate-x-[19px] duration-100 will-change-transform" />
       </Switch.Root>
 
       <ToggleGroup.Root
@@ -51,16 +60,9 @@ export default function TimeoutControls({ boutId }: TimeoutControlsProps) {
       >
         <ToggleGroup.Item value="home">Home</ToggleGroup.Item>
         <ToggleGroup.Item value="away">Away</ToggleGroup.Item>
-      </ToggleGroup.Root>
-
-      <ToggleGroup.Root
-        type="single"
-        onValueChange={(value: "true" | "false") => {
-          setTimeoutState((timeout) => ({ ...timeout, type: value }));
-        }}
-      >
-        <ToggleGroup.Item value="true">Timeout</ToggleGroup.Item>
-        <ToggleGroup.Item value="false">Review</ToggleGroup.Item>
+        <ToggleGroup.Item disabled={timeoutState.isReview} value="official">
+          Official
+        </ToggleGroup.Item>
       </ToggleGroup.Root>
     </>
   );
