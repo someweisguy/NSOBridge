2fc39310384a096fc373a7d84b6e8d0203662f6e
Mitch Weisbrod <mitch@theweisbrods.com>
2025-06-20 19:18:05 -0700

    refactor: simplify num_jams method to use a list comprehension for better readability

--- backend/src/core/models/bout/bout.py
+++ backend/src/core/models/bout/bout.py
@@ -77,11 +77,8 @@ class Bout(ProjectModel):
 
     @computed_field
     @property
-    def num_jams(self) -> tuple[int, int, int]:
-        num_jams: list[int] = [0, 0, 0]
-        for i, period in enumerate(self._jams):
-            num_jams[i] = len(period)
-        return tuple(num_jams)
+    def num_jams(self) -> tuple[int, ...]:
+        return tuple(len(period) for period in self._jams)
 
     def get_jam(self, period_num: int, jam_num: int) -> Jam:
         return self._jams[period_num][jam_num]
