d649e957d6d175773435a38c9907589a02882235
Mitch Weisbrod <mitch@theweisbrods.com>
2025-07-28 19:37:40 -0700

    add cacheable schema

--- backend/src/schemas/schemas.py
+++ backend/src/schemas/schemas.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 from math import floor
 from typing import Literal
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field, computed_field
 from pydantic.alias_generators import to_camel
 
 type ModelKey = tuple[Literal['bout'], str] | tuple[Literal['jam'], str, int, int]
@@ -18,7 +18,18 @@ class ServerSchema(BaseModel):
         from_attributes=True,
         json_encoders={timedelta: _timedelta_encoder},
         validate_by_name=True,
+        serialize_by_alias=True,
     )
+
+
+class CacheableSchema(ServerSchema):
+    id: int = Field(exclude=True, validation_alias='_id')
+    table: str = Field(exclude=True, validation_alias='__tablename__')
+
+    @computed_field
+    @property
+    def key(self) -> tuple[str, int]:
+        return (self.table, self.id)
 
 
 class ClientSchema(BaseModel):
