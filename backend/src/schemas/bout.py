8b5a384f64602dc1e9c05d8abe549ec6a480394c
Mitch Weisbrod <mitch@theweisbrods.com>
2025-07-28 19:36:53 -0700

    simply bout API

--- backend/src/schemas/bout.py
+++ backend/src/schemas/bout.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
 from pydantic import Field, computed_field
 
-from schemas.jam import JamSchema
-from schemas.schemas import ServerSchema
+from schemas.schemas import CacheableSchema, ServerSchema
 from schemas.team import TeamSchema
 from schemas.time import ClockSchema
 
@@ -10,7 +11,14 @@ class TimeoutSchema(ServerSchema):
     pass
 
 
-class BoutSchema(ServerSchema):
+class JamSchema(ServerSchema):
+    period: int
+    jam: int
+    start_timestamp: datetime | None
+    stop_timestamp: datetime | None
+
+
+class BoutSchema(CacheableSchema):
     ruleset: str
     clock: ClockSchema
     teams: list[TeamSchema]
@@ -19,18 +27,24 @@ class BoutSchema(ServerSchema):
 
     @computed_field
     @property
-    def num_jams(self) -> int:
-        return len(self.jams)
+    def num_jams(self) -> list[int]:
+        num_jams: list[int] = []
+        for jam in self.jams:
+            # A naive solution but it works because data is ordered
+            if len(num_jams) <= jam.period:
+                num_jams.append(0)
+            num_jams[-1] += 1
+        return num_jams
 
     @computed_field
     @property
     def current_jam(self) -> JamSchema | None:
-        num_jams: int = self.num_jams
-        if num_jams == 0:
+        num_jams: list[int] = self.num_jams
+        if len(num_jams) == 0:
             return None
         jam: JamSchema = self.jams[-1]
         if jam.start_timestamp is None:
-            if num_jams == 1:
+            if jam.jam == 0:
                 return None
             jam = self.jams[-2]
         return jam
