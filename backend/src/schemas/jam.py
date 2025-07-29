b0506762fc9c69bde6b41fc684ca97c2cd92a202
Mitch Weisbrod <mitch@theweisbrods.com>
2025-07-28 18:31:17 -0700

    add star pass

--- backend/src/schemas/jam.py
+++ backend/src/schemas/jam.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-from pydantic import Field
+from pydantic import Field, computed_field
 
-from schemas.schemas import ServerSchema
+from .schemas import ServerSchema
 
 
 class TripSchema(ServerSchema):
@@ -10,10 +10,26 @@ class TripSchema(ServerSchema):
     passes: int
 
 
+class StarPassSchema(ServerSchema):
+    trip: TripSchema | None
+    timestamp: datetime
+
+
 class TeamJamSchema(ServerSchema):
     lead: datetime | None
     lost: bool
     trips: list[TripSchema]
+    star_passes: list[StarPassSchema] = Field(exclude=True)
+
+    @computed_field
+    @property
+    def star_pass(self) -> int | None:
+        if len(self.star_passes) == 0:
+            return None
+        star_pass: StarPassSchema = self.star_passes[0]
+        if star_pass.trip is None:
+            return 0
+        return self.trips.index(star_pass.trip)
 
 
 class JamSchema(ServerSchema):
