0c6c327c37ddcb8e977225b2eb26a979dbb8c1cb
Mitch Weisbrod <mitch@theweisbrods.com>
2025-07-29 20:12:16 -0700

    add test_hook for flushes

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -2,7 +2,7 @@ from typing import Any, Callable
 
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, UOWTransaction
 
 from models import rulesets
 from models.bout import GenericBoutModel
@@ -11,7 +11,17 @@ from models.models import SessionLocal, SQLModel, engine
 from models.team import RosterModel, TeamModel
 from models.time import ClockModel
 
-pre_commit_hook: Callable[[Session], None] | None = None
+
+def test_hook(session: Session, flush_context: UOWTransaction) -> None:
+    print(
+        [
+            [type(i).__name__ for i in collection]
+            for collection in [session.dirty, session.new, session.deleted]
+        ]
+    )
+
+
+pre_commit_hook: Callable[[Session, UOWTransaction], None] | None = test_hook
 
 
 async def setup_db() -> None:
