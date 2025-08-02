1445744d7852adebbda710ff8a24f159ba5b259a
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-02 15:40:57 -0700

    update parent search hook

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -12,30 +12,23 @@ from models.team import RosterModel, TeamModel
 from models.time import ClockModel
 
 
-def _recursive_add(model: SQLModel) -> set[CacheableModel]:
-    cacheables: set[CacheableModel] = set()
-    for parent in model.parents:
-        if parent is None:
-            continue  # TODO: log a warning of improper use of this function
-        elif isinstance(parent, CacheableModel):
-            cacheables.add(parent)
-        cacheables |= _recursive_add(parent)
-    return cacheables
-
-
 def test_hook(session: Session, flush_context: UOWTransaction) -> None:
     # Get each cacheable model that is new
     cacheables: set[CacheableModel] = {
         item for item in session.new if isinstance(item, CacheableModel)
     }
     # Recursively add each item that is dirty or deleted
-    for model in [
-        item
-        for identity_map in [session.dirty, session.deleted]
-        for item in identity_map
-        if isinstance(item, SQLModel)
-    ]:
-        cacheables |= _recursive_add(model)
+    cacheables |= {
+        parent
+        for model in [
+            record
+            for identity_map in [session.dirty, session.deleted]
+            for record in identity_map
+            if isinstance(record, SQLModel)
+        ]
+        for parent in model.search_parents()
+        if isinstance(parent, CacheableModel)
+    }
 
     print(list(cacheables))
 
