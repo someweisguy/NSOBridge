34146e88f7fc3f9473e4a6eaa4faa9f15b2fc401
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-03 14:36:53 -0700

    set consistent imports

--- backend/src/schemas/jam.py
+++ backend/src/schemas/jam.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from pydantic import Field, computed_field
 
-from .schemas import ServerSchema
+from schemas.schemas import ServerSchema
 
 
 class TripSchema(ServerSchema):
