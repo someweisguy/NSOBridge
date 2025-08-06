007c4c72dc0512e26762d315a52e7bb757b0b7d0
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-05 19:28:21 -0700

    add todo

--- backend/src/schemas/time.py
+++ backend/src/schemas/time.py
@@ -4,6 +4,7 @@ from pydantic import Field
 
 from schemas.schemas import ClientSchema
 
+# TODO: documentation, see https://en.wikipedia.org/wiki/Cristian%27s_algorithm
 
 class ProcessTimeSchema(ClientSchema):
     process: datetime
