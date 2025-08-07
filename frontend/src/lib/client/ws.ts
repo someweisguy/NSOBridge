449b306c74683e1e6148a65288b84f8aa09679a3
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-06 21:00:36 -0700

    create websocket callback registry to handle custom callbacks

--- frontend/src/lib/client/ws.ts
+++ frontend/src/lib/client/ws.ts
@@ -1,60 +1,32 @@
-import "@/lib/client/sync.ts";
-
-export type UpdateKey = ["bouts", string] | ["jams", string, number, number];
-
-export class UpdateEvent extends Event {
-  constructor(public readonly key: UpdateKey) {
-    super("update");
-  }
-}
-
-export class ConnectionEvent extends Event {
-  constructor(public readonly online: boolean) {
-    super("connection");
-  }
-}
-
-export class SyncEvent extends Event {
-  constructor(public readonly data: SyncData) {
-    super("sync");
-  }
-}
-
-export interface SyncData {
-  process: Date;
-  server: Date;
-}
-
-declare global {
-  interface WindowEventMap {
-    connection: ConnectionEvent;
-    sync: SyncEvent;
-    update: UpdateEvent;
-  }
-}
-
-interface SyncSchema {
-  type: "sync";
-  data: SyncData;
-}
-
-interface UpdateSchema {
-  type: "update";
-  data: UpdateKey[];
-}
-
+const callbacks: Map<string, (d: unknown, ts: Date) => void> = new Map();
 const socket: WebSocket = new WebSocket(`ws://${window.location.host}/ws/`);
-socket.onopen = () => window.dispatchEvent(new ConnectionEvent(true));
-socket.onclose = () => window.dispatchEvent(new ConnectionEvent(false));
+
+function handle_connection_event(connect: boolean) {
+  const now = new Date();
+  const callback = callbacks.get("connection");
+  if (callback != undefined) {
+    callback(connect, now);
+  }
+}
+
+socket.onopen = () => handle_connection_event(true);
+socket.onclose = () => handle_connection_event(false);
 socket.onmessage = (event: MessageEvent<string>) => {
-  const payload = JSON.parse(event.data) as SyncSchema | UpdateSchema;
-  if (payload.type === "sync") {
-    window.dispatchEvent(new SyncEvent(payload.data));
-  } else if (payload.type === "update") {
-    for (const update of payload.data) {
-      window.dispatchEvent(new UpdateEvent(update));
-    }
-  } else {
-    // TODO: log invalid data type
+  const now = new Date();
+  const payload = JSON.parse(event.data) as { type: string; data: unknown };
+  const callback = callbacks.get(payload.type);
+  if (callback != undefined) {
+    callback(payload.data, now);
   }
 };
+
+export function register_callback(
+  type: string,
+  cb: (d: any, ts: Date) => void
+): void {
+  callbacks.set(type, cb);
+}
+
+export function delete_callback(type: string): boolean {
+  return callbacks.delete(type);
+}
