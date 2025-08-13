0b085bce2949901456ab57d5e190f1bb43ca8592
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-12 22:44:41 -0700

    format clock

--- frontend/src/components/clock.tsx
+++ frontend/src/components/clock.tsx
@@ -5,6 +5,40 @@ interface ClockProps {
   stopTimestamp?: Date | null;
   elapsed?: number;
   alarm?: number;
+  formatter?: (millis: number, displayMillis: boolean) => string;
+}
+
+function formatMilliseconds(millis: number, displayMillis: boolean): string {
+  const isNegative = millis < 0;
+  millis = Math.abs(millis);
+  let m = String(Math.floor((millis % 3600000) / 60000));
+  let s = String(Math.floor((millis / 1000) % 60));
+
+  let output: string;
+  if (millis >= 3600000) {
+    const h = Math.floor(millis / 3600000);
+    m = String(m).padStart(2, "0");
+    s = String(s).padStart(2, "0");
+    output = `${h}:${m}:${s}`;
+  } else if (millis >= 60000) {
+    s = String(s).padStart(2, "0");
+    output = `${m}:${s}`;
+  } else {
+    output = s;
+  }
+
+  // Render tenths of a seconds
+  if (displayMillis) {
+    const ds = String(Math.floor((millis % 1000) / 100)).padStart(1, "0");
+    output += `.${ds}`;
+  }
+
+  // Render a negative sign if the format string is not zero
+  if (isNegative && ((displayMillis && millis >= 100) || millis >= 1000)) {
+    output = "-" + output;
+  }
+
+  return output;
 }
 
 export default function Clock({
@@ -12,6 +46,7 @@ export default function Clock({
   stopTimestamp,
   elapsed = 0,
   alarm,
+  formatter = formatMilliseconds,
 }: ClockProps) {
   const clockMillis = useServerClock(
     startTimestamp,
@@ -20,5 +55,5 @@ export default function Clock({
     alarm
   );
 
-  return <>{clockMillis}</>;
+  return <>{formatter(clockMillis, clockMillis < 1000 * 10)}</>;
 }
