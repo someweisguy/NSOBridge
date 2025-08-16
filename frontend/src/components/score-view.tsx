1285f07aa0ac78b8d9fea4b2163790e2c3ef5c43
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-16 12:45:53 -0700

    add ScoreView for viewing jamScore and boutScore

---
+++ frontend/src/components/score-view.tsx
@@ -0,0 +1,20 @@
+interface ScoreViewProps {
+  boutScore: number;
+  scoreOffset: number;
+  jamScore: number;
+}
+
+export default function ScoreView({
+  boutScore,
+  scoreOffset = 0,
+  jamScore,
+}: ScoreViewProps) {
+  return (
+    <div className="flex justify-evenly items-baseline gap-4 m-4">
+      <div className="w-full font-bold text-7xl text-right">
+        {boutScore + scoreOffset}
+      </div>
+      <div className="w-full text-4xl">{jamScore}</div>
+    </div>
+  );
+}
