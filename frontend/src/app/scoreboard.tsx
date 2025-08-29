4a2bc45d79e5eb9ab0ba865aa4b768a41ca2b4e8
Mitch Weisbrod <mitch@theweisbrods.com>
2025-08-28 19:49:40 -0700

    rough in scoreboard information

--- frontend/src/app/scoreboard.tsx
+++ frontend/src/app/scoreboard.tsx
@@ -1,8 +1,15 @@
+import Clock from "@/components/clock";
+import JamNumView from "@/components/jam-num-view";
+import TeamView from "@/components/team-view";
+import useBout from "@/hooks/use-bout";
+import useBoutContext from "@/hooks/use-bout-context";
+import { Team } from "@/lib/bout";
 import queryClient from "@/lib/cache";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
+import useServerOffset from "@/hooks/use-server-offset";
 
 const root: HTMLElement = document.getElementById("root")!;
 createRoot(root).render(<App />);
@@ -20,5 +27,54 @@ export default function App() {
 }
 
 function Test() {
-  return <>This is the second page.</>;
+  void useServerOffset(); // Prefetch
+  const bout = useBout(1);
+  const context = useBoutContext(1);
+
+  return (
+    <>
+      <div className="place-content-around grid grid-flow-col">
+        {bout.teams.map((team: Team, index: number) => (
+          <TeamView key={index} team={team} context={context} />
+        ))}
+      </div>
+      <BoutTimeInformation />
+    </>
+  );
+}
+
+function BoutTimeInformation() {
+  const bout = useBout(1);
+  const context = useBoutContext(1);
+
+  if (bout.activeJam === null) {
+    const numPeriods = bout.jamCounts.length;
+    let copy = "";
+    if (numPeriods === 0) {
+      copy = "Starting Soon";
+    } else if (numPeriods === 1) {
+      copy = "Halftime";
+    } else if (!bout.isFinal) {
+      copy = "Unofficial Score";
+    } else {
+      copy = "Final Score";
+    }
+
+    return <div className="m-2 text-center">{copy}</div>;
+  }
+
+  return (
+    <div className="gap-3 grid grid-flow-col">
+      {bout.jamCounts.length > 2 ? "OT" : <Clock {...bout.clock} />}
+      <JamNumView {...bout.activeJam} jamCounts={bout.jamCounts} />
+      {!bout.activeJam.hasStarted() || bout.activeJam.isRunning() ? (
+        <Clock {...bout.activeJam} alarm={context.jamDuration} />
+      ) : (
+        <Clock
+          startTimestamp={bout.activeJam.stopTimestamp}
+          alarm={context.lineupDuration}
+        />
+      )}
+    </div>
+  );
 }
