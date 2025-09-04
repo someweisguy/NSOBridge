f110005bd4d93b6ec3dc376df0e4ac930d811109
Mitch Weisbrod <mitch@theweisbrods.com>
2025-09-03 19:13:47 -0700

    update tests

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,16 +1,16 @@
+import Button from "@/components/button";
 import Clock from "@/components/clock";
 import JamNumView from "@/components/jam-num-view";
 import TeamView from "@/components/team-view";
 import useBout from "@/hooks/use-bout";
 import useBoutContext from "@/hooks/use-bout-context";
-import { Team } from "@/lib/bout";
+import useServerOffset from "@/hooks/use-server-offset";
+import { Bout, Team } from "@/lib/bout";
 import queryClient from "@/lib/cache";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode, Suspense, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import useServerOffset from "@/hooks/use-server-offset";
-import Button from "@/components/button";
 
 const root: HTMLElement = document.getElementById("root")!;
 createRoot(root).render(<App />);
@@ -29,7 +29,7 @@ export default function App() {
 
 function Test() {
   void useServerOffset(); // Prefetch
-  const bout = useBout(1);
+  const bout: Bout = useBout(1);
   const context = useBoutContext(1);
 
   useEffect(() => {
