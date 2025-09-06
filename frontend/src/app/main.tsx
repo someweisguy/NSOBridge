e1bd32f7bef23e54b075455a61d535348c83c404
Mitch Weisbrod <mitch@theweisbrods.com>
2025-09-06 16:58:34 -0700

    go to the next bout when it is created

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -10,7 +10,14 @@ import { Bout, createBout, Team } from "@/lib/bout";
 import queryClient from "@/lib/cache";
 import { Series } from "@/lib/series";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode, Suspense, useCallback, useEffect } from "react";
+import {
+  StrictMode,
+  Suspense,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 
@@ -31,13 +38,24 @@ export default function App() {
 
 function Test() {
   void useServerOffset(); // Prefetch
-  const series: Series = useSeries(0);
+  const [boutIndex, setBoutIndex] = useState(0);
+  const goToNextBout = useRef(false);
+
+  const series: Series = useSeries(1);
   if (series.bouts.length == 0) {
     // TODO: Go to Bout creation page
     throw new Error("This Series does not have any Bouts");
   }
-  const bout: Bout = useBout(series.bouts.at(-1)!.id);
-  const context = useBoutContext(series.bouts.at(-1)!.id);
+  const bout: Bout = useBout(series.bouts[boutIndex].id);
+  const context = useBoutContext(series.bouts[boutIndex].id);
+
+  useEffect(() => {
+    if (goToNextBout.current && series.bouts.length > boutIndex + 1) {
+      goToNextBout.current = false;
+      setBoutIndex((i) => i + 1);
+    }
+    console.log("series or boutIndex update");
+  }, [series, boutIndex]);
 
   useEffect(() => {
     console.log(`Bout state: ${bout.getState()}`);
@@ -45,7 +63,8 @@ function Test() {
 
   const createBoutCallback = useCallback(() => {
     const rosterIds = bout.teams.map((t) => t.roster.id);
-    void createBout("WFTDA 2025", rosterIds);
+    goToNextBout.current = true;
+    void createBout(rosterIds);
   }, [bout]);
 
   return (
@@ -55,7 +74,7 @@ function Test() {
           <TeamView key={index} team={team} context={context} />
         ))}
       </div>
-      <BoutTimeInformation />
+      <BoutTimeInformation boutId={bout.id} />
       <div className="place-content-around grid grid-flow-col">
         <Button onClick={() => void bout.setupTrack()}>Start Period</Button>
         <Button onClick={() => void bout.startJam()}>Start Jam</Button>
@@ -69,9 +88,9 @@ function Test() {
   );
 }
 
-function BoutTimeInformation() {
-  const bout = useBout(1);
-  const context = useBoutContext(1);
+function BoutTimeInformation({ boutId }: { boutId: number }) {
+  const bout = useBout(boutId);
+  const context = useBoutContext(boutId);
 
   // Render a placeholder message when the Bout is not running
   if (bout.activeJam === null) {
