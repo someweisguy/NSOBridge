8875f6f5f6608d1fa1bbfd93b7483060e7aa7853
Mitch Weisbrod <mitch@theweisbrods.com>
2025-09-08 20:37:31 -0700

    temporarily use Roster ID as placeholder

--- frontend/components/team-view.tsx
+++ frontend/components/team-view.tsx
@@ -11,20 +11,19 @@ interface TeamsViewProps {
 export default function TeamView({
   team,
   context,
-  children
+  children,
 }: PropsWithChildren<TeamsViewProps>) {
+  // FIXME: Use Roster name, not roster ID
   return (
     <div className="justify-around grid grid-flow-col w-full">
-        <div className="place-items-center gap-7 grid grid-flow-row">
-          <div className="text-5xl text-center">{team.roster.name}</div>
-          <div className="grid grid-flow-col w-56">
-            <TimeoutBar {...team} {...context} />
-            <ScoreView {...team} />
-          </div>
-          <div>
-            {children}
-          </div>
+      <div className="place-items-center gap-7 grid grid-flow-row">
+        <div className="text-5xl text-center">{team.rosterId}</div>
+        <div className="grid grid-flow-col w-56">
+          <TimeoutBar {...team} {...context} />
+          <ScoreView {...team} />
         </div>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
