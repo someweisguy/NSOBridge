224741e3e0e2ee9d71670bc20585f011c19cad20
Mitch Weisbrod <mitch@theweisbrods.com>
2025-09-23 18:42:26 -0700

    don't minify for debugging purposes

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { createMpaPlugin } from "vite-plugin-virtual-mpa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [
     react(),
     tailwindcss(),
