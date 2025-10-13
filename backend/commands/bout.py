8a7b6a47d35898fc4cc615ab31c852bf90eaea35
Mitch Weisbrod <mitch@theweisbrods.com>
2025-10-13 13:33:28 -0700

    add Timeout controls

--- backend/commands/bout.py
+++ backend/commands/bout.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from typing import override
 
-from models import GenericBoutModel
+from models import GenericBoutModel, JamModel
+from models.time import TimeoutModel
 
 from .commands import Command
 
@@ -38,3 +39,51 @@ class SetBoutIsFinalCommand(Command):
     @override
     def execute(self) -> None:
         self.bout.is_final = self.is_final
+
+
+class BoutStartTimeout(Command):
+    def __init__(
+        self, bout: GenericBoutModel, timestamp: datetime, stop_clock: bool = True
+    ) -> None:
+        self.bout: GenericBoutModel = bout
+        self.timestamp: datetime = timestamp
+        self.stop_clock: bool = stop_clock
+
+    @override
+    def execute(self) -> None:
+        if self.stop_clock and self.bout.clock.is_running():
+            self.bout.clock.stop(self.timestamp)
+
+        # Timeouts are recorded on the latest running Jam
+        latest: JamModel = (
+            self.bout.jams[-2] if len(self.bout.jams) > 1 else self.bout.jams[-1]
+        )
+        timeout: TimeoutModel = TimeoutModel(
+            period=latest.period,
+            jam=latest.jam,
+            start_timestamp=self.timestamp,
+            clock_elapsed=self.bout.clock.get_duration(self.timestamp),
+        )
+        self.bout.timeouts.append(timeout)
+
+
+class BoutStopTimeout(Command):
+    def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
+        self.bout: GenericBoutModel = bout
+        self.timestamp: datetime = timestamp
+
+    @override
+    def execute(self) -> None:
+        if self.bout.get_state() != 'timeout':
+            raise RuntimeError('There is no active Timeout to stop')
+        timeout: TimeoutModel = self.bout.timeouts[-1]
+
+        timeout.stop(self.timestamp)
+
+        # Decrement the Timeout or Official Review if it was not retained
+        if timeout.team is not None and not timeout.retained:
+            # Only decrement if the value is greater than zero
+            if timeout.is_review and timeout.team.reviews_remaining > 0:
+                timeout.team.reviews_remaining -= 1
+            elif timeout.team.timeouts_remaining > 0:
+                timeout.team.timeouts_remaining -= 1
