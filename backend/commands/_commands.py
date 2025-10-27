c9a161b00d75bde5eff16562046d0273519ffc9c
Mitch Weisbrod <mitch@theweisbrods.com>
2025-10-26 19:29:48 -0700

    refactor AggregrateCommand and rename to MultiCommand

--- backend/commands/_commands.py
+++ backend/commands/_commands.py
@@ -1,39 +1,28 @@
 from abc import ABC, abstractmethod
+from functools import cached_property
 from typing import override
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import DeclarativeBase
 
 
 class Command(ABC):
-    # TODO: change arg name to 'session'
     @abstractmethod
     async def execute(self, session: AsyncSession) -> None: ...
 
-    # TODO: change arg name to 'session'
     @abstractmethod
     async def undo(self, session: AsyncSession) -> None: ...
 
-    # # TODO: change arg name to 'session'
-    # async def merge(self, session: AsyncSession) -> None:
-    #     for attr_name, attr_value in self.__dict__.items():
-    #         if isinstance(attr_value, DeclarativeBase):
-    #             setattr(self, attr_name, await session.merge(attr_value))
 
-
-class AggregateCommand(Command):
-    def __init__(self) -> None:
-        self._commands: list[Command] = []
-
-    def add(self, command: Command) -> None:
-        self._commands.append(command)
+class MultiCommand(Command):
+    @cached_property
+    def commands(self) -> tuple[Command, ...]: ...
 
     @override
     async def execute(self, session: AsyncSession) -> None:
-        for command in self._commands:
+        for command in self.commands:
             await command.execute(session)
-            
+
     @override
     async def undo(self, session: AsyncSession) -> None:
-        for command in reversed(self._commands):
+        for command in reversed(self.commands):
             await command.undo(session)
--- backend/commands/rulesets/wftda_2025/__init__.py
+++ backend/commands/rulesets/wftda_2025/__init__.py
@@ -1,8 +1,10 @@
+from dataclasses import dataclass
 from datetime import datetime
+from functools import cached_property
 from typing import Final
 
-from commands import Bout, Clock, Jam
-from commands._commands import AggregateCommand
+from commands import Bout, Clock, Command, Jam
+from commands._commands import MultiCommand
 from models import GenericBoutModel, JamModel
 
 NUM_PERIODS: Final[int] = 2
@@ -23,99 +25,126 @@ def get_next_jam_num(
     return (jam_num, period_num)
 
 
-class BeginPeriod(AggregateCommand):
-    def __init__(self, bout: GenericBoutModel) -> None:
-        super().__init__()
+class BeginPeriod(MultiCommand):
+    bout: GenericBoutModel
 
-        if bout.get_state() != 'stopped':
+    @cached_property
+    def commands(self) -> tuple[Command, ...]:
+        commands: list[Command] = []
+
+        if self.bout.get_state() != 'stopped':
             raise RuntimeError('The Bout cannot be started now')
 
-        home, away = bout.teams[:2]
-        period_num, jam_num = get_next_jam_num(bout, True)
+        home, away = self.bout.teams[:2]
+        period_num, jam_num = get_next_jam_num(self.bout, True)
         jam: JamModel = JamModel(period_num, jam_num, home, away)
-        self.add(Bout.AddJam(bout, jam))
+        commands.append(Bout.AddJam(self.bout, jam))
 
-        self.add(Bout.SetIsRunning(bout, True))
+        commands.append(Bout.SetIsRunning(self.bout, True))
 
-        if bout.get_period() < NUM_PERIODS:
-            self.add(Clock.Reset(bout.clock))
+        if self.bout.get_period() < NUM_PERIODS:
+            commands.append(Clock.Reset(self.bout.clock))
+
+        return tuple(commands)
 
 
-class EndPeriod(AggregateCommand):
-    def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
-        super().__init__()
+@dataclass
+class EndPeriod(MultiCommand):
+    bout: GenericBoutModel
+    timestamp: datetime
 
-        if bout.is_running and bout.get_state() != 'lineup':
+    @cached_property
+    def commands(self) -> tuple[Command, ...]:
+        commands: list[Command] = []
+
+        if self.bout.is_running and self.bout.get_state() != 'lineup':
             raise RuntimeError('The Bout cannot be stopped now')
 
         # TODO: Figure out a method to forfeit a Bout
 
-        if not bout.is_running and bout.get_period() >= NUM_PERIODS:
+        if not self.bout.is_running and self.bout.get_period() >= NUM_PERIODS:
             # Two EndPeriod commands in a row finalizes the bout
-            self.add(Bout.SetIsFinal(bout, True))
-        elif not bout.is_running:
+            commands.append(Bout.SetIsFinal(self.bout, True))
+        elif not self.bout.is_running:
             raise RuntimeError('The Bout cannot be ended yet')
 
         # End the Period
-        if bout.clock.is_running():
-            self.add(Clock.Stop(bout.clock, timestamp))
-        self.add(Bout.SetIsRunning(bout, False))
+        if self.bout.clock.is_running():
+            commands.append(Clock.Stop(self.bout.clock, self.timestamp))
+        commands.append(Bout.SetIsRunning(self.bout, False))
+
+        return tuple(commands)
 
 
-class StartJam(AggregateCommand):
-    def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
-        super().__init__()
+@dataclass
+class StartJam(MultiCommand):
+    bout: GenericBoutModel
+    timestamp: datetime
+
+    @cached_property
+    def commands(self) -> tuple[Command, ...]:
+        commands: list[Command] = []
 
         jam: JamModel
-        if not bout.is_running:
-            home, away = bout.teams[:2]
-            period_num, jam_num = get_next_jam_num(bout, True)
+        if not self.bout.is_running:
+            # FIXME: make this a call to BeginPeriod.commands
+            home, away = self.bout.teams[:2]
+            period_num, jam_num = get_next_jam_num(self.bout, True)
             jam = JamModel(period_num, jam_num, home, away)
-            self.add(Bout.AddJam(bout, jam))
+            commands.append(Bout.AddJam(self.bout, jam))
 
-            self.add(Bout.SetIsRunning(bout, True))
+            commands.append(Bout.SetIsRunning(self.bout, True))
 
-            if bout.get_period() < NUM_PERIODS:
-                self.add(Clock.Reset(bout.clock))
-        elif bout.get_state() == 'lineup':
-            jam = bout.jams[-1]
+            if self.bout.get_period() < NUM_PERIODS:
+                commands.append(Clock.Reset(self.bout.clock))
+        elif self.bout.get_state() == 'lineup':
+            jam = self.bout.jams[-1]
         else:
             raise RuntimeError('The Jam cannot be started now')
 
-        self.add(Jam.JamStart(jam, timestamp))
-        if not bout.clock.is_running() and bout.get_period() < NUM_PERIODS:
-            self.add(Clock.Start(bout.clock, timestamp))
+        commands.append(Jam.JamStart(jam, self.timestamp))
+        if not self.bout.clock.is_running() and self.bout.get_period() < NUM_PERIODS:
+            commands.append(Clock.Start(self.bout.clock, self.timestamp))
+
+        return tuple(commands)
 
 
-class StopJam(AggregateCommand):
-    def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
-        super().__init__()
+@dataclass
+class StopJam(MultiCommand):
+    bout: GenericBoutModel
+    timestamp: datetime
 
-        if bout.get_state() != 'jam':
+    @cached_property
+    def commands(self) -> tuple[Command, ...]:
+        commands: list[Command] = []
+        if self.bout.get_state() != 'jam':
             raise RuntimeError('There is no active Jam to stop')
 
-        self.add(Jam.JamStop(bout.jams[-1], timestamp))
+        commands.append(Jam.JamStop(self.bout.jams[-1], self.timestamp))
 
-        home, away = bout.teams[:2]
-        period_num, jam_num = get_next_jam_num(bout)
+        home, away = self.bout.teams[:2]
+        period_num, jam_num = get_next_jam_num(self.bout)
         jam: JamModel = JamModel(period_num, jam_num, home, away)
-        self.add(Bout.AddJam(bout, jam))
+        commands.append(Bout.AddJam(self.bout, jam))
+
+        return tuple(commands)
 
 
-class StartTimeout(AggregateCommand):
-    def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
-        super().__init__()
+# TODO: Convert to MultiCommands
+# class StartTimeout(AggregateCommand):
+#     def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
+#         super().__init__()
 
-        self.add(Bout.TimeoutStart(bout, timestamp))
+#         self.add(Bout.TimeoutStart(bout, timestamp))
 
-        if bout.clock.is_running():
-            self.add(Clock.Stop(bout.clock, timestamp))
+#         if bout.clock.is_running():
+#             self.add(Clock.Stop(bout.clock, timestamp))
 
 
-class StopTimeout(AggregateCommand):
-    def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
-        super().__init__()
+# class StopTimeout(AggregateCommand):
+#     def __init__(self, bout: GenericBoutModel, timestamp: datetime) -> None:
+#         super().__init__()
 
-        self.add(Bout.TimeoutStop(bout, timestamp))
+#         self.add(Bout.TimeoutStop(bout, timestamp))
 
-        # TODO: reduce team timeout count
+#         # TODO: reduce team timeout count
