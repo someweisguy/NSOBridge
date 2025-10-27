86212ed19d59773e8c2360723b91faa547fa7947
Mitch Weisbrod <mitch@theweisbrods.com>
2025-10-26 21:40:26 -0700

    remove duplicate class

--- backend/commands/_timeout.py
+++ backend/commands/_timeout.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import TYPE_CHECKING, Annotated, override
 
-from models.jam import TeamJamModel, TripEventModel
 from models.time import TimeoutModel
 from sqlalchemy import inspect, select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -46,25 +45,3 @@ class TimeoutStop(Command):
     async def undo(self, session: AsyncSession) -> None:
         timeout: TimeoutModel = await session.merge(self.detached_timeout)
         timeout.stop_timestamp = None
-
-
-@dataclass
-class AddTrip(Command):
-    detached_parent: TeamJamModel
-    trip_event: TripEventModel
-
-    @override
-    async def execute(self, session: AsyncSession) -> None:
-        if inspect(self.trip_event).transient:
-            self.detached_parent.events.append(self.trip_event)
-        else:
-            _ = await session.merge(self.trip_event)
-
-    @override
-    async def undo(self, session: AsyncSession) -> None:
-        trip_event: TripEventModel = await session.merge(self.trip_event)
-        await session.delete(trip_event)
-        self.trip_event = trip_event
-
-
-# TODO: DeleteTrip
