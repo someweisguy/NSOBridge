e204894b621005308446e2e733d0d800fa41fcb4
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-04 22:41:15 -0800

    rename variable

--- backend/api/series.py
+++ backend/api/series.py
@@ -11,18 +11,18 @@ router: Final[APIRouter] = APIRouter(prefix='/series')
 
 @router.get('', response_model=SeriesSchema)
 async def get_series(
-    db: AsyncSessionDepends, index: Annotated[int, Query(alias='seriesIndex')]
+    session: AsyncSessionDepends, index: Annotated[int, Query(alias='seriesIndex')]
 ) -> SeriesModel:
     statement = select(SeriesModel).limit(1).offset(index - 1)
-    results = await db.execute(statement)
+    results = await session.execute(statement)
     series: SeriesModel | None = (
-        results.scalar_one_or_none() if index == 0 else results.scalar_one()
+        results.scalar_one_or_none() if index == 1 else results.scalar_one()
     )
     if series is None:
         # No default Series exists so instantiate one
         series = SeriesModel()
-        db.add(series)
-        await db.flush()
+        session.add(series)
+        await session.flush()
     return series
 
 
