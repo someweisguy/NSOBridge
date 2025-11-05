6acdb9359448a30c522c83332d51f23a73a78f2c
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-04 20:38:48 -0800

    cull imports

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,11 +1,3 @@
-from abc import ABC
-from dataclasses import dataclass, field
-from inspect import Traceback
-from typing import Final, override
-
-from core.database import SessionLocal
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from .bout import GenericBoutModel
 from .jam import JamModel, TeamJamModel, TeamName
 from .models import CacheableModel
