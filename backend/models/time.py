ebde0880e4819e9565924381063ab4eec71b6d5e
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-04 20:48:27 -0800

    make clock_elapsed required

--- backend/models/time.py
+++ backend/models/time.py
@@ -70,7 +70,6 @@ class TimeoutModel(AbstractOneShotModel):
     _jam_id: Mapped[int] = mapped_column(ForeignKey('jams.id'))
     _team_id: Mapped[int | None] = mapped_column(ForeignKey('teams.id'))
 
-    # TODO: Make clock_elapsed an Optional[timedelta]
     clock_elapsed: Mapped[timedelta] = mapped_column(TimedeltaAsMilliseconds)
     details: Mapped[str | None] = mapped_column(default=None)
     is_review: Mapped[bool] = mapped_column(default=False)
@@ -82,6 +81,9 @@ class TimeoutModel(AbstractOneShotModel):
     team: Mapped[TeamModel | None] = relationship(
         back_populates='timeouts', foreign_keys=[_team_id]
     )
+
+    def __init__(self, clock_elapsed: timedelta) -> None:
+        super().__init__(clock_elapsed=clock_elapsed)
 
     @property
     @override
