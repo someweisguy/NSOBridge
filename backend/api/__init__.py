2d3becf1b32e6de0bb6c9c7b3592ee2bbe2a3815
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-04 22:53:26 -0800

    move main API router to __init__ file

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,15 +1,28 @@
 from typing import Final
 
-from fastapi.routing import APIRouter
+from core import UserDepends
+from fastapi import APIRouter
 
-from .api import router as api_router
 from .bout import router as bout_router
 from .jam import router as jam_router
 from .roster import router as roster_router
 from .series import router as series_router
 
+router: Final[APIRouter] = APIRouter()
+
+
+@router.post('/undo')
+async def undo(user: UserDepends) -> None:
+    await user.undo()
+
+
+@router.post('/redo')
+async def redo(user: UserDepends) -> None:
+    await user.redo()
+
+
 ROUTERS: Final[tuple[APIRouter, ...]] = (
-    api_router,
+    router,
     bout_router,
     jam_router,
     roster_router,
