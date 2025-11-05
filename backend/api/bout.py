eaf233e4ac8dcf1d011b60f3f46b21710c6935ac
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-04 21:09:19 -0800

    streamline the creation of mementos when handling Bouts

--- backend/api/bout.py
+++ backend/api/bout.py
@@ -1,12 +1,11 @@
+from collections.abc import AsyncGenerator
 from datetime import datetime
 from typing import TYPE_CHECKING, Annotated, Final, TypeAlias
 
 from core import UserDepends
 from core.database import AsyncSessionDepends
-from fastapi import APIRouter, Depends, Query
-from models import GenericBoutModel
-from models.bout import BoutContext
-from models.rulesets.wftda_2025 import BoutModel
+from fastapi import APIRouter, Depends, Query, Request
+from models.bout import BoutContext, GenericBoutModel
 from schemas import BoutSchema
 from schemas.bout import BoutContextSchema
 from sqlalchemy import select
@@ -19,13 +18,21 @@ router: Final[APIRouter] = APIRouter(prefix='/bout')
 
 @router.get('', response_model=BoutSchema)
 async def get_bout(
+    request: Request,
+    history: UserDepends,
     session: AsyncSessionDepends,
     bout_id: Annotated[int, Query(alias='boutId')],
-) -> GenericBoutModel:
+) -> AsyncGenerator[GenericBoutModel, None]:
+    # Query the database for the desired Bout
     statement = select(GenericBoutModel).where(GenericBoutModel.id == bout_id)
     results = await session.execute(statement)
     bout: GenericBoutModel = results.scalar_one()
-    return bout
+
+    # Optionally take a snapshot of the Bout state and yield the Bout
+    memento: Memento | None = bout.get_snapshot() if request.method != 'GET' else None
+    yield bout
+    if memento is not None:
+        history.push(memento)
 
 
 BoutDepends: TypeAlias = Annotated[GenericBoutModel, Depends(get_bout)]
@@ -33,69 +40,39 @@ BoutDepends: TypeAlias = Annotated[GenericBoutModel, Depends(get_bout)]
 
 @router.get('/context', response_model=BoutContextSchema)
 async def get_bout_context(
-    bout: Annotated[BoutModel, Depends(get_bout)],
+    bout: Annotated[GenericBoutModel, Depends(get_bout)],
 ) -> BoutContext:
     return bout.context
 
 
 @router.post('/setup-track')  # TODO: rename endpoint to begin-period
-async def begin_period(
-    history: UserDepends,
-    bout: BoutDepends,
-) -> None:
-    memento: Memento = bout.get_snapshot()
+async def begin_period(bout: BoutDepends) -> None:
     bout.begin_period(datetime.now())
-    history.push(memento)
 
 
 @router.post('/clear-track')  # TODO: rename endpoint to end-period
-async def end_period(
-    history: UserDepends,
-    bout: BoutDepends,
-) -> None:
-    memento: Memento = bout.get_snapshot()
+async def end_period(bout: BoutDepends) -> None:
     bout.end_period(datetime.now())
-    history.push(memento)
 
 
 @router.post('/start-jam')
-async def start_jam(
-    history: UserDepends,
-    bout: BoutDepends,
-) -> None:
-    memento: Memento = bout.get_snapshot()
+async def start_jam(bout: BoutDepends) -> None:
     bout.start_jam(datetime.now())
-    history.push(memento)
 
 
 @router.post('/stop-jam')
-async def stop_jam(
-    history: UserDepends,
-    bout: BoutDepends,
-) -> None:
-    memento: Memento = bout.get_snapshot()
+async def stop_jam(bout: BoutDepends) -> None:
     bout.stop_jam(datetime.now())
-    history.push(memento)
 
 
 @router.post('/call-timeout')  # TODO: rename endpoint to start-timeout
-async def call_timeout(
-    history: UserDepends,
-    bout: BoutDepends,
-) -> None:
-    memento: Memento = bout.get_snapshot()
+async def call_timeout(bout: BoutDepends) -> None:
     bout.start_timeout(datetime.now())
-    history.push(memento)
 
 
 @router.post(path='/end-timeout')  # TODO: rename endpoint to stop-timeout
-async def end_timeout(
-    history: UserDepends,
-    bout: BoutDepends,
-) -> None:
-    memento: Memento = bout.get_snapshot()
+async def end_timeout(bout: BoutDepends) -> None:
     bout.stop_timeout(datetime.now())
-    history.push(memento)
 
 
 __all__ = ('router',)
