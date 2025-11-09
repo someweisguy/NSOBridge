5abf9b9f6c9a1a5b483e8b763bb0f635579f5f9e
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-08 20:46:09 -0800

    fix endpoint prefix

--- backend/game/timeouts/router.py
+++ backend/game/timeouts/router.py
@@ -1,9 +1,9 @@
 from typing import Final
 
 from fastapi import APIRouter
-from game.timeouts.dependencies import get_timeout
 
+from .dependencies import get_timeout
 from .schemas import TimeoutSchema
 
-router: Final[APIRouter] = APIRouter(prefix='/jam')
+router: Final[APIRouter] = APIRouter(prefix='/timeout')
 router.add_api_route('', get_timeout, response_model=TimeoutSchema)
