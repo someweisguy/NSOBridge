2184847ead96aac5a52547dd0ee5bf120ed1189e
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-08 22:05:03 -0800

    create roster class to allow key generation

--- frontend/hooks/use-roster.tsx
+++ frontend/hooks/use-roster.tsx
@@ -3,7 +3,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 export default function useRoster(rosterId: number): Roster {
   const { data } = useSuspenseQuery<Roster[], unknown, Roster>({
-    queryKey: ["rosters", rosterId], // TODO: use key generator
+    queryKey: Roster.generateKey(rosterId),
     queryFn: () => getRosters([rosterId]),
     select: (rosters: Roster[]) => {
       // Only cache one item despite getRosters() returning multiple results
--- frontend/lib/game/rosters.ts
+++ frontend/lib/game/rosters.ts
@@ -1,13 +1,25 @@
 import genericRequest from "../requests";
 
-export interface Roster {
+export class Roster {
   readonly id: number;
   readonly name: string;
+
+  static generateKey(id: number) {
+    return ["rosters", id];
+  }
+
+  constructor(init: Partial<Roster>) {
+    Object.assign(this, init);
+  }
 }
 
 export async function getRosters(rosterIds: number[]): Promise<Roster[]> {
   const params = new URLSearchParams();
   rosterIds.map((id: number) => params.append("rosterId", id.toString()));
-  const response: Roster[] = await genericRequest("roster", "GET", params);
-  return response;
+  const response: Partial<Roster>[] = await genericRequest(
+    "roster",
+    "GET",
+    params,
+  );
+  return response.map((roster) => new Roster(roster));
 }
