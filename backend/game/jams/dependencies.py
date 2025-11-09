d0bf91654eb2b05dbdaf1fb72ac7d18eae994c3c
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-09 08:37:13 -0800

    make Jam API return optional

--- backend/game/jams/dependencies.py
+++ backend/game/jams/dependencies.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Annotated, TypeAlias
+from typing import TYPE_CHECKING, Annotated, Literal, TypeAlias, overload
 
 from database import AsyncSessionDepends
 from fastapi import Depends, Query
@@ -13,6 +13,43 @@ if TYPE_CHECKING:
     from sqlalchemy.sql.selectable import Select
 
 
+@overload
+async def get_jam_or_none(
+    session: AsyncSessionDepends,
+    bout_id: Annotated[int, Query(alias='boutId')],
+    period_num: Annotated[int, Query(alias='periodNum')],
+    jam_num: Annotated[int, Query(alias='jamNum')],
+    allow_none: Literal[True],
+) -> JamModel | None: ...
+
+
+@overload
+async def get_jam_or_none(
+    session: AsyncSessionDepends,
+    bout_id: Annotated[int, Query(alias='boutId')],
+    period_num: Annotated[int, Query(alias='periodNum')],
+    jam_num: Annotated[int, Query(alias='jamNum')],
+    allow_none: Literal[False],
+) -> JamModel: ...
+
+
+async def get_jam_or_none(
+    session: AsyncSessionDepends,
+    bout_id: Annotated[int, Query(alias='boutId')],
+    period_num: Annotated[int, Query(alias='periodNum')],
+    jam_num: Annotated[int, Query(alias='jamNum')],
+    allow_none: Annotated[bool, Query(include_in_schema=False)] = True,
+) -> JamModel | None:
+    statement: Select[tuple[JamModel]] = (
+        select(JamModel)
+        .where(JamModel.bout_id == bout_id)
+        .where(JamModel.period == period_num)
+        .where(JamModel.num == jam_num)
+    )
+    results: Result[tuple[JamModel]] = await session.execute(statement)
+    return results.scalar_one_or_none() if allow_none else results.scalar_one()
+
+
 async def get_jam(
     request: Request,
     user: UserDepends,
@@ -21,18 +58,19 @@ async def get_jam(
     period_num: Annotated[int, Query(alias='periodNum')],
     jam_num: Annotated[int, Query(alias='jamNum')],
 ) -> JamModel:
-    statement: Select[tuple[JamModel]] = (
-        select(JamModel)
-        .where(JamModel.bout_id == bout_id)
-        .where(JamModel.period == period_num)
-        .where(JamModel.num == jam_num)
+    allow_none: bool = False
+    jam: JamModel = await get_jam_or_none(
+        session,
+        bout_id,
+        period_num,
+        jam_num,
+        allow_none,
     )
-    results: Result[tuple[JamModel]] = await session.execute(statement)
-    jam: JamModel = results.scalar_one()
 
-    # Optionally take a snapshot of the state and return the Jam
+    # Optionally take a snapshot of the state
     if request.method != 'GET':
         user.stage(jam.get_snapshot())
+
     return jam
 
 
--- backend/game/jams/router.py
+++ backend/game/jams/router.py
@@ -2,8 +2,8 @@ from typing import Final
 
 from fastapi import APIRouter
 
-from .dependencies import get_jam
+from .dependencies import get_jam_or_none
 from .schemas import JamSchema
 
 router: Final[APIRouter] = APIRouter(prefix='/jam')
-router.add_api_route('', get_jam, response_model=JamSchema)
+router.add_api_route('', get_jam_or_none, response_model=JamSchema | None)
