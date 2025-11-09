2a928dcae33a8feb495bc1194e230cbaef6e04e7
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-09 08:21:00 -0800

    add getter with optional return

--- backend/game/timeouts/dependencies.py
+++ backend/game/timeouts/dependencies.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Annotated, TypeAlias
+from typing import TYPE_CHECKING, Annotated, Literal, TypeAlias, overload
 
 from database import AsyncSessionDepends
 from fastapi import Depends, Query
@@ -11,11 +11,32 @@ if TYPE_CHECKING:
     from sqlalchemy.sql.selectable import Select
 
 
-async def get_timeout(
+@overload
+async def get_timeout_or_none(
+    session: AsyncSessionDepends,
+    bout_id: int,
+    index: int,
+    allow_none: Literal[True],
+) -> TimeoutModel | None: ...
+
+
+@overload
+async def get_timeout_or_none(
+    session: AsyncSessionDepends,
+    bout_id: int,
+    index: int,
+    allow_none: Literal[False],
+) -> TimeoutModel: ...
+
+
+async def get_timeout_or_none(
     session: AsyncSessionDepends,
     bout_id: Annotated[int, Query(alias='boutId')],
     index: Annotated[int, Query(alias='index')],
-) -> TimeoutModel:
+    allow_none: Annotated[
+        bool, Query(alias='allowNone', include_in_schema=False)
+    ] = True,
+) -> TimeoutModel | None:
     statement: Select[tuple[TimeoutModel]] = (
         select(TimeoutModel)
         .where(TimeoutModel.bout_id == bout_id)
@@ -23,7 +44,21 @@ async def get_timeout(
         .limit(1)
     )
     results: Result[tuple[TimeoutModel]] = await session.execute(statement)
-    return results.scalar_one()
+    return results.scalar_one_or_none() if allow_none else results.scalar_one()
+
+
+async def get_timeout(
+    session: AsyncSessionDepends,
+    bout_id: Annotated[int, Query(alias='boutId')],
+    index: Annotated[int, Query(alias='index')],
+) -> TimeoutModel:
+    allow_none: bool = False
+    return await get_timeout_or_none(
+        session,
+        bout_id,
+        index,
+        allow_none,
+    )
 
 
 TimeoutDepends: TypeAlias = Annotated[TimeoutModel, Depends(get_timeout)]
--- backend/game/timeouts/router.py
+++ backend/game/timeouts/router.py
@@ -2,8 +2,8 @@ from typing import Final
 
 from fastapi import APIRouter
 
-from .dependencies import get_timeout
+from .dependencies import get_timeout_or_none
 from .schemas import TimeoutSchema
 
 router: Final[APIRouter] = APIRouter(prefix='/timeout')
-router.add_api_route('', get_timeout, response_model=TimeoutSchema)
+router.add_api_route('', get_timeout_or_none, response_model=TimeoutSchema | None)
