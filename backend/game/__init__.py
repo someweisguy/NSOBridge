2b7da86bbc5f78f1ce992c29135557cc004c2bfe
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-10 14:40:23 -0800

    add endpoints for trip api

--- backend/game/__init__.py
+++ backend/game/__init__.py
@@ -6,6 +6,7 @@ from .bouts.router import router as bout_router
 from .jams.router import router as jam_router
 from .rosters.router import router as roster_router
 from .series.router import router as series_router
+from .team_jams.router import router as team_jam_router
 from .timeouts.router import router as timeout_router
 
 ROUTERS: Final[tuple[APIRouter, ...]] = (
@@ -14,6 +15,7 @@ ROUTERS: Final[tuple[APIRouter, ...]] = (
     roster_router,
     series_router,
     timeout_router,
+    team_jam_router,
 )
 
 
--- backend/game/team_jams/router.py
+++ backend/game/team_jams/router.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from typing import Final
+from typing import Annotated, Final
 
 from fastapi import APIRouter
-from game.trip_events.models import TripEvent
+from fastapi.param_functions import Body
 
 from .dependencies import TeamJamDepends, get_team_jam
 from .schemas import TeamJamSchema
@@ -11,7 +11,23 @@ router: Final[APIRouter] = APIRouter(prefix='/team-jam')
 router.add_api_route('', get_team_jam, response_model=TeamJamSchema)
 
 
-@router.post('add-trip')
-async def add_trip(team_jam: TeamJamDepends) -> None:
-    event: TripEvent = TripEvent(datetime.now(), passes=4)  # FIXME
-    team_jam.add_trip(event)
+@router.post('/add-pass')
+async def add_trip(team_jam: TeamJamDepends, passes: Annotated[int, Body()]) -> None:
+    team_jam.add_trip(datetime.now(), passes)
+
+
+@router.post('/set-lead')
+async def set_lead(team_jam: TeamJamDepends, lead: Annotated[int, Body()]) -> None:
+    team_jam.set_lead(datetime.now(), lead)
+
+
+@router.post('/set-lost')
+async def set_lost(team_jam: TeamJamDepends, lost: Annotated[int, Body()]) -> None:
+    team_jam.set_lost(datetime.now(), lost)
+
+
+@router.post('/set-star-pass')
+async def set_star_pass(
+    team_jam: TeamJamDepends, star_pass: Annotated[int, Body()]
+) -> None:
+    team_jam.set_star_pass(datetime.now(), star_pass)
