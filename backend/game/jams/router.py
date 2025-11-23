3b5ef65a84aebc3e0d4269e714d26740a20e709a
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-23 14:06:46 -0800

    update API to throw error on failed GET

--- backend/game/jams/router.py
+++ backend/game/jams/router.py
@@ -2,8 +2,8 @@ from typing import Final
 
 from fastapi import APIRouter
 
-from .dependencies import get_jam_or_none
+from .dependencies import get_jam
 from .schemas import JamSchema
 
 router: Final[APIRouter] = APIRouter(prefix='/jam')
-router.add_api_route('', get_jam_or_none, response_model=JamSchema | None)
+router.add_api_route('', get_jam, response_model=JamSchema)
