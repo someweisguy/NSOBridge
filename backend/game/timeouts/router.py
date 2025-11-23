8d89ae17abd21a580c775ee4142d654defddbe1f
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-22 20:20:50 -1000

    implement required timeout API

--- backend/game/timeouts/router.py
+++ backend/game/timeouts/router.py
@@ -1,6 +1,7 @@
-from typing import Final
+from typing import Annotated, Final
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from game.teams.dependencies import OptionalTeamDepends
 
 from .dependencies import TimeoutDepends, get_timeout
 from .schemas import TimeoutSchema
@@ -10,13 +11,19 @@ router.add_api_route('', get_timeout, response_model=TimeoutSchema | None)
 
 
 @router.post('/type')
-async def set_type(timeout: TimeoutDepends) -> None:
-    pass  # TODO: set timeout team and whether or not the timeout is a review
+async def set_type(
+    timeout: TimeoutDepends,
+    team_or_none: OptionalTeamDepends,  # TODO: this should be a Body parameter
+    is_review: bool,
+) -> None:
+    timeout.set_type(team_or_none, is_review)
 
 
 @router.post('/retained')
-async def set_retained(timeout: TimeoutDepends) -> None:
-    pass  # TODO: set whether or not the timeout is retained
+async def set_retained(
+    timeout: TimeoutDepends, retained: Annotated[bool, Body()]
+) -> None:
+    timeout.set_retained(retained)
 
 
 @router.put('/details')
