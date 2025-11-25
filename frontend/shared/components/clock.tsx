cfe18b39e36292685b7e3c8a9cfec38b5db9a2e7
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-24 21:54:55 -0800

    add mixin for clock component

--- frontend/shared/components/clock.tsx
+++ frontend/shared/components/clock.tsx
@@ -1,48 +1,13 @@
 import useClock from "@/shared/hooks/use-clock";
 import { useEffect, useState } from "react";
-
-function formatMilliseconds(
-  millis: number,
-  showTenths: boolean,
-  sign = "+",
-): string {
-  const isNegative = millis < 0;
-  millis = Math.abs(millis);
-  let m = String(Math.floor((millis % 3600000) / 60000));
-  let s = String(Math.floor((millis / 1000) % 60));
-
-  let output: string;
-  if (millis >= 3600000) {
-    const h = Math.floor(millis / 3600000);
-    m = String(m).padStart(2, "0");
-    s = String(s).padStart(2, "0");
-    output = `${h}:${m}:${s}`;
-  } else if (millis >= 60000) {
-    s = String(s).padStart(2, "0");
-    output = `${m}:${s}`;
-  } else {
-    output = s;
-  }
-
-  // Render tenths of a seconds
-  if (showTenths) {
-    const ds = String(Math.floor((millis % 1000) / 100)).padStart(1, "0");
-    output += `.${ds}`;
-  }
-
-  // Render a negative sign if the format string is not zero
-  if (isNegative && ((showTenths && millis >= 100) || millis >= 1000)) {
-    output = sign + output;
-  }
-
-  return output;
-}
+import defaultTimeStringFormatter from "../utils/DefaultTimeStringFormatter";
 
 interface ClockProps {
   startTimestamp: Date | null;
   stopTimestamp?: Date | null;
   elapsed?: number;
   alarm?: number;
+  formatter: (milliseconds: number) => string;
 }
 
 export default function Clock({
@@ -50,6 +15,7 @@ export default function Clock({
   stopTimestamp,
   elapsed = 0,
   alarm,
+  formatter = defaultTimeStringFormatter,
 }: ClockProps) {
   const [run, setRun] = useState<boolean>(false);
   const [clockMillis] = useClock(
@@ -63,11 +29,5 @@ export default function Clock({
   }, [clockMillis]);
 
   const displayMillis = clockMillis > 0 ? clockMillis : 0;
-  const showTenths =
-    alarm != null && clockMillis < 10000 && clockMillis > -2000;
-  return (
-    <label className="tabular-nums">
-      {formatMilliseconds(displayMillis, showTenths)}
-    </label>
-  );
+  return <label className="tabular-nums">{formatter(displayMillis)}</label>;
 }
---
+++ frontend/shared/utils/DefaultTimeStringFormatter.ts
@@ -0,0 +1,30 @@
+export default function defaultTimeStringFormatter(millis: number): string {
+  const isNegative = millis < 0;
+  millis = Math.abs(millis);
+  let m = String(Math.floor((millis % 3600000) / 60000));
+  let s = String(Math.floor((millis / 1000) % 60));
+
+  let output: string;
+  if (millis >= 3600000) {
+    const h = Math.floor(millis / 3600000);
+    m = String(m).padStart(2, "0");
+    s = String(s).padStart(2, "0");
+    output = `${h}:${m}:${s}`;
+  } else if (millis >= 60000) {
+    s = String(s).padStart(2, "0");
+    output = `${m}:${s}`;
+  } else {
+    output = s;
+  }
+
+  // Render tenths of a seconds
+  const ds = String(Math.floor((millis % 1000) / 100)).padStart(1, "0");
+  output += `.${ds}`;
+
+  // Render a negative sign if the format string is not zero
+  if (isNegative && (millis >= 100 || millis >= 1000)) {
+    output = "+" + output;
+  }
+
+  return output;
+}
