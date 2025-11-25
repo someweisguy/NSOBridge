787d554a276b0e749799a893ef78b2d8932dce2d
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-24 21:45:12 -0800

    change type to class

--- frontend/shared/types/bouts.ts
+++ frontend/shared/types/bouts.ts
@@ -29,10 +29,10 @@ export class Bout {
   }
 }
 
-export type BoutContext = Readonly<{
+export class BoutContext {
   jamDuration: number;
   lineupDuration: number;
   pointsPerTrip: number;
   numTimeouts: number;
   numReviews: number;
-}>;
+}
