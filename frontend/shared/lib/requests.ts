c496cb0bc65b9a756f8a4be56453cebe63a075ff
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-24 19:00:41 -0800

    update API to use a class with dedicated HTML methods

--- frontend/shared/lib/requests.ts
+++ frontend/shared/lib/requests.ts
@@ -1,30 +1,78 @@
-export default async function genericRequest<T = unknown>(
-  endpoint: string,
-  method: "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "PATCH",
-  query?: URLSearchParams | Record<string, unknown>,
-  body?: string | number | boolean | object | null,
-): Promise<T> {
-  // Generate the request URL
-  const url = new URL(`/api/${endpoint}`, window.location.href);
-  if (query !== undefined) {
-    url.search = new URLSearchParams(
-      query as Record<string, string>,
-    ).toString();
+export default class API {
+  readonly host: string;
+
+  constructor(host: string) {
+    this.host = host;
   }
 
-  // Send the request
-  const response = await fetch(url, {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: body instanceof Object ? JSON.stringify(body) : body?.toString(),
-  });
+  private async genericRequest<T = unknown>(
+    endpoint: string,
+    method: "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "PATCH",
+    query?: URLSearchParams | Record<string, unknown>,
+    body?: string | number | boolean | object | null,
+  ): Promise<T> {
+    // Generate the request URL
+    const url = new URL(`/api/${endpoint}`, this.host);
+    if (query !== undefined) {
+      url.search = new URLSearchParams(
+        query as Record<string, string>,
+      ).toString();
+    }
 
-  // Parse the response
-  if (!response.ok) {
-    const error = (await response.json()) as { message: string };
-    throw new Error(error.message);
+    // Send the request
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body instanceof Object ? JSON.stringify(body) : body?.toString(),
+    });
+
+    // Parse the response
+    if (!response.ok) {
+      const error = (await response.json()) as { message: string };
+      throw new Error(error.message);
+    }
+    return (await response.json()) as T;
   }
-  return (await response.json()) as T;
+
+  async get<T = unknown>(
+    endpoint: string,
+    query?: URLSearchParams | Record<string, unknown>,
+  ): Promise<T> {
+    return this.genericRequest(endpoint, "GET", query);
+  }
+
+  async head<T = unknown>(
+    endpoint: string,
+    query?: URLSearchParams | Record<string, unknown>,
+  ): Promise<T> {
+    return this.genericRequest(endpoint, "HEAD", query);
+  }
+
+  async post<T = unknown>(
+    endpoint: string,
+    query?: URLSearchParams | Record<string, unknown>,
+    body?: string | number | boolean | object | null,
+  ): Promise<T> {
+    return this.genericRequest(endpoint, "POST", query, body);
+  }
+
+  async put<T = unknown>(
+    endpoint: string,
+    query?: URLSearchParams | Record<string, unknown>,
+    body?: string | number | boolean | object | null,
+  ): Promise<T> {
+    return this.genericRequest(endpoint, "PUT", query, body);
+  }
+
+  async delete<T = unknown>(
+    endpoint: string,
+    query?: URLSearchParams | Record<string, unknown>,
+    body?: string | number | boolean | object | null,
+  ): Promise<T> {
+    return this.genericRequest(endpoint, "DELETE", query, body);
+  }
 }
+
+export const localAPI = new API(window.location.href);
