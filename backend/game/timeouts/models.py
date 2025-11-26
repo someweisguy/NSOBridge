e9b8366b045b3bf11baeec7985a4292ccb9d874a
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-25 19:44:43 -0800

    reduce arguments to init function

--- backend/game/timeouts/models.py
+++ backend/game/timeouts/models.py
@@ -56,8 +56,8 @@ class BaseTimeout(AbstractOneShotModel, CacheableSQLModel):
         'polymorphic_on': ruleset,
     }
 
-    def __init__(self, clock_elapsed: timedelta, is_review: bool = False) -> None:
-        super().__init__(clock_elapsed=clock_elapsed, is_review=is_review)
+    def __init__(self, clock_elapsed: timedelta) -> None:
+        super().__init__(clock_elapsed=clock_elapsed)
 
     @override
     def cache_key(self) -> tuple[Any, ...]:
