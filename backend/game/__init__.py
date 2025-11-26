4e4a7426184a82c2d89aa7f38d534e84e31b846a
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-25 16:24:48 -0800

    move team_jam router to jam

--- backend/game/__init__.py
+++ backend/game/__init__.py
@@ -6,7 +6,6 @@ from .bouts.router import router as bout_router
 from .jams.router import router as jam_router
 from .rosters.router import router as roster_router
 from .series.router import router as series_router
-from .team_jams.router import router as team_jam_router
 from .timeouts.router import router as timeout_router
 
 ROUTERS: Final[tuple[APIRouter, ...]] = (
@@ -15,7 +14,6 @@ ROUTERS: Final[tuple[APIRouter, ...]] = (
     roster_router,
     series_router,
     timeout_router,
-    team_jam_router,
 )
 
 
--- backend/game/jams/dependencies.py
+++ backend/game/jams/dependencies.py
@@ -1,8 +1,8 @@
 from typing import TYPE_CHECKING, Annotated, Literal, TypeAlias, overload
 
 from database import AsyncSessionDepends
-from fastapi import Depends, Query
-from fastapi.requests import Request
+from fastapi import Depends, Query, Request
+from game.team_jams.models import BaseTeamJam
 from sqlalchemy import select
 from users.dependencies import UserDepends
 
@@ -77,3 +77,18 @@ async def get_jam(
 
 
 JamDepends: TypeAlias = Annotated[BaseJam, Depends(get_jam)]
+
+
+async def get_team_jam(
+    jam: JamDepends,
+    team_id: Annotated[int, Query()],
+) -> BaseTeamJam:
+    team_jam: BaseTeamJam | None = next(
+        (tj for tj in jam.team_jams if tj.id == team_id), None
+    )
+    if team_jam is None:
+        raise KeyError('the specified team was not found in this Jam')
+    return team_jam
+
+
+TeamJamDepends: TypeAlias = Annotated[BaseTeamJam, Depends(get_team_jam)]
