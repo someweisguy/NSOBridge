12a9a051ca5913c5b3664c462fe11812da26af82
Mitch Weisbrod <mitch@theweisbrods.com>
2025-11-25 20:25:36 -0800

    add computed field decorator

--- backend/game/series/schemas.py
+++ backend/game/series/schemas.py
@@ -1,5 +1,6 @@
 from game.bouts.schemas import BoutSchema
 from pydantic import Field
+from pydantic.fields import computed_field
 from schemas import ServerSchema
 
 
@@ -7,6 +8,7 @@ class SeriesSchema(ServerSchema):
     name: str
     bouts: list[BoutSchema] = Field(exclude=True)
 
+    @computed_field
     @property
     def bout_ids(self) -> list[int]:
         return [bout.id for bout in self.bouts]
