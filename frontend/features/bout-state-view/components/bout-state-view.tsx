5620dd51ff95dab0d76c8d05d928f4ddbf44ba9c
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-01 17:50:26 -0800

    render non-Jam states

--- frontend/features/bout-state-view/components/bout-state-view.tsx
+++ frontend/features/bout-state-view/components/bout-state-view.tsx
@@ -3,10 +3,12 @@ import useRuleset from "@/hooks/use-ruleset";
 import { Bout } from "@/types/game";
 import useActiveJam from "../hooks/use-active-jam";
 import IntermissionStateView from "./intermission-state-view";
+import useTimeout from "@/hooks/use-timeout";
 
 export default function BoutStateView({ bout }: { bout: Bout }) {
   const ruleset = useRuleset(bout.id);
   const activeJam = useActiveJam(bout.id);
+  const latestTimeout = useTimeout(bout.id, bout.numTimeouts - 1);
 
   if (!bout.isRunning) {
     return <IntermissionStateView bout={bout} />;
@@ -20,19 +22,37 @@ export default function BoutStateView({ bout }: { bout: Bout }) {
     jamNum += bout.jamCounts[1];
   }
 
+  // Render non-Jam Bout states
+  let gameStopTimestamp: Date | null = null;
+  let gameState = "";
+  if (bout.state === "lineup" && activeJam.stopTimestamp != null) {
+    gameState = "Lineup";
+    gameStopTimestamp = activeJam.stopTimestamp;
+  } else if (bout.state === "timeout") {
+    gameState = "Timeout";
+    gameStopTimestamp = latestTimeout!.startTimestamp;
+  }
+
   return (
-    <div className="justify-evenly grid grid-cols-3 text-7xl text-center">
+    <div className="justify-evenly grid grid-cols-3 p-4 text-7xl text-center">
       <div>{activeJam.period >= 2 ? "OT" : <Clock {...bout.clock} />}</div>
       <div>
         P{periodNum + 1} J{jamNum + 1}
       </div>
       <div>
-        {/* TODO: This div should show the call reason when a Jam ends */}
-        <Clock {...activeJam} alarm={ruleset.jamDuration} />
+        {/* TODO: This section should show the call reason when a Jam ends */}
+        {!activeJam.hasStarted() || activeJam.isRunning() ? (
+          <Clock {...activeJam} alarm={ruleset.jamDuration} />
+        ) : (
+          "-"
+        )}
       </div>
 
       {/* TODO: This div is for Lineup, Timeout, Unofficial, and Final*/}
-      <div className="col-start-2 text-5xl"> </div>
+      <div className="col-start-2 text-5xl">
+        {gameState}&nbsp;
+        {gameStopTimestamp && <Clock startTimestamp={gameStopTimestamp} />}
+      </div>
     </div>
   );
 }
