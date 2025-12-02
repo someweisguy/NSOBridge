dde99e7d174da93e718a75c4b1cfe822c4e3252d
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-01 19:08:17 -0800

    remove unused component

--- frontend/app/scoreboard.tsx
+++ frontend/app/scoreboard.tsx
@@ -2,7 +2,7 @@ import BoutStateView from "@/features/bout-state-view/components/bout-state-view
 import TeamView from "@/features/team-view/team-view";
 import useBout from "@/hooks/use-bout";
 import queryClient from "@/lib/cache";
-import { Bout, Team } from "@/types/game";
+import { Team } from "@/types/game";
 import FitScreen from "@fit-screen/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode, Suspense } from "react";
@@ -37,31 +37,10 @@ function Test() {
           <TeamView key={i} team={team} />
         ))}
       </div>
-      <div className="place-content-center text-center basis-1/8 shrink-0 grow-0">
-        {/* Tertiary Information (Game State) */}
-        <BoutTertiaryInformation bout={bout} />
-      </div>
-      <div className="justify-stretch items-stretch grid basis-3/8 shrink-0">
+      <div className="justify-stretch items-stretch grid basis-1/2 shrink-0">
         {/* Secondary Information (Clocks, etc.) */}
         <BoutStateView bout={bout} />
       </div>
     </div>
   );
-}
-
-function BoutTertiaryInformation({ bout }: { bout: Bout }) {
-  if (["timeout", "lineup"].includes(bout.state)) {
-    let content = "";
-    if (bout.state == "timeout") {
-      // TODO: Implement Team Timeout and Official Review
-      content = "Timeout";
-    } else if (bout.state == "lineup") {
-      content = "Lineup";
-    }
-
-    return <h1 className="text-7xl">{content}</h1>;
-  }
-
-  // TODO: Implement Jam state
-  return <></>;
 }
