e7616f56efaff02777ddb120f4cae28fc4eff232
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-04 20:53:04 -0800

    add first pass of add trip buttons

--- frontend/features/team-jam-view/team-jam-view.tsx
+++ frontend/features/team-jam-view/team-jam-view.tsx
@@ -1,4 +1,7 @@
+import { jamAddTrip } from "@/lib/game/jams";
 import { Jam, Team, TeamJam } from "@/types/game";
+import { Button, SimpleGrid } from "@mantine/core";
+import { useMutation } from "@tanstack/react-query";
 
 interface TeamJamViewProps {
   jam: Jam;
@@ -19,8 +22,48 @@ export default function TeamJamView({ jam, team }: TeamJamViewProps) {
 
   return (
     <>
-      TeamJam ID: {teamJam.id}, Lead = {String(lead)}, Lost = {String(lost)},
-      Star Pass = {String(starPass)}
+      <AddTripButtons jam={jam} team={team} />
+      Lead: {String(lead)}, Lost: {String(lost)}, Star Pass: {String(starPass)}
     </>
+  );
+}
+
+function AddTripButtons({ jam, team }: TeamJamViewProps) {
+  const teamJam: TeamJam | undefined = jam.teamJams.find(
+    (teamJam: TeamJam) => teamJam.teamId === team.id,
+  );
+  if (teamJam == undefined) {
+    throw new Error("team jam not found");
+  }
+
+  const addTrip = useMutation({
+    mutationFn: (passes: number) =>
+      jamAddTrip(team.boutId, jam.period, jam.num, team.id, passes),
+  });
+
+  if (teamJam.events.length == 0) {
+    return (
+      <SimpleGrid cols={2} spacing="md">
+        <Button variant="filled" onClick={() => addTrip.mutate(0)}>
+          No Pass
+        </Button>
+        <Button variant="filled" onClick={() => addTrip.mutate(4)}>
+          Initial
+        </Button>
+      </SimpleGrid>
+    );
+  }
+  return (
+    <SimpleGrid cols={5} spacing="md">
+      {[0, 1, 2, 3, 4].map((passes: number) => (
+        <Button
+          variant="filled"
+          key={passes}
+          onClick={() => addTrip.mutate(passes)}
+        >
+          {passes}
+        </Button>
+      ))}
+    </SimpleGrid>
   );
 }
