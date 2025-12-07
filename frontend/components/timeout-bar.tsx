f050c63a5c17a6fe736296c858a83b9119ca9048
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-06 22:17:37 -0800

    use Mantine

--- frontend/components/timeout-bar.tsx
+++ frontend/components/timeout-bar.tsx
@@ -1,27 +1,39 @@
 import { RulesetContext } from "@/lib/game/bouts";
 import { Team, Timeout } from "@/types/game";
-import { twMerge } from "tailwind-merge";
-
-const dotStyle = "bg-black rounded-full aspect-square";
-
-const sizeStyles = {
-  small: "w-10",
-  medium: "w-16",
-  large: "w-20",
-};
+import { Card, Center, Divider } from "@mantine/core";
 
 interface TimeoutBarProps {
   team: Team;
   activeTimeout?: Timeout | null;
   ruleset: RulesetContext;
-  size?: keyof typeof sizeStyles;
+  size: number;
+}
+
+interface TimeoutPipProps {
+  size: number;
+}
+
+function TimeoutPip({ size }: TimeoutPipProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="icon icon-tabler icons-tabler-filled icon-tabler-circle"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z" />
+    </svg>
+  );
 }
 
 export default function TimeoutBar({
-  team,
+  // team,
   activeTimeout,
   ruleset,
-  size = "medium",
+  size = 30,
 }: TimeoutBarProps) {
   if (!activeTimeout?.isRunning()) {
     // There is no active Timeout
@@ -29,39 +41,61 @@ export default function TimeoutBar({
   }
 
   return (
-    <div
-      className={twMerge(
-        sizeStyles[size],
-        "flex flex-col justify-start gap-4 bg-gray-300 p-2 rounded-2xl h-fit",
-      )}
-    >
-      {Array.from({ length: ruleset.numTimeouts }, (_, k) => (
-        <div
-          key={`t${k}`}
-          className={twMerge(
-            dotStyle,
-            k >= team.timeoutsRemaining && "invisible",
-            k == team.timeoutsRemaining - 1 &&
-              activeTimeout?.teamId == team.id &&
-              !activeTimeout?.isReview &&
-              "animate-blink",
-          )}
-        ></div>
+    <Card withBorder w={size} radius="md">
+      {Array.from({ length: ruleset.numTimeouts }, (_, i) => (
+        <Card.Section key={i}>
+          <Center>
+            <TimeoutPip size={size} />
+          </Center>
+        </Card.Section>
       ))}
-      <hr className="mx-1 border-gray-400"></hr>
-      {Array.from({ length: ruleset.numReviews }, (_, k) => (
-        <div
-          key={`r${k}`}
-          className={twMerge(
-            dotStyle,
-            k >= team.reviewsRemaining && "invisible",
-            k == team.reviewsRemaining - 1 &&
-              activeTimeout?.teamId == team.id &&
-              activeTimeout?.isReview &&
-              "animate-blink",
-          )}
-        ></div>
+      <Card.Section>
+        <Divider mx={4} />
+      </Card.Section>
+      {Array.from({ length: ruleset.numReviews }, (_, i) => (
+        <Card.Section key={i}>
+          <Center>
+            <TimeoutPip size={size} />
+          </Center>
+        </Card.Section>
       ))}
-    </div>
+    </Card>
   );
+
+  // return (
+  //   <div
+  //     className={twMerge(
+  //       sizeStyles[size],
+  //       "flex flex-col justify-start gap-4 bg-gray-300 p-2 rounded-2xl h-fit"
+  //     )}
+  //   >
+  //     {Array.from({ length: ruleset.numTimeouts }, (_, k) => (
+  //       <div
+  //         key={`t${k}`}
+  //         className={twMerge(
+  //           dotStyle,
+  //           k >= team.timeoutsRemaining && "invisible",
+  //           k == team.timeoutsRemaining - 1 &&
+  //             activeTimeout?.teamId == team.id &&
+  //             !activeTimeout?.isReview &&
+  //             "animate-blink"
+  //         )}
+  //       ></div>
+  //     ))}
+  //     <hr className="mx-1 border-gray-400"></hr>
+  //     {Array.from({ length: ruleset.numReviews }, (_, k) => (
+  //       <div
+  //         key={`r${k}`}
+  //         className={twMerge(
+  //           dotStyle,
+  //           k >= team.reviewsRemaining && "invisible",
+  //           k == team.reviewsRemaining - 1 &&
+  //             activeTimeout?.teamId == team.id &&
+  //             activeTimeout?.isReview &&
+  //             "animate-blink"
+  //         )}
+  //       ></div>
+  //     ))}
+  //   </div>
+  // );
 }
