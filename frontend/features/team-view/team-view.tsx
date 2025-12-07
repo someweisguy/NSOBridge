095067255ff05a92d2a8a687f379861e6ecdae4e
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-06 23:00:47 -0800

    polish layout

--- frontend/features/team-view/team-view.tsx
+++ frontend/features/team-view/team-view.tsx
@@ -3,7 +3,7 @@ import useRoster from "@/hooks/use-roster";
 import useRuleset from "@/hooks/use-ruleset";
 import useTimeout from "@/hooks/use-timeout";
 import { Team } from "@/types/game";
-import { Center, Grid, Stack, Title } from "@mantine/core";
+import { Center, SimpleGrid, Stack, Title } from "@mantine/core";
 import JammerStatus from "../../components/jammer-status";
 import TimeoutBar from "../../components/timeout-bar";
 
@@ -18,38 +18,31 @@ export default function TeamView({ team }: TeamsViewProps) {
   const ruleset = useRuleset(team.boutId);
 
   return (
-    <Stack>
+    <Stack w="full">
       <Center>
-        <Title order={2} size={48}>
+        <Title order={2} size={56}>
           <b>{roster.name}</b>
         </Title>
       </Center>
-      <Grid columns={3} justify="center" align="center">
-        <Grid.Col span="auto">
-          <TimeoutBar
-            team={team}
-            activeTimeout={activeTimeout}
-            ruleset={ruleset}
-          />
-        </Grid.Col>
-        <Grid.Col span="auto">
-          <Grid columns={2} justify="center" align="center">
-            <Grid.Col span={1}>
-              <Title order={1} size={64}>
-                <b>{team.boutScore + team.scoreOffset}</b>
-              </Title>
-            </Grid.Col>
-            <Grid.Col span={1}>
-              <Title order={1} size={30}>
-                {team.jamScore}
-              </Title>
-            </Grid.Col>
-          </Grid>
-        </Grid.Col>
-        <Grid.Col span="auto">
-          <JammerStatus lead={false} lost={false} starPass={false} />
-        </Grid.Col>
-      </Grid>
+      <SimpleGrid cols={3}>
+        <TimeoutBar
+          team={team}
+          activeTimeout={activeTimeout}
+          ruleset={ruleset}
+          size={30}
+        />
+        <SimpleGrid cols={2} spacing="sm">
+          <Center>
+            <Title size={72}>
+              <b>{team.boutScore + team.scoreOffset}</b>
+            </Title>
+          </Center>
+          <Center>
+            <Title size={30}>{team.jamScore}</Title>
+          </Center>
+        </SimpleGrid>
+        <JammerStatus lead={false} lost={false} starPass={false} />
+      </SimpleGrid>
     </Stack>
   );
 }
