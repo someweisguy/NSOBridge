b3c438a96de16be7ba90b28d0fef8ec7d5057efb
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-08 17:13:50 -0800

    extract component to separate file

--- frontend/features/team-jam-view/team-jam-view.tsx
+++ frontend/features/team-jam-view/team-jam-view.tsx
@@ -1,8 +1,8 @@
-import TripEvent from "@/components/trip-event";
 import { Jam, Team, TeamJam } from "@/types/game";
-import { Button, Group, ScrollArea } from "@mantine/core";
+import { Stack } from "@mantine/core";
 import { useEffect, useRef } from "react";
 import AddTripButtons from "./add-trip-buttons";
+import TripEventView from "./trip-event-view";
 
 interface TeamJamViewProps {
   jam: Jam;
@@ -29,71 +29,9 @@ export default function TeamJamView({ jam, team }: TeamJamViewProps) {
   }, [teamJam.events.length]);
 
   return (
-    <>
+    <Stack>
       <AddTripButtons jam={jam} team={team} />
-
-      <Group gap={0} justify="center" wrap="nowrap">
-        <Button p={0} pr={1} variant="subtle" h={60} w={30}>
-          <ChevronLeft />
-        </Button>
-        <ScrollArea.Autosize
-          h={60}
-          w={200}
-          viewportRef={viewport}
-          scrollbars={false}
-        >
-          <Group gap={0} preventGrowOverflow={false} wrap="nowrap">
-            {teamJam.events
-              .filter((event) => event.passes != null)
-              .map((teamJam, i) => (
-                <TripEvent key={i} tripNum={i} {...teamJam} />
-              ))}
-          </Group>
-        </ScrollArea.Autosize>
-        <Button p={0} pl={1} variant="subtle" h={60} w={30}>
-          <ChevronRight />
-        </Button>
-      </Group>
-    </>
-  );
-}
-
-function ChevronLeft() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon-tabler-chevron-left icons-tabler-outline icon icon-tabler"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M15 6l-6 6l6 6" />
-    </svg>
-  );
-}
-
-function ChevronRight() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon-tabler-chevron-right icons-tabler-outline icon icon-tabler"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M9 6l6 6l-6 6" />
-    </svg>
+      <TripEventView jam={jam} team={team} />
+    </Stack>
   );
 }
---
+++ frontend/features/team-jam-view/trip-event-view.tsx
@@ -0,0 +1,94 @@
+import TripEvent from "@/components/trip-event";
+import { Jam, Team, TeamJam } from "@/types/game";
+import { Button, Group, ScrollArea } from "@mantine/core";
+import { useEffect, useRef } from "react";
+
+interface TripEventViewProps {
+  jam: Jam;
+  team: Team;
+}
+
+export default function TripEventView({ jam, team }: TripEventViewProps) {
+  const teamJam: TeamJam | undefined = jam.teamJams.find(
+    (teamJam: TeamJam) => teamJam.teamId === team.id,
+  );
+  if (teamJam == undefined) {
+    throw new Error("team jam not found");
+  }
+  const viewport = useRef<HTMLDivElement>(null);
+  const isHydrated = useRef<boolean>(false);
+
+  useEffect(() => {
+    const behavior = isHydrated.current ? "smooth" : "instant";
+    viewport.current!.scrollTo({
+      left: viewport.current!.scrollWidth,
+      behavior,
+    });
+    isHydrated.current = true;
+  }, [teamJam.events.length]);
+
+  return (
+    <Group gap={0} justify="center" wrap="nowrap">
+      <Button p={0} pr={1} variant="subtle" h={60} w={30}>
+        <ChevronLeft />
+      </Button>
+      <ScrollArea.Autosize
+        h={60}
+        w={200}
+        viewportRef={viewport}
+        scrollbars={false}
+      >
+        <Group gap={0} preventGrowOverflow={false} wrap="nowrap">
+          {teamJam.events
+            .filter((event) => event.passes != null)
+            .map((teamJam, i) => (
+              <TripEvent key={i} tripNum={i} {...teamJam} />
+            ))}
+        </Group>
+      </ScrollArea.Autosize>
+      <Button p={0} pl={1} variant="subtle" h={60} w={30}>
+        <ChevronRight />
+      </Button>
+    </Group>
+  );
+}
+
+function ChevronLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon-tabler-chevron-left icons-tabler-outline icon icon-tabler"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M15 6l-6 6l6 6" />
+    </svg>
+  );
+}
+
+function ChevronRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon-tabler-chevron-right icons-tabler-outline icon icon-tabler"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 6l6 6l-6 6" />
+    </svg>
+  );
+}
