2fcc87d74cb21febb1616ffb869f20aa7fb51e27
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-08 20:02:29 -0800

    add contexts

---
+++ frontend/utils/contexts.ts
@@ -0,0 +1,6 @@
+import { Bout, Jam } from "@/types/game";
+import { createContext } from "react";
+
+export const BoutContext = createContext<Bout | null>(null);
+
+export const JamContext = createContext<Jam | null>(null);
