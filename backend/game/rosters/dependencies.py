c6d4bf80e1fe7b4b97f05503eec48d95039cc444
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-18 19:44:03 -0800

    remove now unused method

--- backend/game/rosters/dependencies.py
+++ backend/game/rosters/dependencies.py
@@ -1,4 +1,3 @@
-from collections.abc import Sequence
 from typing import Annotated, TypeAlias
 
 from database import AsyncSessionDepends
@@ -6,17 +5,6 @@ from fastapi import Depends, Query
 from sqlalchemy import Result, select
 
 from .models import Roster
-
-
-async def get_rosters(
-    session: AsyncSessionDepends,
-    roster_ids: Annotated[list[int], Query(alias='rosterId')],
-) -> Sequence[Roster]:
-    results: Result[tuple[Roster]] = await session.execute(
-        select(Roster).where(Roster.id.in_(roster_ids))
-    )
-    rosters: Sequence[Roster] = results.scalars().all()
-    return rosters
 
 
 async def get_roster(
@@ -29,7 +17,5 @@ async def get_roster(
     roster: Roster = results.scalar_one()
     return roster
 
-
-RosterDepends: TypeAlias = Annotated[Sequence[Roster], Depends(get_rosters)]
 
 GetRoster: TypeAlias = Annotated[Roster, Depends(get_roster)]
