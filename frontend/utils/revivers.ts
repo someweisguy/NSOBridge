61e5bbb2a332b029185d84b0654fcb01449faf6b
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-22 12:19:41 -0800

    add regex to ensure only ISO8601 strings are revived as Date objects

--- frontend/utils/revivers.ts
+++ frontend/utils/revivers.ts
@@ -1,5 +1,9 @@
+const dateTimeExpression = new RegExp(
+  /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/,
+);
+
 export function dateReviver<T>(_: string, value: T): T | Date {
-  if (typeof value === "string") {
+  if (typeof value === "string" && dateTimeExpression.test(value)) {
     const date: Date = new Date(value);
     if (!isNaN(date.getTime())) {
       return date;
