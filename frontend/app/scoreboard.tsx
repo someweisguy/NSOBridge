20a45ae92dce971d6000b86e3499e4b1ef283c39
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-23 14:12:39 -0800

    update component args

--- frontend/app/scoreboard.tsx
+++ frontend/app/scoreboard.tsx
@@ -1,12 +1,15 @@
 import BoutStateView from "@/components/bout-state-view";
 import TeamView from "@/features/team-view/team-view";
 import {
+  useActiveOrLatestJamIndex,
+  useActiveOrLatestTimeoutIndex,
   useBout,
-  useLatestJamIndex,
-  useLatestTimeoutIndex,
+  usePrefetchBoutData,
 } from "@/hooks/use-bout";
 import { useJam } from "@/hooks/use-jam";
+import { useRuleset } from "@/hooks/use-ruleset";
 import { useSeries } from "@/hooks/use-series";
+import { usePrefetchServerTime } from "@/hooks/use-server-time";
 import { useTimeout } from "@/hooks/use-timeout";
 import queryClient from "@/lib/cache";
 import { Team } from "@/lib/game/bouts";
@@ -35,25 +38,44 @@ export default function App() {
 }
 
 function Scoreboard() {
+  usePrefetchServerTime();
+
   const { data: series } = useSeries(0);
-  const { data: bout } = useBout(series, 0);
-  const { data: activeJam } = useJam(bout, ...useLatestJamIndex(bout));
-  const { data: latestTimeout } = useTimeout(bout, useLatestTimeoutIndex(bout));
+  const { data: bout } = useBout(series);
+  usePrefetchBoutData(bout);
+
+  const { data: ruleset } = useRuleset(bout);
+
+  // Fetch Jam data
+  const jamIndex = useActiveOrLatestJamIndex(bout);
+  const [periodNum, jamNum] = jamIndex;
+  const { data: jam } = useJam(bout, periodNum, jamNum);
+
+  // Fetch Timeout data
+  const timeoutIndex = useActiveOrLatestTimeoutIndex(bout);
+  const { data: timeout } = useTimeout(bout, timeoutIndex);
 
   return (
     <div className="flex flex-col flex-nowrap grid-flow-row h-screen size-screen">
       <div className="justify-stretch items-stretch grid grid-flow-col basis-1/2 shrink-0 grow-0">
         {/* Primary Information (Team Info) */}
         {bout.teams.map((team: Team, i: number) => (
-          <TeamView key={i} bout={bout} team={team} timeout={latestTimeout} />
+          <TeamView
+            key={i}
+            bout={bout}
+            team={team}
+            timeout={timeout}
+            ruleset={ruleset}
+          />
         ))}
       </div>
       <div className="justify-stretch items-stretch grid basis-1/2 shrink-0">
         {/* Secondary Information (Clocks, etc.) */}
         <BoutStateView
           bout={bout}
-          activeJam={activeJam}
-          latestTimeout={latestTimeout}
+          activeJam={jam}
+          latestTimeout={timeout}
+          ruleset={ruleset}
         />
       </div>
     </div>
