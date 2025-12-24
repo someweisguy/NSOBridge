1a8bbeb16f53faa081ed6eb7a68d3ae5d857e434
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-24 15:09:22 -0800

    rough in class-based db connection

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import os
+from pathlib import Path
 from typing import TYPE_CHECKING, Annotated, Final, Protocol, TypeAlias
 
 from fastapi import Depends
@@ -15,30 +15,44 @@ if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
 
 
-DATABASE: Final[str] = os.environ.get('DB_PATH', ':memory:')
-DEBUG: Final[bool] = os.environ.get('SQLALCHEMY_DEBUG', 'false').lower() in {
-    'true',
-    'yes',
-}
-
-engine: Final[AsyncEngine] = create_async_engine(
-    f'sqlite+aiosqlite:///{DATABASE}',
-    echo=DEBUG,
-)
-session_factory: Final[async_sessionmaker[AsyncSession]] = async_sessionmaker(
-    bind=engine,
-    expire_on_commit=False,
-)
-
-
 class Memento(Protocol):
     async def restore(self) -> Memento: ...
 
 
-async def _get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with session_factory() as session, session.begin():
-        yield session
-        await session.commit()
+class Database:
+    PROTOCOL: Final[str] = 'sqlite+aiosqlite:///'
+
+    def __init__(
+        self, db_path: Path | str = Path(':memory:'), debug: bool = False
+    ) -> None:
+        url: str = Database.PROTOCOL + str(db_path)
+        self.engine: AsyncEngine = create_async_engine(url, echo=debug)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, expire_on_commit=False
+        )
+        self.connected: bool = True
+
+    async def get_async_session(self) -> AsyncGenerator[AsyncSession, None]:
+        if not self.connected:
+            raise RuntimeError('This database connection is not open')
+        async with self.session_factory() as session, session.begin():
+            yield session
+            await session.commit()
+
+    async def dispose(self) -> None:
+        if not self.connected:
+            return
+        await self.engine.dispose()
+        self.connected = False
 
 
-AsyncSessionDepends: TypeAlias = Annotated[AsyncSession, Depends(_get_async_session)]
+_db: Database | None = None
+
+
+def get_db() -> Database:
+    if _db is None:
+        raise RuntimeError('A Database connection has not yet been established')
+    return _db
+
+
+AsyncSessionDepends: TypeAlias = Annotated[AsyncSession, Depends(_db.get_async_session)]
