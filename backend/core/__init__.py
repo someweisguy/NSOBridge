2d002e69f97e1d2b3c44ff0f6b88f41ce6aa7fb3
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-24 23:39:10 -0800

    move core app construction to module

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,1 +1,67 @@
+import logging
+import os
+from pathlib import Path
+from typing import Final, LiteralString
 
+import ws
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.routing import Mount
+from fastapi.staticfiles import StaticFiles
+from uvicorn import Config, Server
+
+from core.exceptions import RulesError
+
+logging.basicConfig(
+    format='{levelname}: {message}',
+    datefmt='%m/%d/%Y %H:%M:%S',
+    style='{',
+    level=logging.INFO,
+)
+
+
+# Initialize the application and set the appropriate routes
+FRONTEND: Final[Path] = Path(os.environ['VITE_BUILD_DIR'])
+app: FastAPI = FastAPI(
+    debug=True,
+    routes=[
+        Mount('/assets', StaticFiles(directory=FRONTEND / 'assets')),
+        Mount('/ws', ws.app),
+    ],
+)
+API_PREFIX: LiteralString = '/api'
+
+
+@app.get('/')
+async def render_index() -> FileResponse:
+    return FileResponse(FRONTEND / 'index.html')
+
+
+@app.get('/sb')
+async def render_generic(request: Request) -> FileResponse:
+    return FileResponse(FRONTEND / (request.url.path[1:] + '.html'))
+
+
+@app.exception_handler(RulesError)
+async def rules_error_handler(request: Request, e: RulesError) -> JSONResponse:
+    return JSONResponse(
+        status_code=409,
+        content={'message': str(e)},
+    )
+
+
+async def run(host: str = '0.0.0.0', port: int = 8000) -> None:
+    # Configure the server
+    server: Server = Server(
+        Config(
+            app,
+            host=host,
+            port=port,
+            log_config=None,
+            access_log=False,
+            log_level='warning',
+            server_header=False,
+        )
+    )
+
+    await server.serve()
--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,17 @@
 import asyncio
 import logging
 import os
-from pathlib import Path
 from socket import AF_INET, SOCK_DGRAM, socket
-from typing import TYPE_CHECKING, Final, LiteralString
+from typing import TYPE_CHECKING, Final
 
-import ws
-from core.exceptions import RulesError
+import core
 from core.models import Database
-from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse, JSONResponse
-from fastapi.routing import Mount
-from fastapi.staticfiles import StaticFiles
-from game import ROUTERS as GAME_ROUTERS
+from game import ROUTERS as game_routers
 from game.rosters.models import Roster
 from game.rulesets.wftda_2025 import Bout
 from game.series.models import Series
 from sqlalchemy import Result, Select, select
 from users.router import router as user_router
-from uvicorn import Config, Server
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import async_sessionmaker
@@ -32,40 +25,7 @@ logging.basicConfig(
 )
 
 
-# Initialize the application and set the appropriate routes
-PORT: Final[int] = int(os.environ.get('UVICORN_PORT', str(8000)))
-FRONTEND: Final[Path] = Path(os.environ['VITE_BUILD_DIR'])
-app: FastAPI = FastAPI(
-    debug=True,
-    routes=[
-        Mount('/assets', StaticFiles(directory=FRONTEND / 'assets')),
-        Mount('/ws', ws.app),
-    ],
-)
-API_PREFIX: LiteralString = '/api'
-for router in [*GAME_ROUTERS, user_router]:
-    app.include_router(router, prefix=API_PREFIX)
-
-
-@app.get('/')
-async def render_index() -> FileResponse:
-    return FileResponse(FRONTEND / 'index.html')
-
-
-@app.get('/sb')
-async def render_generic(request: Request) -> FileResponse:
-    return FileResponse(FRONTEND / (request.url.path[1:] + '.html'))
-
-
-@app.exception_handler(RulesError)
-async def rules_error_handler(request: Request, e: RulesError) -> JSONResponse:
-    return JSONResponse(
-        status_code=409,
-        content={'message': str(e)},
-    )
-
-
-async def main(host: str = '0.0.0.0', port: int = 8000) -> None:
+async def main(*, host: str = '0.0.0.0', port: int = 8000) -> None:
     # Create a Bout model if one does not already exist
     bout: Bout | None = None
     session_factory: async_sessionmaker = await Database.get_async_session_factory()
@@ -82,18 +42,9 @@ async def main(host: str = '0.0.0.0', port: int = 8000) -> None:
             session.add(bout)
         await session.commit()
 
-    # Configure the server
-    server: Server = Server(
-        Config(
-            app,
-            host=host,
-            port=port,
-            log_config=None,
-            access_log=False,
-            log_level='warning',
-            server_header=False,
-        )
-    )
+    # Load the server API
+    for router in [*game_routers, user_router]:
+        core.app.include_router(router, prefix='/api')
 
     # Log the server's address and serve the application
     try:
@@ -104,8 +55,11 @@ async def main(host: str = '0.0.0.0', port: int = 8000) -> None:
         ip = '127.0.0.1'
     HTTP_PORT: Final[int] = 80
     print(f'Starting server at http://{ip}{f":{PORT}" if PORT != HTTP_PORT else ""}')
-    await server.serve()
+    await core.run()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    HOST: Final[str] = os.environ.get('UVICORN_HOST', '0.0.0.0')
+    PORT: Final[int] = int(os.environ.get('UVICORN_PORT', str(8000)))
+
+    asyncio.run(main(host=HOST, port=PORT))
