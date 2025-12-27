0e8a9132098244095fc8a0ddbb4325dd8523047f
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-27 11:27:18 -0800

    documentation

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -2,4 +2,10 @@
 
 
 class ClientError(Exception):
+    """The base exception for client errors.
+
+    This exception should be thrown when an error originating from client input occurs.
+    These types of exception should almost always be recoverable.
+    """
+
     pass
