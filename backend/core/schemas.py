cfa89867907c8149c2bb21da4f70e3959721efb5
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-27 11:34:46 -0800

    documentation

--- backend/core/schemas.py
+++ backend/core/schemas.py
@@ -10,6 +10,13 @@ from .utils import _timedelta_encoder
 
 
 class ServerSchema(BaseModel):
+    """The base schema for schemas which originate from this server.
+
+    This is the base schema for all schemas that this server generates. It automatically
+    converts all snake_case attributes, the Python standard, to camelCase, the
+    Javascript and Typescript standard.
+    """
+
     model_config: ClassVar[ConfigDict] = ConfigDict(
         alias_generator=to_camel,
         from_attributes=True,
@@ -20,6 +27,15 @@ class ServerSchema(BaseModel):
 
 
 class ClientSchema(BaseModel):
+    """The base schema for schemas which originate from outside of this server.
+
+    This is the base schema for all schemas which are generated from outside of this
+    server. It automatically converst all camelCase attributes, the Javascript and
+    Typescript standard, to snake_case, the Python standard. This schema also forbids
+    extra arguments, raising exceptions when additional fields are provided.
+
+    """
+
     model_config: ClassVar[ConfigDict] = ConfigDict(
         alias_generator=to_camel,
         extra='forbid',
