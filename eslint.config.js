92bb8a3e663037a9e9d8e8dd67e53440c1858fdc
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-30 10:11:38 -0800

    move tsconfig files to frontend

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,10 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        project: [
+          "./frontend/tsconfig.node.json",
+          "./frontend/tsconfig.app.json",
+        ],
         tsconfigRootDir: import.meta.dirname,
       },
     },
