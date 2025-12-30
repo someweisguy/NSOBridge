cee907a6769d086c557f1a76035858652b151df7
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-29 18:35:31 -0800

    add recursive parent fetcher

--- backend/core/models.py
+++ backend/core/models.py
@@ -28,14 +28,27 @@ class BaseSQLModel(AsyncAttrs, DeclarativeBase):
     __type_annotation_map__: dict = {timedelta: _TimedeltaAsMilliseconds}
 
     async def get_parents(self) -> tuple[BaseSQLModel, ...]:
-        """Asynchronously get a tuple of this model's parents.
-
-        This method is used to get the hierarchical branch of models that this model
-        is on. This is useful to ensure that clients can refresh objects that have
-        updated.
+        """Asynchronously get a tuple of this model's direct parents.
 
         Returns:
             tuple[BaseSQLModel]: the immediate parents of this model.
 
         """
         ...
+
+    async def get_recursive_parents(self) -> tuple[BaseSQLModel, ...]:
+        """Recursively and asynchronously get a tuple of this model's parents.
+
+        This method is used to get the hierarchical branch of models that this model
+        is on. This is useful to ensure that clients can refresh objects that have
+        updated.
+
+        Returns:
+            tuple[BaseSQLModel]: the recursive parents of this model.
+
+        """
+        recursive_parents: list[BaseSQLModel] = list(await self.get_parents())
+        for parent in await self.get_parents():
+            if isinstance(parent, BaseSQLModel):
+                recursive_parents.extend(await parent.get_recursive_parents())
+        return tuple(recursive_parents)
