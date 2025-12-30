68ad3a1b1e232f7df1aed6654d5e372c5e6ac599
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-30 12:19:53 -0800

    method docstrings

--- backend/game/series/schemas.py
+++ backend/game/series/schemas.py
@@ -15,9 +15,15 @@ class SeriesSchema(ServerSchema):
     @computed_field
     @property
     def bout_ids(self) -> list[int]:
+        """Get a list representing the IDs of this Series' Bouts."""
         return [bout.id for bout in self.bouts]
 
     @computed_field
     @property
     def active_bout_id(self) -> int | None:
+        """The active Bout ID of this Series or None if there is no active Bout.
+
+        The active Bout is the first Bout in the Series which is not final.
+
+        """
         return next((bout.id for bout in self.bouts if not bout.is_final), None)
