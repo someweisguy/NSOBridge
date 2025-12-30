a46556a0cabf4eabf6aa73d2e06516a5b2fe7d28
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-29 21:58:59 -0800

    rename method

--- backend/game/bouts/router.py
+++ backend/game/bouts/router.py
@@ -18,7 +18,7 @@ router.add_api_route('', _get_bout, response_model=BoutSchema, tags=[BOUTS_TAG])
 
 
 @router.get('/ruleset', response_model=Ruleset, tags=[BOUTS_TAG])
-async def get_ruleset_context(bout: BoutDepends) -> Ruleset:
+async def _get_ruleset(bout: BoutDepends) -> Ruleset:
     return bout.rules
 
 
