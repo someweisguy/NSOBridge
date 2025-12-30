e4fdcedb4b18f8325734442377b6051ae0255540
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-29 18:36:50 -0800

    use recursive parents

--- backend/ws/service.py
+++ backend/ws/service.py
@@ -97,7 +97,7 @@ def invalidate_queries(models: Iterable[BaseSQLModel]) -> None:
             for model in models:
                 cacheables |= {
                     parent
-                    for parent in await model.get_parents()
+                    for parent in await model.get_recursive_parents()
                     if isinstance(parent, CacheableSQLModel)
                 }
             cache_keys: list[CacheKey] = [
