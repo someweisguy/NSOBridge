e9ec12c74ee87f1e093c2d89874a3dea13f4bfa9
Mitch Weisbrod <mitch@theweisbrods.com>
2025-12-30 23:39:14 -0800

    basic undo/redo logs

--- backend/user/router.py
+++ backend/user/router.py
@@ -1,5 +1,6 @@
 """Endpoints for user commands such as undo and redo."""
 
+import logging
 from typing import Final
 
 from fastapi import APIRouter
@@ -14,10 +15,12 @@ router: Final[APIRouter] = APIRouter()
 @router.post('/undo', tags=[HISTORY_TAG])
 async def _undo(user: GetUser) -> None:
     """Undo the last command that this user executed."""
+    logging.info('User called undo')
     await user.undo()
 
 
 @router.post('/redo', tags=[HISTORY_TAG])
 async def _redo(user: GetUser) -> None:
     """Redo the last command that this user executed."""
+    logging.info('User called redo')
     await user.redo()
