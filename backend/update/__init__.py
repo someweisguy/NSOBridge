e3f32afc84cf782ff6d9b03bb9a0106308cc369a
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-03 16:25:37 -0800

    documentation

--- backend/update/__init__.py
+++ backend/update/__init__.py
@@ -2,7 +2,10 @@
 
 Updating checking is a convenience feature for users. It is likely that checking for
 updates will fail most of the time since most users are not likely to be connected to
-the internet while using this app.
+the internet while using this app. This is to be expected.
+
+During the few times that this app is run with an internet connection it should be made
+easy for users to update to the latest version of the app.
 """
 
 from .schemas import GithubReleaseSchema
