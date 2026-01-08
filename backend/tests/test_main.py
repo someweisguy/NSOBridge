3560bdd9e25b824b02fa89868cd94045823a76b0
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-07 21:13:57 -0800

    ensure app version is semver

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,6 +1,12 @@
 """Main tests."""
 
 from fastapi.testclient import TestClient
+from semver import VersionInfo
 from src.main import app
 
 client = TestClient(app)
+
+
+def test_app_version_is_semver():
+    """Ensures that the app version is a valid SemVer string."""
+    assert VersionInfo.is_valid(app.version)
