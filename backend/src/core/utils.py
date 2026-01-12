c21f0da34f3d6a0b40f9a79c88264f083d260903
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-11 19:01:53 -0800

    fix resource path for non-bundled version

--- backend/src/core/utils.py
+++ backend/src/core/utils.py
@@ -88,7 +88,5 @@ def get_resource_path(relative_path: LiteralString) -> Path:
         Path: a path to the resource.
 
     """
-    base_path: str = getattr(
-        sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__))
-    )
+    base_path: str | Path = getattr(sys, '_MEIPASS', Path.cwd())
     return Path(os.path.join(base_path, relative_path))
