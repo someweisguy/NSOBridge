6bd012d259d326db0d49ad72c76af13bd9aa84a7
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-13 22:59:19 -0800

    improve requirements for tag naming conventions

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -16,15 +16,18 @@ def test_app_version_is_semver():
 def test_app_version_matches_tag(request: pytest.FixtureRequest):
     """Ensure that the git tag name matches the app version.
 
+    Git tag names should begin with a single, lowercase 'v' and be followed by a semver
+    identifier.
+
     Args:
         request (pytest.FixtureRequest): the request to run pytest. Must contain the git
         tag name as '--tag-name' or this test will be skipped.
 
     """
-    git_tag_name: str = request.config.getoption('--tag-name')
+    git_tag_name: str | None = request.config.getoption('--tag-name', None)
     if git_tag_name is None:
-        pytest.skip('Skipping because a git tag name was not provided')
-    if git_tag_name.startswith('v'):
-        git_tag_name = git_tag_name[1:]
+        pytest.skip('A git tag name was not provided')
+    assert git_tag_name.startswith('v')
+    git_tag_name = git_tag_name[1:]
     assert VersionInfo.is_valid(git_tag_name)
     assert git_tag_name == str(app.version)
