c68af21368c74afff525819584d33f387556471c
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-13 22:45:09 -0800

    update app version to test github workflow

--- backend/src/main.py
+++ backend/src/main.py
@@ -26,7 +26,7 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 
-APP_VERSION_INFO: Final[VersionInfo] = VersionInfo(0, 1, 0)
+APP_VERSION_INFO: Final[VersionInfo] = VersionInfo(0, 1, 1)
 CONFIG_FILE_NAME: Path = core.get_resource_path('./config.ini')
 LOG_DIR_NAME: Path = core.get_resource_path('./logs')
 API_PREFIX: str = '/api'
