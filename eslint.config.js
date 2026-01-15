5ac514aa754743496ac1764aed8626f0947110fa
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-14 19:55:16 -0800

    add lint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: [".venv", "dist", "www"] },
   {
     extends: [
       js.configs.recommended,
