e7aedde56b310daa8d996c02be1c5a28b05f0c2f
Mitch Weisbrod <mitch@theweisbrods.com>
2026-01-27 20:41:41 -0800

    use bout getter strategy

--- frontend/app/scoreboard.tsx
+++ frontend/app/scoreboard.tsx
@@ -1,17 +1,19 @@
 import BoutStateView from "@/components/bout-state-view";
 import TeamView from "@/features/team-view/team-view";
-import { useSuspenseAllBouts, useSuspenseBout } from "@/hooks/use-bout";
+import { useSuspenseBout } from "@/hooks/use-bout";
 import { useJam, useSuspenseJam } from "@/hooks/use-jam";
 import { useSuspenseRuleset } from "@/hooks/use-ruleset";
+import { useSuspenseAllSeries } from "@/hooks/use-series";
 import { usePrefetchServerTime } from "@/hooks/use-server-time";
 import { useSuspenseTimeout } from "@/hooks/use-timeout";
 import queryClient from "@/lib/cache";
-import { Bout, Team } from "@/lib/game/bouts";
+import { Team } from "@/lib/game/bouts";
+import { Series } from "@/lib/game/series";
 import FitScreen from "@fit-screen/react";
 import { Center, Grid, MantineProvider, Stack } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode, Suspense, useEffect } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 
@@ -37,15 +39,12 @@ export default function App() {
 function Scoreboard() {
   usePrefetchServerTime();
 
-  const { data: allBouts } = useSuspenseAllBouts();
+  const { data: allSeries } = useSuspenseAllSeries();
 
-  useEffect(() => {
-    for (const bout of allBouts) {
-      queryClient.setQueryData(Bout.generateKey(bout.uuid), bout);
-    }
-  }, [allBouts]);
-
-  const { data: bout } = useSuspenseBout(allBouts[0].uuid);
+  const series: Series = allSeries[0];
+  const { data: bout } = useSuspenseBout(
+    series.boutUuids[series.activeBoutIndex ?? series.boutUuids.length - 1],
+  );
 
   // Eagerly query the latest Jam and Timeout to avoid suspending
   void useJam(bout, ...bout.getLatestJamNum());
