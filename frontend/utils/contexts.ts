04ef06da31857e618b628c56912b670f070647ac
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-01 17:14:51 -0800

    remove ruleset context

--- frontend/utils/contexts.ts
+++ frontend/utils/contexts.ts
@@ -1,10 +1,7 @@
 import { Bout } from "@/lib/game/bouts";
 import { Jam } from "@/lib/game/jams";
-import { Ruleset } from "@/lib/game/ruleset";
 import { createContext } from "react";
 
 export const BoutContext = createContext<Bout | null>(null);
 
 export const JamContext = createContext<Jam | null>(null);
-
-export const RulesetContext = createContext<Ruleset | null>(null);
