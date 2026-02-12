ad7dba26fef1fe795efb66ee3b848f6be37aa769
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-11 19:04:27 -0800

    split code for easier readability

--- frontend/app/main.tsx
+++ frontend/app/main.tsx
@@ -1,35 +1,17 @@
 import BoutPicker from "@/components/bout-picker";
-import BoutProvider from "@/components/bout-provider";
-import JamProvider from "@/components/jam-provider";
-import TeamProvider from "@/components/team-provider";
-import BoutControlButtons from "@/features/bout-control/bout-control-buttons";
-import PrimaryBoutStatus from "@/features/bout/components/primary-bout-status";
-import { SecondaryBoutStatus } from "@/features/bout/components/secondary-bout-status";
-import TeamJamView from "@/features/team-jam-vew/team-jam-view";
-import TeamBoutScore from "@/features/team/components/bout-score";
-import TeamJamScore from "@/features/team/components/jam-score";
-import TeamName from "@/features/team/components/team-name";
-import TeamTimeoutBar from "@/features/team/components/timeout-bar";
-import { useSuspenseAllBouts, useSuspenseBout } from "@/hooks/use-bout";
-import { usePrefetchServerTime } from "@/hooks/use-server-time";
+import { useSuspenseAllBouts } from "@/hooks/use-bout";
 import queryClient from "@/lib/cache";
-import { Team } from "@/lib/game/bouts";
 import { redo, undo } from "@/lib/history";
-import {
-  AppShell,
-  Burger,
-  Flex,
-  MantineProvider,
-  SimpleGrid,
-  Stack,
-} from "@mantine/core";
+import { AppShell, Burger, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { useDisclosure } from "@mantine/hooks";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode, Suspense, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
+import Operator from "./operator";
 
+// Register Ctrl+Z and Ctrl+Y as undo and redo respectively
 document.addEventListener("keydown", (event) => {
   if (event.ctrlKey) {
     if (event.key === "z") {
@@ -44,7 +26,7 @@ document.addEventListener("keydown", (event) => {
 const root: HTMLElement = document.getElementById("root")!;
 createRoot(root).render(<App />);
 
-export default function App() {
+export function App() {
   const [opened, { toggle }] = useDisclosure();
 
   const { data: bouts } = useSuspenseAllBouts();
@@ -79,82 +61,12 @@ export default function App() {
 
             <AppShell.Main>
               <Suspense fallback={"Loading..."}>
-                <Main boutUuid={boutUuid} />
+                <Operator boutUuid={boutUuid} />
               </Suspense>
             </AppShell.Main>
           </AppShell>
         </QueryClientProvider>
       </MantineProvider>
     </StrictMode>
-  );
-}
-
-function Main({ boutUuid }: { boutUuid: string }) {
-  usePrefetchServerTime();
-
-  const { data: bout } = useSuspenseBout(boutUuid);
-  const [activePeriodNum, activeJamNum] = bout.getActiveOrLatestJamNum();
-
-  return (
-    <BoutProvider bout={bout}>
-      <Stack align="stretch" justify="flex-start">
-        {/* Bout State control */}
-        <BoutControlButtons bout={bout} />
-
-        {/* Team information */}
-        <SimpleGrid cols={bout.teams.length}>
-          {bout.teams.map((team: Team, i: number) => (
-            <TeamProvider key={i} team={team}>
-              <Stack justify="center">
-                <TeamName ta="center" fw="bolder" size="36pt" />
-                <Flex
-                  direction={i % 2 ? "row-reverse" : "row"}
-                  align="center"
-                  justify="center"
-                  gap="md"
-                >
-                  <TeamTimeoutBar size={24} />
-                  <TeamBoutScore fw="bold" w={150} ta="center" size="48pt" />
-                  <TeamJamScore
-                    ta={i % 2 ? "right" : "left"}
-                    size="24pt"
-                    w={50}
-                  />
-                </Flex>
-              </Stack>
-            </TeamProvider>
-          ))}
-        </SimpleGrid>
-
-        {/* Bout State View */}
-        <SimpleGrid>
-          <PrimaryBoutStatus
-            withClock
-            justify="center"
-            align="center"
-            size="36pt"
-          />
-          <SecondaryBoutStatus withClock ta="center" size="24pt" />
-        </SimpleGrid>
-
-        {/* TeamJam score editors */}
-        <Suspense fallback={"Loading..."}>
-          <JamProvider
-            bout={bout}
-            periodNum={activePeriodNum}
-            jamNum={activeJamNum}
-          >
-            <SimpleGrid cols={bout.teams.length}>
-              {bout.teams.map((team: Team, i: number) => (
-                <TeamJamView key={i} bout={bout} team={team} />
-              ))}
-            </SimpleGrid>
-          </JamProvider>
-        </Suspense>
-
-        {/* Lineup editors */}
-        {/* TODO */}
-      </Stack>
-    </BoutProvider>
   );
 }
---
+++ frontend/app/operator.tsx
@@ -0,0 +1,87 @@
+import BoutProvider from "@/components/bout-provider";
+import JamProvider from "@/components/jam-provider";
+import TeamProvider from "@/components/team-provider";
+import BoutControlButtons from "@/features/bout-control/bout-control-buttons";
+import PrimaryBoutStatus from "@/features/bout/components/primary-bout-status";
+import { SecondaryBoutStatus } from "@/features/bout/components/secondary-bout-status";
+import TeamJamView from "@/features/team-jam-vew/team-jam-view";
+import TeamBoutScore from "@/features/team/components/bout-score";
+import TeamJamScore from "@/features/team/components/jam-score";
+import TeamName from "@/features/team/components/team-name";
+import TeamTimeoutBar from "@/features/team/components/timeout-bar";
+import { useSuspenseBout } from "@/hooks/use-bout";
+import { usePrefetchServerTime } from "@/hooks/use-server-time";
+import { Team } from "@/lib/game/bouts";
+import { Flex, SimpleGrid, Stack } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { Suspense } from "react";
+
+export default function Operator({ boutUuid }: { boutUuid: string }) {
+  usePrefetchServerTime();
+
+  const { data: bout } = useSuspenseBout(boutUuid);
+  const [activePeriodNum, activeJamNum] = bout.getActiveOrLatestJamNum();
+
+  return (
+    <BoutProvider bout={bout}>
+      <Stack align="stretch" justify="flex-start">
+        {/* Bout State control */}
+        <BoutControlButtons bout={bout} />
+
+        {/* Team information */}
+        <SimpleGrid cols={bout.teams.length}>
+          {bout.teams.map((team: Team, i: number) => (
+            <TeamProvider key={i} team={team}>
+              <Stack justify="center">
+                <TeamName ta="center" fw="bolder" size="36pt" />
+                <Flex
+                  direction={i % 2 ? "row-reverse" : "row"}
+                  align="center"
+                  justify="center"
+                  gap="md"
+                >
+                  <TeamTimeoutBar size={24} />
+                  <TeamBoutScore fw="bold" w={150} ta="center" size="48pt" />
+                  <TeamJamScore
+                    ta={i % 2 ? "right" : "left"}
+                    size="24pt"
+                    w={50}
+                  />
+                </Flex>
+              </Stack>
+            </TeamProvider>
+          ))}
+        </SimpleGrid>
+
+        {/* Bout State View */}
+        <SimpleGrid>
+          <PrimaryBoutStatus
+            withClock
+            justify="center"
+            align="center"
+            size="36pt"
+          />
+          <SecondaryBoutStatus withClock ta="center" size="24pt" />
+        </SimpleGrid>
+
+        {/* TeamJam score editors */}
+        <Suspense fallback={"Loading..."}>
+          <JamProvider
+            bout={bout}
+            periodNum={activePeriodNum}
+            jamNum={activeJamNum}
+          >
+            <SimpleGrid cols={bout.teams.length}>
+              {bout.teams.map((team: Team, i: number) => (
+                <TeamJamView key={i} bout={bout} team={team} />
+              ))}
+            </SimpleGrid>
+          </JamProvider>
+        </Suspense>
+
+        {/* Lineup editors */}
+        {/* TODO */}
+      </Stack>
+    </BoutProvider>
+  );
+}
