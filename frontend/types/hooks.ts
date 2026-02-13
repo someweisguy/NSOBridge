62dd3c979c667003e7939575a541d96c45d4ae29
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-12 21:04:29 -0800

    add type to facilitate dry code

---
+++ frontend/types/hooks.ts
@@ -0,0 +1,11 @@
+import {
+  UseQueryOptions,
+  UseSuspenseQueryOptions,
+} from "@tanstack/react-query";
+
+export type SuspenseQueryOptions<T> = Omit<
+  UseSuspenseQueryOptions<T>,
+  "queryKey" | "queryFn"
+>;
+
+export type QueryOptions<T> = Omit<UseQueryOptions<T>, "queryKey" | "queryFn">;
