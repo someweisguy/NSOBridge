be3a57dc8e752e0e78f3b67ef0295cd175a2db98
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-15 12:39:15 -0800

    normalize scroll amounts

--- frontend/features/jams/components/trip-history.tsx
+++ frontend/features/jams/components/trip-history.tsx
@@ -3,46 +3,60 @@ import { TeamJam } from "@/lib/game/jams";
 import { TeamJamContext } from "@/utils/contexts";
 import { Button, Group, ScrollArea } from "@mantine/core";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 
 interface TripEventViewProps {
-  scrollWidth?: number;
+  w?: number;
 }
 
-function scrollLeft(scrollArea: HTMLDivElement) {
-  scrollArea.scrollBy({ left: -50.5, behavior: "smooth" });
-}
-
-function scrollRight(scrollArea: HTMLDivElement) {
-  scrollArea.scrollBy({ left: 50.5, behavior: "smooth" });
-}
-
-export default function TeamJamTripHistory({
-  scrollWidth = 200,
-}: TripEventViewProps) {
+export default function TeamJamTripHistory({ w = 200 }: TripEventViewProps) {
   const teamJam: TeamJam | null = useContext(TeamJamContext);
   if (teamJam == null) {
     throw new Error("TeamJamTripHistory must be used within a TeamJamProvider");
   }
 
-  const viewport = useRef<HTMLDivElement>(null);
-  const isHydrated = useRef<boolean>(false);
-  const [scrollPosition, onScrollPositionChange] = useState({ x: 0, y: 0 });
+  const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 });
+  const viewportRef = useRef<HTMLDivElement>(null);
+  const groupRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const behavior = isHydrated.current ? "smooth" : "instant";
-    viewport.current!.scrollTo({
-      left: viewport.current!.scrollWidth,
-      behavior,
+    // Scroll to end
+    viewportRef.current?.scrollTo({
+      behavior: "smooth",
+      left: viewportRef.current.scrollWidth,
     });
-    isHydrated.current = true;
   }, [teamJam.events.length]);
+
+  const scrollByOneChild = useCallback(
+    (direction: "left" | "right") => {
+      if (groupRef.current == null) {
+        return;
+      }
+      const numChildren = groupRef.current.children.length;
+      const childWidth = Math.round(groupRef.current.scrollWidth) / numChildren;
+      const scrollAmount = direction == "left" ? -childWidth : childWidth;
+
+      // Normalize the scroll position to be a multiple of the child width
+      let left = Math.round(scrollPosition.x) + scrollAmount;
+      if (direction == "left") {
+        left += left % childWidth;
+      } else {
+        left -= left % childWidth;
+      }
+
+      viewportRef.current?.scrollTo({
+        behavior: "smooth",
+        left,
+      });
+    },
+    [scrollPosition.x],
+  );
 
   return (
     <Group gap={0} justify="center" wrap="nowrap">
       <Button
         disabled={scrollPosition.x == 0}
-        onClick={() => scrollLeft(viewport.current!)}
+        onClick={() => scrollByOneChild("left")}
         p={0}
         pr={1}
         variant="subtle"
@@ -54,12 +68,12 @@ export default function TeamJamTripHistory({
       </Button>
       <ScrollArea.Autosize
         type="never"
-        onScrollPositionChange={onScrollPositionChange}
-        viewportRef={viewport}
+        onScrollPositionChange={setScrollPosition}
+        viewportRef={viewportRef}
         h={60}
-        w={scrollWidth}
+        w={w}
       >
-        <Group mx={0} gap={0} preventGrowOverflow={false} wrap="nowrap">
+        <Group ref={groupRef} mx={0} px={0} gap={0} wrap="nowrap">
           {teamJam.events
             .filter((event) => event.passes != null)
             .map((teamJam, i) => (
@@ -74,7 +88,7 @@ export default function TeamJamTripHistory({
         //   viewport.current.scrollWidth <= scrollWidth ||
         //   scrollPosition.x >= viewport.current.scrollWidth - scrollWidth
         // }
-        onClick={() => scrollRight(viewport.current!)}
+        onClick={() => scrollByOneChild("right")}
         p={0}
         pl={1}
         variant="subtle"
