ca4978baa749910c359507822e350185740ea8ed
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-17 17:36:45 -0800

    polish component

--- frontend/features/timeouts/components/type-editor.tsx
+++ frontend/features/timeouts/components/type-editor.tsx
@@ -1,13 +1,24 @@
+import { Timeout } from "@/lib/game/timeouts";
+import { TimeoutContext } from "@/utils/contexts";
 import {
   SegmentedControl,
   SegmentedControlProps,
   Stack,
   Text,
 } from "@mantine/core";
+import { useContext } from "react";
+import { useSetType } from "../hooks/set-type";
 
 export default function TimeoutTypeEditor({
   ...props
 }: Omit<SegmentedControlProps, "data" | "value" | "onChange">) {
+  const timeout: Timeout | null = useContext(TimeoutContext);
+  if (timeout == null) {
+    throw new Error("TimeoutTypeEditor must be used within a TimeoutProvider");
+  }
+
+  const setType = useSetType(timeout);
+
   return (
     <Stack gap="0">
       <Text size="sm" fw="300">
@@ -18,10 +29,13 @@ export default function TimeoutTypeEditor({
           { value: "timeout", label: "Timeout" },
           { value: "review", label: "Official Review" },
         ]}
-        // value={timeout.isReview ? "review" : "timeout"} // TODO
-        onChange={
-          (type: string) => console.log(type) // FIXME
-        }
+        value={timeout.isReview ? "review" : "timeout"}
+        onChange={(type: string) => {
+          if (type != "review" && type != "timeout") {
+            throw new Error("TimeoutTypeEditor is incorrectly configured");
+          }
+          setType.mutate(type);
+        }}
         {...props}
       />
     </Stack>
