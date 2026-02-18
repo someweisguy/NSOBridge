ecadd4e432eab8af8691d2142e339430729b5fe2
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-17 17:27:59 -0800

    polish component

--- frontend/features/timeouts/components/calling-team-editor.tsx
+++ frontend/features/timeouts/components/calling-team-editor.tsx
@@ -1,20 +1,34 @@
-import { Bout } from "@/lib/game/bouts";
-import { BoutContext } from "@/utils/contexts";
+import { Bout, Team } from "@/lib/game/bouts";
+import { Timeout } from "@/lib/game/timeouts";
+import { BoutContext, TimeoutContext } from "@/utils/contexts";
 import {
   SegmentedControl,
   SegmentedControlProps,
   Stack,
   Text,
 } from "@mantine/core";
-import { useContext } from "react";
+import { useContext, useDeferredValue } from "react";
+import { useSetTeam } from "../hooks/set-team";
 
 export default function TimeoutCallingTeamEditor({
   ...props
 }: Omit<SegmentedControlProps, "data" | "value" | "onChange">) {
   const bout: Bout | null = useContext(BoutContext);
-  if (bout == null) {
-    throw new Error("PrimaryBoutStatus must be used within a BoutProvider");
+  const timeout: Timeout | null = useContext(TimeoutContext);
+  if (bout == null || timeout == null) {
+    throw new Error(
+      "TimeoutCallingTeamEditor must be used within a TimeoutProvider",
+    );
   }
+
+  // Used to solve a minor UI glitch that occurs when selecting the initial value of a
+  // SegmentedControl component
+  const isInitialSelection = useDeferredValue(
+    timeout.teamNum == null && !timeout.teamIsOfficials,
+  );
+
+  const setTeam = useSetTeam(timeout);
+
   return (
     <Stack gap="0">
       <Text size="sm" fw="300">
@@ -22,28 +36,28 @@ export default function TimeoutCallingTeamEditor({
       </Text>
       <SegmentedControl
         data={[
-          // TODO: labels should display Roster name/mnemonic
-          { value: String(bout.teams[0].num), label: "Home" },
-          { value: String(bout.teams[1].num), label: "Away" },
+          ...bout.teams.map((team: Team) => {
+            return {
+              value: String(team.num),
+              label: team.name,
+            };
+          }),
           {
             value: String(NaN),
             label: "Official",
-            disabled: false, // FIXME: disable if timeout.isReview,
+            disabled: timeout.isReview,
           },
         ]}
         value={
-          ""
-          // FIXME
-          // timeout.teamNum == null
-          //   ? timeout.teamIsOfficials
-          //     ? String(NaN)
-          //     : ""
-          //   : String(timeout.teamNum)
+          timeout.teamNum == null
+            ? timeout.teamIsOfficials
+              ? String(NaN)
+              : "" // Nothing selected
+            : String(timeout.teamNum)
         }
-        transitionDuration={200} // TODO: set duration = 0 if no value selected
-        onChange={
-          (teamNum) => console.log(teamNum) // FIXME
-          // setTeam.mutate(teamNum == String(NaN) ? null : Number(teamNum))
+        transitionDuration={isInitialSelection ? 0 : 200}
+        onChange={(teamNum) =>
+          setTeam.mutate(teamNum == String(NaN) ? null : Number(teamNum))
         }
         {...props}
       />
