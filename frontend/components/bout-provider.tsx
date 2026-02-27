0b214a9dfcb31fbda0666f6f32a8646a77555b75
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-26 21:58:56 -0800

    remove contexts

--- frontend/components/bout-provider.tsx
+++ frontend/components/bout-provider.tsx
@@ -1,6 +1,5 @@
 import { useSuspenseBout } from "@/hooks/use-suspense-bout";
-import { useSuspenseRuleset } from "@/hooks/use-suspense-ruleset";
-import { BoutContext, RulesetContext } from "@/utils/contexts";
+import { BoutContext } from "@/utils/contexts";
 import { PropsWithChildren } from "react";
 
 interface BoutProviderProps extends PropsWithChildren {
@@ -12,10 +11,5 @@ export default function BoutProvider({
   children,
 }: BoutProviderProps) {
   const { data: bout } = useSuspenseBout(boutUuid);
-  const { data: ruleset } = useSuspenseRuleset(bout);
-  return (
-    <BoutContext value={bout}>
-      <RulesetContext value={ruleset}>{children}</RulesetContext>
-    </BoutContext>
-  );
+  return <BoutContext value={bout}>{children}</BoutContext>;
 }
