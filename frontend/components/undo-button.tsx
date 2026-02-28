c4672d9e6c938fdfdc69c9a10815790d1d6b4db6
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-27 19:08:48 -0800

    documentation

--- frontend/components/undo-button.tsx
+++ frontend/components/undo-button.tsx
@@ -2,6 +2,10 @@ import { useUndo } from "@/hooks/use-undo";
 import { ActionIcon, ActionIconProps } from "@mantine/core";
 import { IconArrowBackUp } from "@tabler/icons-react";
 
+/**
+ * Used to undo the last action that was performed by the user. Undo histories are
+ * tracked per-user.
+ */
 export default function UndoButton({
   ...props
 }: Omit<ActionIconProps, "onClick">) {
