9bef2f9a7f0291b940d8fca4765b6984d11c6251
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-27 20:01:06 -0800

    add stories

---
+++ frontend/features/bouts/stories/BoutSecondaryStatus.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { BoutSecondaryStatus } from "../components/bout-secondary-status";
+
+const meta: Meta<typeof BoutSecondaryStatus> = {
+  component: BoutSecondaryStatus,
+  argTypes: {
+    since: {
+      control: "date",
+    },
+  },
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const Lineup: Story = {
+  args: {
+    stateText: "Lineup",
+    since: new Date(),
+  },
+};
+
+export const Halftime: Story = {
+  args: {
+    stateText: "Halftime",
+    since: new Date(new Date().getTime() + 1800000),
+  },
+};
+
+export const NoState: Story = {
+  args: {
+    stateText: "",
+    since: new Date(new Date().getTime() + 1800000),
+  },
+};
+
+export default meta;
---
+++ frontend/features/bouts/stories/BoutStatus.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import BoutStatus from "../components/bout-status";
+
+const meta: Meta<typeof BoutStatus> = {
+  component: BoutStatus,
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    isOvertime: false,
+    startTimestamp: new Date(),
+    alarm: 1800000,
+    overtimeText: "OT",
+  },
+};
+
+export const Centered: Story = {
+  args: {
+    isOvertime: false,
+    startTimestamp: new Date(),
+    alarm: 1800000,
+    overtimeText: "OT",
+    ta: "center",
+  },
+};
+
+export default meta;
