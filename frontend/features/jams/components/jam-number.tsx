3f38e8e60ff7aee4c51abf18d7818e3b8cfcdd63
Mitch Weisbrod <mitch@theweisbrods.com>
2026-02-28 21:59:03 -0800

    add docs and story

--- frontend/features/jams/components/jam-number.tsx
+++ frontend/features/jams/components/jam-number.tsx
@@ -1,10 +1,19 @@
 import { Text, TextProps } from "@mantine/core";
 
 interface JamNumberProps extends TextProps {
+  /**
+   * The Period number to display where 0 is the initial Period.
+   */
   periodNum: number;
+  /**
+   * The Jam number to display where 0 is the initial Jam.
+   */
   jamNum: number;
 }
 
+/**
+ * Display a Jam's Period and Jam number in a standardized format.
+ */
 export default function JamNumber({
   periodNum,
   jamNum,
---
+++ frontend/features/jams/stories/jam-number.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import JamNumber from "../components/jam-number";
+
+const meta: Meta<typeof JamNumber> = {
+  component: JamNumber,
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    jamNum: 0,
+    periodNum: 0,
+  },
+};
+
+export default meta;
