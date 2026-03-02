de84ac6fd3ce7e5dd23b1dd1a65819f71fd6de3d
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-01 20:39:52 -0800

    remove glob for .mdx files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: [
-    "../frontend/stories/**/*.mdx",
     "../frontend/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../frontend/features/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
