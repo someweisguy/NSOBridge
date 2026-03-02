17d374b2c5a540335f6e48cb3cfd261d9b3b7cfb
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-01 22:38:52 -0800

    use URI as props

--- frontend/features/timeouts/components/timeouts-left-container.tsx
+++ frontend/features/timeouts/components/timeouts-left-container.tsx
@@ -4,8 +4,8 @@ import { useTimeout } from "@/hooks/use-timeout";
 
 interface TimeoutsLeftContainerProps {
   boutUuid: string;
+  timeoutNum: number | null;
   teamNum: number;
-  timeoutCount: number;
   timeoutsRemaining: number;
   reviewsRemaining: number;
   size: number;
@@ -14,7 +14,7 @@ interface TimeoutsLeftContainerProps {
 export default function TimeoutsLeftContainer({
   boutUuid,
   teamNum,
-  timeoutCount,
+  timeoutNum,
   timeoutsRemaining,
   reviewsRemaining,
   size,
@@ -25,8 +25,8 @@ export default function TimeoutsLeftContainer({
     data: latestTimeout,
     isPending,
     isEnabled,
-  } = useTimeout(boutUuid, timeoutCount - 1, {
-    enabled: timeoutCount > 0,
+  } = useTimeout(boutUuid, timeoutNum ?? 0, {
+    enabled: timeoutNum != null && timeoutNum > 0,
   });
 
   const timeoutIsActive =
