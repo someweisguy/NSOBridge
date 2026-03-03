4669dc42eef036ff565f040cb4df69d54f22b786
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-02 21:48:50 -0800

    documentation

--- frontend/hooks/use-bout.ts
+++ frontend/hooks/use-bout.ts
@@ -2,6 +2,12 @@ import { Bout, getBout } from "@/lib/game/bouts";
 import { BoutUri, QueryOptions } from "@/types/query";
 import { useQuery } from "@tanstack/react-query";
 
+/**
+ * Fetches the desired Bout from the server. This hook is a wrapper for call to TanStack
+ * Query's `useQuery` function.
+ *
+ * @returns a Tanstack useQuery object containing the desired Bout.
+ */
 export const useBout = <T = null>({
   boutUuid,
   ...options
--- frontend/hooks/use-suspense-bout.ts
+++ frontend/hooks/use-suspense-bout.ts
@@ -2,6 +2,12 @@ import { Bout, getBout } from "@/lib/game/bouts";
 import { BoutUri, SuspenseQueryOptions } from "@/types/query";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+/**
+ * Fetches the desired Bout from the server. This hook is a wrapper for call to TanStack
+ * Query's `useSuspenseQuery` function.
+ *
+ * @returns a Tanstack useSuspenseQuery object containing the desired Bout.
+ */
 export const useSuspenseBout = ({
   boutUuid,
   ...options
