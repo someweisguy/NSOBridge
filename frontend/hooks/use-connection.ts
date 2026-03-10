5ba44e21cb30872a60f84299380612ea6b969459
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-09 20:06:07 -0700

    documentation

--- frontend/hooks/use-connection.ts
+++ frontend/hooks/use-connection.ts
@@ -1,6 +1,11 @@
 import { onlineManager } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
+/**
+ * Used to determine if the client is connected to the server.
+ *
+ * @returns true if the client is connected to the scoreboard server.
+ */
 export const useConnection = (): boolean => {
   const [isOnline, setIsOnline] = useState(onlineManager.isOnline());
 
