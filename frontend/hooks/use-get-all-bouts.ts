be841c6fe6c77f39b8792fdbb116651adcab44e8
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-09 20:17:27 -0700

    documentation

--- frontend/hooks/use-get-all-bouts.ts
+++ frontend/hooks/use-get-all-bouts.ts
@@ -3,6 +3,13 @@ import { Bout, getAllBouts } from "@/lib/game/bouts";
 import { QueryOptions } from "@/types/query";
 import { useQuery } from "@tanstack/react-query";
 
+/**
+ * Gets all the Bouts from the server. Each individual Bout is automatically cached
+ * after it is fetched. This hook is a wrapper for call to TanStack Query's `useQuery`
+ * function.
+ *
+ * @returns a Tanstack useQuery object containing an array of all Bouts.
+ */
 export const useGetAllBouts = (
   options?: Omit<QueryOptions<Bout[]>, "queryKey" | "queryFn">,
 ) =>
--- frontend/hooks/use-suspense-get-all-series.ts
+++ frontend/hooks/use-suspense-get-all-series.ts
@@ -1,12 +1,26 @@
+import queryClient from "@/lib/cache";
 import { getAllSeries, Series } from "@/lib/game/series";
 import { SuspenseQueryOptions } from "@/types/query";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+/**
+ * Gets all the Series from the server. Each individual Series is automatically cached
+ * after it is fetched. This hook is a wrapper for call to TanStack Query's
+ * `useSuspenseQuery` function.
+ *
+ * @returns a Tanstack useSuspenseQuery object containing an array of all Series.
+ */
 export const useSuspenseGetAllSeries = (
   options?: Omit<SuspenseQueryOptions<Series[]>, "queryKey" | "queryFn">,
 ) =>
   useSuspenseQuery<Series[]>({
     queryKey: Series.generateKey(),
-    queryFn: () => getAllSeries(),
+    queryFn: () =>
+      getAllSeries().then((allSeries: Series[]) => {
+        for (const series of allSeries) {
+          queryClient.setQueryData(Series.generateKey(series.uuid), series);
+        }
+        return allSeries;
+      }),
     ...options,
   });
