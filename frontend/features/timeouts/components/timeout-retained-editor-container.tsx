29476810c3b2d30128f6b0528f1fb3cd13736ee8
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-11 22:32:20 -0700

    cleanup props and add documentation

--- frontend/features/timeouts/components/timeout-retained-editor-container.tsx
+++ frontend/features/timeouts/components/timeout-retained-editor-container.tsx
@@ -1,20 +1,19 @@
 import { useSuspenseTimeout } from "@/hooks/use-suspense-timeout";
+import { TimeoutUri } from "@/types/query";
 import { Checkbox, CheckboxProps } from "@mantine/core";
 import { useSetTimeoutRetained } from "../hooks/use-set-timeout-retained";
 
-interface TimeoutRetainedEditorContainerProps extends Omit<
-  CheckboxProps,
-  "onChange"
-> {
-  boutUuid: string;
-  timeoutNum: number;
-}
-
+/**
+ * Display a control with allows users to edit whether or not a Timeout or Official
+ * Review was retained. In WFTDA 2026 rules, only Official Reviews can be retained. For
+ * simplicity and flexibility the database schema allows either Timeouts or Reviews to
+ * be retained.
+ */
 export default function TimeoutRetainedEditorContainer({
   boutUuid,
   timeoutNum,
   label = "Review is Retained?",
-}: TimeoutRetainedEditorContainerProps) {
+}: TimeoutUri & Omit<CheckboxProps, "onChange">) {
   const { data: timeout } = useSuspenseTimeout({ boutUuid, timeoutNum });
 
   const setRetained = useSetTimeoutRetained({ boutUuid, timeoutNum });
