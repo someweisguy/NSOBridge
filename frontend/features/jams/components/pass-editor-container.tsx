ef3e7cc75b2ac175785d1387e806fcd1ae2a92d1
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-11 22:42:49 -0700

    cleanup props and add docs

--- frontend/features/jams/components/pass-editor-container.tsx
+++ frontend/features/jams/components/pass-editor-container.tsx
@@ -2,21 +2,20 @@ import { useTeamJamAddTrip } from "@/features/jams/hooks/use-team-jam-add-trip";
 import { useSuspenseJam } from "@/hooks/use-suspense-jam";
 import { useSuspenseRuleset } from "@/hooks/use-suspense-ruleset";
 import { TeamJam } from "@/lib/game/jams";
+import { TeamJamUri } from "@/types/query";
 import PassEditor from "./pass-editor";
 
-interface TeamJamPassEditorProps {
-  boutUuid: string;
-  periodNum: number;
-  jamNum: number;
-  teamNum: number;
-}
-
+/**
+ * Display the buttons which users may use to add trips to the Jammer's Jam. Typically
+ * on the initial pass, a special set of buttons is displayed to simplify the UI.
+ * Otherwise, a set of buttons to add passes to a Trip is displayed.
+ */
 export default function TeamJamPassEditorContainer({
   boutUuid,
   periodNum,
   jamNum,
   teamNum,
-}: TeamJamPassEditorProps) {
+}: TeamJamUri) {
   const { data: ruleset } = useSuspenseRuleset({ boutUuid });
   const { data: jam } = useSuspenseJam({ boutUuid, periodNum, jamNum });
   const teamJam: TeamJam | undefined = jam.teamJams.find(
