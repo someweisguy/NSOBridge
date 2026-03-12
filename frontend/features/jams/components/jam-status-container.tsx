38029405ec6fb5dbc310cfd47219022c2bd5938b
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-11 22:38:04 -0700

    prop cleanup and docs

--- frontend/features/jams/components/jam-status-container.tsx
+++ frontend/features/jams/components/jam-status-container.tsx
@@ -2,47 +2,28 @@ import { useSuspenseJam } from "@/hooks/use-suspense-jam";
 import { TextProps } from "@mantine/core";
 
 import { useSuspenseRuleset } from "@/hooks/use-suspense-ruleset";
-import { StopReasonString } from "@/lib/game/jams";
+import { JamUri } from "@/types/query";
 import { JamClock } from "./jam-clock";
 
-interface JamStatusContainerProps extends TextProps {
-  /**
-   * The desired Bout UUID.
-   */
-  boutUuid: string;
-  /**
-   * The Period number of the desired Jam.
-   */
-  periodNum: number;
-  /**
-   * The Jam number of the desired Jam.
-   */
-  jamNum: number;
-  /**
-   * An object containing the stop reason texts.
-   */
-  stopReasonTexts?: Record<StopReasonString, string>;
-  /**
-   * The offset in time in milliseconds between the server and the host.
-   */
-  serverOffset?: number;
-}
+// Default strings to display for the various reason a Jam can be stopped.
+const stopReasonTexts = {
+  called: "Called",
+  elapsed: "Time",
+  injury: "Injury",
+  other: "-",
+};
 
 /**
- * Fetch server data and display the status of the desired Jam.
+ * Display a component representing the desired Jam's status. If the Jam has not yet
+ * started or if the Jam is running a clock is displayed. If the Jam has ended, a string
+ * representing the reason that the Jam was stopped is displayed.
  */
 export default function JamStatusContainer({
   boutUuid,
   periodNum,
   jamNum,
-  stopReasonTexts = {
-    called: "Called",
-    elapsed: "Time",
-    injury: "Injury",
-    other: "-",
-  },
   ...props
-}: JamStatusContainerProps) {
+}: JamUri & TextProps) {
   const { data: ruleset } = useSuspenseRuleset({ boutUuid });
   const { data: jam } = useSuspenseJam({ boutUuid, periodNum, jamNum });
 
