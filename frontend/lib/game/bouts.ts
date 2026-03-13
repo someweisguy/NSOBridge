ad8b209237f8f3c98da262841c0a280997bf94aa
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-12 21:42:21 -0700

    documentation

--- frontend/lib/game/bouts.ts
+++ frontend/lib/game/bouts.ts
@@ -17,18 +17,58 @@ export type BoutStateString =
  * Represents a roller derby Bout - the game unit within this app.
  */
 export class Bout {
+  /**
+   * A unique identifier used with this Bout.
+   */
   uuid: string;
+  /**
+   * The unique identifier of the Series with which this Bout is associated.
+   */
   seriesUuid: string;
+  /**
+   * The unique name of the Ruleset that this Bout uses.
+   */
   rulesetName: string;
-
+  /**
+   * The timestamp at which the next Period will begin or null if no such timestamp has
+   * been set. This value is used to set the "time-to-derby" count-down during pregame
+   * and halftime.
+   */
   startCountdown: Date | null;
+  /**
+   * The Period clock. Keeps track of the amount of time that has elapsed in the current
+   * half.
+   */
   clock: Clock;
-
+  /**
+   * The current state of the Bout.  // TODO: more documentation required here.
+   */
   state: BoutStateString;
+  /**
+   * True if this Bout is currently running. A Bout is considered to be running when it
+   * is not in pregame, halftime, or final. It is possible for a Bout to be running even
+   * when the Period clock is stopped.
+   */
   isRunning: boolean;
+  /**
+   * True if this Bout has been finalized. In this situation, a winner (or a tie) has
+   * been declared.
+   */
   isFinal: boolean;
+  /**
+   * An array of the Teams competing in this Bout. At least two Teams are required.
+   */
   teams: Team[];
+  /**
+   * The number of Jams in this Bout, per Period. The "third Period" value is used to
+   * track the number of overtime Jams and is used to determine if this Bout is in
+   * overtime. It is guaranteed that there will always be at least one Jam in a Bout,
+   * even if it hasn't started.
+   */
   jamCounts: [number, number, number];
+  /**
+   * The number of Timeouts that have been called in this Bout.
+   */
   timeoutCount: number;
 
   /**
@@ -113,16 +153,43 @@ export class Bout {
  * An object representing a roller derby Team.
  */
 export interface Team {
+  /**
+   * A unique number representing this Team in this Bout.
+   */
   num: number;
-
+  /**
+   * The name of this Team.
+   */
   name: string;
+  /**
+   * The league associated with this Team.
+   */
   league: string;
+  /**
+   * An abbreviation for this Team name.
+   */
   mnemonic: string;
-
+  /**
+   * The total score of this Team.
+   */
   boutScore: number;
+  /**
+   * The number of points which were contributed to this Team's total score in the
+   * previous Bout.
+   */
   jamScore: number;
+  /**
+   * The number of Timeouts that this Team may call.
+   */
   timeoutsRemaining: number;
+  /**
+   * The number of Official Reviews that this Team may call.
+   */
   reviewsRemaining: number;
+  /**
+   * The score offset which to add to this Team's Bout score. This value is allowed in
+   * the WFTDA 2026 stats paperwork but is used extremely rarely.
+   */
   scoreOffset: number;
 }
 
