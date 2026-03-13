3b87134d6def4f00e0b5b6c7fdc3e3350e7bfae9
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-12 21:25:25 -0700

    documentation

--- frontend/lib/game/series.ts
+++ frontend/lib/game/series.ts
@@ -1,6 +1,11 @@
 import { CacheKey } from "@/types/ws";
 import { localAPI } from "../requests";
 
+/**
+ * Represent an event with multiple Bouts. A Series is simply a collection of multiple
+ * Bouts. This grouping can be used to track per-event statistics, such as the number
+ * of team wins/losses during a tournament Series.
+ */
 export class Series {
   uuid: string;
   name: string;
@@ -22,6 +27,11 @@ export class Series {
   }
 }
 
+/**
+ * Get all Series on the server.
+ *
+ * @returns an array of all Series.
+ */
 export async function getAllSeries(): Promise<Series[]> {
   const data = await localAPI.get<Partial<Series>[]>("series/allSeries");
   return data.map((series: Partial<Series>) =>
@@ -29,6 +39,12 @@ export async function getAllSeries(): Promise<Series[]> {
   );
 }
 
+/**
+ * Get a Series from the server
+ *
+ * @param seriesUuid the UUID of the desired Series.
+ * @returns the desired Series.
+ */
 export async function getSeries(seriesUuid: string): Promise<Series> {
   const data = await localAPI.get<Partial<Series>>("series", {
     query: { seriesUuid },
