9167fa810c3b824d7a2a87a9931aa95d5513a5c5
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-12 19:34:31 -0700

    add documentation

--- frontend/lib/history.ts
+++ frontend/lib/history.ts
@@ -1,9 +1,17 @@
 import { localAPI } from "./requests";
 
+/**
+ * Call the Undo API. Undo and redo histories are based off a user UUID cookie.
+ * Histories are volatile and are erased when restarting the server.
+ */
 export async function undo(): Promise<void> {
   await localAPI.post("undo");
 }
 
+/**
+ * Call the Redo API. Undo and redo histories are based off a user UUID cookie.
+ * Histories are volatile and are erased when restarting the server.
+ */
 export async function redo(): Promise<void> {
   await localAPI.post("redo");
 }
