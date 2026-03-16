2237b56cdcfa27602e29bddf9ae22c6ab92aa95e
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-15 20:13:53 -0700

    add request to dependency

--- backend/src/core/dependencies.py
+++ backend/src/core/dependencies.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Annotated, AsyncGenerator, ClassVar, TypeAlias
 
-from fastapi import Depends
+from fastapi import Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from .database import BaseSQLModel, DatabaseEngine
@@ -55,7 +55,9 @@ class EngineFactory:
         return DatabaseEngine(BaseSQLModel, db_path)
 
     @classmethod
-    async def yield_async_session(cls) -> AsyncGenerator[AsyncSession, None]:
+    async def yield_async_session(
+        cls, request: Request
+    ) -> AsyncGenerator[AsyncSession, None]:
         """Yield a session which automatically commits.
 
         This method is useful for FastAPI dependency injection.
