248f18f0294a784668a096e1ad50f21c4d2b9d0f
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-20 19:48:49 -0700

    documentation

--- backend/src/core/types.py
+++ backend/src/core/types.py
@@ -3,4 +3,7 @@
 from typing import Any, Sequence
 
 type CacheKey = tuple[Any, ...]
+"""Used by the client to cache requests locally."""
+
 type CacheServerSchema = Sequence[CacheKey]
+"""The schema for packet WebSocket cache invalidation messages."""
