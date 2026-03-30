2d942a1e4c2d8769174005efea8f011deeba9ad2
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-29 17:39:59 -0700

    add protocol method

---
+++ backend/src/game/bouts/protocol.py
@@ -0,0 +1,138 @@
+"""Protocols for the Bout.
+
+The primary protocol here is the Protocol which describes all of the "rule-bound"
+methods. These are methods which should change depending on the ruleset of Roller Derby
+in use. For example, WFTDA Roller Derby typically has a different method for calculating
+the core of a Bout then, say, Survival Rules. Therefore, each Bout would implement a
+different of the same Protocol.
+
+The Protocol is declared in this file for ease of readability!
+"""
+
+from datetime import datetime
+from typing import Protocol
+
+from game.teams.models import Team
+
+
+class Ruleset(Protocol):
+    """Handles ruleset events."""
+
+    def init_bout(self) -> None:
+        """Do Bout initialization.
+
+        Args:
+            bout (Bout): the Bout to initialize.
+
+        """
+        ...
+
+    def begin_period(self, timestamp: datetime) -> None:
+        """Begin the next Period.
+
+        Args:
+            timestamp (datetime): the timestamp at which to begin the Period.
+
+        """
+        ...
+
+    def end_period(self, timestamp: datetime) -> None:
+        """End the current Period.
+
+        Args:
+            timestamp (datetime): the timestamp at which to end the Period.
+
+        """
+        ...
+
+    def start_jam(self, timestamp: datetime) -> None:
+        """Start the next Jam.
+
+        Args:
+            timestamp (datetime): the timestamp at which to start the Jam.
+
+        Returns:
+            BaseJam: the Jam that was started.
+
+        """
+        ...
+
+    def stop_jam(self, timestamp: datetime) -> None:
+        """Stop the current Jam.
+
+        Args:
+            timestamp (datetime): the timestamp at which to stop the Jam.
+
+        Returns:
+            BaseJam: the Jam that was stopped.
+
+        """
+        ...
+
+    def start_timeout(self, timestamp: datetime) -> None:
+        """Call a Timeout.
+
+        Args:
+            timestamp (datetime): the timestamp at which to call the Timeout.
+
+        Returns:
+            BaseTimeout: the Timeout that was called.
+
+        """
+        ...
+
+    def stop_timeout(self, timestamp: datetime) -> None:
+        """Stop the current Timeout.
+
+        Args:
+            timestamp (datetime): the timestamp at which to stop the Timeout.
+
+        Returns:
+            BaseTimeout: the Timeout that was stopped.
+
+        """
+        ...
+
+    def add_trip(self, team: Team, timestamp: datetime, passes: int) -> None:
+        """Add a Jammer trip to the desired Team's latest or active TeamJam.
+
+        Args:
+            team (BaseTeam): the desired Team.
+            timestamp (datetime): the timestamp at which to add the trip.
+            passes (int): the number of passes the Jammer earned.
+
+        """
+        ...
+
+    def add_lead(self, team: Team, timestamp: datetime, lead: bool) -> None:
+        """Set the lead Jammer status for the desired Team.
+
+        Args:
+            team (BaseTeam): the desired Team.
+            timestamp (datetime): the timestamp at which to set lead.
+            lead (bool): True if the Jammer has been declared lead.
+
+        """
+        ...
+
+    def add_lost(self, team: Team, timestamp: datetime, lost: bool) -> None:
+        """Set the lead eligibility for the desired Team.
+
+        Args:
+            team (BaseTeam): the desired Team.
+            timestamp (datetime): the timestamp at which to set lead eligibility.
+            lost (bool): True if the Jammer has lost lead eligibility.
+
+        """
+        ...
+
+    def add_star_pass(self, team: Team, timestamp: datetime, star_pass: bool) -> None:
+        """Add a star pass to the desired Team.
+
+        Args:
+            team (BaseTeam): the desired Team.
+            timestamp (datetime): the timestamp at which to add the star pass.
+            star_pass (bool): True if the star has been successfully passed.
+
+        """
+        ...
