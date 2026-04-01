688b0262d59aee6eac22b328bcd40d9b3573aeec
Mitch Weisbrod <mitch@theweisbrods.com>
2026-03-31 21:47:43 -0700

    query supported rulesets

--- frontend/components/create-bout-button.tsx
+++ frontend/components/create-bout-button.tsx
@@ -1,13 +1,34 @@
-import { Button, Modal } from "@mantine/core";
+import { useAllRulesetNames } from "@/hooks/use-all-ruleset-names";
+import { Button, Modal, Select } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 
 export default function CreateBoutButton() {
   const [opened, { open, close }] = useDisclosure(false);
+  const { data: rulesetNames } = useAllRulesetNames({ placeholderData: [] });
+
+  const [rulesetName, setRulesetName] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Set the
+    if (rulesetName == null && rulesetNames != null) {
+      setRulesetName(rulesetNames[0]);
+    }
+  }, [rulesetName, rulesetNames]);
 
   return (
     <>
-      <Modal title="Create Bout" opened={opened} onClose={close} centered>
-        {/* // TODO: add Bout creation components */}
+      <Modal title="Create a New Bout" opened={opened} onClose={close} centered>
+        <Select
+          label="New Bout Ruleset"
+          placeholder="Derby Ruleset"
+          data={rulesetNames}
+          value={rulesetName}
+          onChange={(value: string | null) => setRulesetName(value)}
+          loading={rulesetNames == null}
+          allowDeselect={false}
+          autoSelectOnBlur
+        />
       </Modal>
       <Button onClick={open}>Create New Bout</Button>
     </>
