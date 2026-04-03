b70551a634bf978c71913bc563a28a7e66a7d8cd
Mitch Weisbrod <mitch@theweisbrods.com>
2026-04-02 20:28:41 -0700

    fix storybook

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
-            configDir: path.join(dirname, "storybook"),
+            configDir: ".storybook",
           }),
         ],
         test: {
@@ -71,7 +71,7 @@ export default defineConfig({
               },
             ],
           },
-          setupFiles: [".storybook/vitest.setup.ts"],
+          setupFiles: ["../.storybook/vitest.setup.ts"],
         },
       },
     ],
