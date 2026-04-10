bb2d36f8533618250c5e74896cfd98226ecfec5f
Mitch Weisbrod <mitch@theweisbrods.com>
2026-04-09 21:28:38 -0700

    automigrate storybook

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,6 +1,10 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
 import type { StorybookConfig } from "@storybook/react-vite";
 import { fileURLToPath } from "node:url";
-import path from "path";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const frontendDirectory = path.resolve(
   typeof __dirname !== "undefined"
