363a973f4777705dc9f659801d9421c67a19d517
Mitch Weisbrod <mitch@theweisbrods.com>
2026-05-04 19:32:39 -0700

    add story

--- frontend/stories/operator.stories.ts
+++ frontend/stories/operator.stories.ts
@@ -1,6 +1,7 @@
 import Operator from "@/app/operator";
 import { Bout } from "@/types/bout";
 import { Jam } from "@/types/jam";
+import { Timeout } from "@/types/timeout";
 import { mockApiResponse } from "@/utils/mock-api";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { http, HttpResponse } from "msw";
@@ -149,6 +150,109 @@ export const PostJam: Story = {
                   ],
                 },
               ],
+            }),
+          );
+        }),
+      ],
+    },
+  },
+  args: {},
+};
+
+export const InTimeout: Story = {
+  name: "Timeout",
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("/api/bout/ruleset", () => {
+          return HttpResponse.json(mockApiResponse(rulesetData));
+        }),
+        http.get("/api/bout/allBouts", () => {
+          return HttpResponse.json(
+            mockApiResponse<Bout[]>([
+              {
+                uuid: "27ced413-0a20-49e2-b570-7fa776f768c0",
+                rulesetName: "WFTDA 2025",
+                seriesUuid: "e9f0a658-7dfc-43c4-8027-7d7d33c79575",
+                clock: {
+                  startTimestamp: null,
+                  elapsed: 14675,
+                  alarm: 1800000,
+                },
+                isRunning: true,
+                startCountdown: null,
+                isFinal: false,
+                state: "timeout",
+                teams: [
+                  {
+                    name: "Home",
+                    league: "",
+                    mnemonic: "",
+                    num: 0,
+                    boutScore: 8,
+                    jamScore: 8,
+                    timeoutsRemaining: 3,
+                    reviewsRemaining: 1,
+                    scoreOffset: 0,
+                    skaters: [],
+                  },
+                  {
+                    name: "Away",
+                    league: "",
+                    mnemonic: "",
+                    num: 1,
+                    boutScore: 2,
+                    jamScore: 2,
+                    timeoutsRemaining: 3,
+                    reviewsRemaining: 1,
+                    scoreOffset: 0,
+                    skaters: [],
+                  },
+                ],
+                jamCounts: [2, 0, 0],
+                timeoutCount: 1,
+              },
+            ]),
+          );
+        }),
+        http.get("/api/jam", () => {
+          return HttpResponse.json(
+            mockApiResponse<Jam>({
+              boutUuid: "27ced413-0a20-49e2-b570-7fa776f768c0",
+              period: 0,
+              num: 1,
+              startTimestamp: null,
+              stopTimestamp: null,
+              stopReason: null,
+              teamJams: [
+                {
+                  teamNum: 0,
+                  events: [],
+                },
+                {
+                  teamNum: 1,
+                  events: [],
+                },
+              ],
+            }),
+          );
+        }),
+        http.get("/api/timeout", () => {
+          return HttpResponse.json(
+            mockApiResponse<Timeout>({
+              boutUuid: "27ced413-0a20-49e2-b570-7fa776f768c0",
+              num: 0,
+              startTimestamp: "2026-05-04T19:25:55.192421",
+              stopTimestamp: null,
+              clockElapsed: 14675,
+              teamIsOfficials: false,
+              isReview: false,
+              details: "",
+              result: "",
+              retained: false,
+              periodNum: 0,
+              jamNum: 0,
+              teamNum: null,
             }),
           );
         }),
