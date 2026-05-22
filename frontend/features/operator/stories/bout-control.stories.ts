5c342a121faf905d5f089ffa8e2c3e3e75b390ff
Mitch Weisbrod <mitch@theweisbrods.com>
2026-05-21 20:14:42 -0700

    remove unused args

--- frontend/features/operator/stories/bout-control.stories.ts
+++ frontend/features/operator/stories/bout-control.stories.ts
@@ -5,20 +5,9 @@ const meta: Meta<typeof BoutControl> = {
   component: BoutControl,
   title: "Bout Control",
   argTypes: {
-    stopReason: {
-      control: {
-        type: "inline-radio",
-      },
-      options: ["called", "elapsed", "injury", null],
-    },
     state: {
       control: { type: "inline-radio" },
       options: ["lineup", "jam", "timeout", "stopped", "final"],
-    },
-    teamNum: {
-      control: {
-        type: "number",
-      },
     },
   },
 };
@@ -27,19 +16,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    teamData: [
-      { label: "Home", value: "0" },
-      { label: "Away", value: "1" },
-    ],
     state: "lineup",
-    teamNum: null,
-    teamIsOfficials: false,
-    isReview: false,
-    retained: false,
-    latestPeriodNum: 0,
-    latestJamNum: 1,
-    latestTimeoutNum: 1,
-    stopReason: null,
   },
 };
 
