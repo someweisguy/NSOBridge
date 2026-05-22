bf26e4a45c1bdb5c91b438adc16807ad1423971e
Mitch Weisbrod <mitch@theweisbrods.com>
2026-05-21 21:12:29 -0700

    remove redundant css styles

--- frontend/features/operator/components/timeout-editor.tsx
+++ frontend/features/operator/components/timeout-editor.tsx
@@ -63,7 +63,6 @@ export default function TimeoutEditor({
     <Fieldset
       legend={"Edit " + (isReview ? "Official Review" : "Timeout")}
       w="fit-content"
-      h="fit-content"
     >
       <Stack>
         <Group align="center">
