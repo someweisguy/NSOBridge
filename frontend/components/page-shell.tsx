90b0fc25f25a4cbd4ed13ad9b23a03d7dae2a417
Mitch Weisbrod <mitch@theweisbrods.com>
2026-05-31 13:04:08 -0700

    remove burger and add header props

--- frontend/components/page-shell.tsx
+++ frontend/components/page-shell.tsx
@@ -4,16 +4,16 @@ import {
   ActionIcon,
   AppShell,
   AppShellProps,
-  Burger,
   Group,
   Text,
 } from "@mantine/core";
-import { useDisclosure, useOs } from "@mantine/hooks";
+import { useOs } from "@mantine/hooks";
 import { IconArrowBackUp, IconArrowForwardUp } from "@tabler/icons-react";
 import { PropsWithChildren, ReactNode, Suspense, useEffect } from "react";
 
 interface PageShellProps
   extends Pick<AppShellProps, "disabled">, PropsWithChildren {
+  header?: ReactNode;
   navButtons?: ReactNode;
   aside?: ReactNode;
 }
@@ -22,14 +22,12 @@ interface PageShellProps
  * The main shell which wraps the application.
  */
 export default function PageShell({
+  header,
   navButtons,
   aside,
   disabled,
   children,
 }: PageShellProps) {
-  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
-  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
-
   const os = useOs();
   const undo = useUndo();
   const redo = useRedo();
@@ -66,29 +64,17 @@ export default function PageShell({
       aside={{
         width: 200,
         breakpoint: "sm",
-        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
       }}
       disabled={disabled}
     >
       <AppShell.Header>
         <Group h="100%" w="100%" justify="space-between" px="md">
           <Group align="center">
-            <Burger
-              opened={mobileOpened}
-              onClick={toggleMobile}
-              hiddenFrom="sm"
-              size="sm"
-            />
-            <Burger
-              opened={desktopOpened}
-              onClick={toggleDesktop}
-              visibleFrom="sm"
-              size="sm"
-            />
             <Text>NSO Bridge</Text>
           </Group>
 
-          <Group>
+          <Group gap="xs">
+            {header}
             <ActionIcon variant="subtle" onClick={() => undo.mutate()}>
               <IconArrowBackUp size={16} />
             </ActionIcon>
