1e5de8c1796d975be210957889971244d36841da
Mitch Weisbrod <mitch@theweisbrods.com>
2026-05-30 19:13:18 -0700

    change children to buttons to test pointer pass through events

--- frontend/stories/responsive-scroller.stories.tsx
+++ frontend/stories/responsive-scroller.stories.tsx
@@ -1,5 +1,5 @@
 import ResponsiveScroller from "@/components/responsive-scroller";
-import { Card } from "@mantine/core";
+import { Button } from "@mantine/core";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 const meta: Meta<typeof ResponsiveScroller> = {
@@ -13,9 +13,9 @@ export const Default: Story = {
   args: {
     w: "200px",
     children: Array.from({ length: 10 }, (_, i: number) => (
-      <Card withBorder key={i} w="60px" h="80px">
+      <Button variant="subtle" key={i} w="60px" h="80px">
         {i}
-      </Card>
+      </Button>
     )),
   },
 };
