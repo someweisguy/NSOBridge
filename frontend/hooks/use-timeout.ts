a1297e67c9077af840b0814dbc93a2d9b4b93e0d
Mitch Weisbrod <mitch@theweisbrods.com>
2026-06-07 19:50:15 -0700

    consolidate hooks

--- frontend/hooks/use-timeout.ts
+++ frontend/hooks/use-timeout.ts
@@ -1,8 +1,16 @@
 import { localAPI } from "@/lib/requests";
-import { AppQueryOptions, TimeoutUri } from "@/types/query";
+import {
+  AppQueryOptions,
+  AppSuspenseQueryOptions,
+  TimeoutUri,
+} from "@/types/query";
 import { Timeout } from "@/types/timeout";
 import { generateQueryKey } from "@/utils/query";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import {
+  useQuery,
+  UseQueryResult,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 
 /**
  * Fetches the desired Timeout from the server. This hook is a wrapper for call to
@@ -16,6 +24,26 @@ export const useTimeout = ({
   ...options
 }: TimeoutUri & AppQueryOptions<Timeout>): UseQueryResult<Timeout, Error> =>
   useQuery({
+    queryKey: generateQueryKey.timeout(boutUuid, timeoutNum),
+    queryFn: () =>
+      localAPI.get<Timeout>("timeout", {
+        query: { boutUuid, num: timeoutNum }, // TODO: fix alias
+      }),
+    ...options,
+  });
+
+/**
+ * Fetches the desired Timeout from the server. This hook is a wrapper for call to
+ * TanStack Query's `useSuspenseQuery` function.
+ *
+ * @returns a Tanstack useSuspenseQuery object containing the desired Timeout.
+ */
+export const useSuspenseTimeout = ({
+  boutUuid,
+  timeoutNum,
+  ...options
+}: TimeoutUri & AppSuspenseQueryOptions<Timeout>) =>
+  useSuspenseQuery({
     queryKey: generateQueryKey.timeout(boutUuid, timeoutNum),
     queryFn: () =>
       localAPI.get<Timeout>("timeout", {
