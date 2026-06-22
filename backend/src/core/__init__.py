d1fad4aba9a0ff6a29e4bf9f58e3dd0b7ce9fbd3
Mitch Weisbrod <mitch@theweisbrods.com>
2026-06-21 20:18:47 -0700

    docs

--- backend/src/core/__init__.py
+++ backend/src/core/__init__.py
@@ -1,6 +1,17 @@
 """Core NSO Bridge dependencies.
 
-This file exports the core functions needed to run NSO Bridge.
+There are eight sub-modules with this module: `app`, `db`, `server`, `updates`, `users`,
+`ws`, and `exceptions`.
+
+- `app` includes functionality for running the core FastAPI application.
+- `db' pertains to database operations.
+- `server` manages the network layer
+- `updates` is involved in checking for updates to this application.
+- `users` is for user management and related operations.
+- `ws` handles WebSockets.
+- `exceptions` exposes the base exceptions used by this application.
+
+An additional `logging` file is used to configure information and error logging.
 """
 
 from .logging import configure_logging
