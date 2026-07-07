70af5374ea4a67ac7035026e0ce370b2b024160b
Mitch Weisbrod <mitch@theweisbrods.com>
2026-07-07 12:26:12 -0700

    fix greenlet error when bundling with pyinstaller

--- backend/src/game/jams/dependencies.py
+++ backend/src/game/jams/dependencies.py
@@ -1,30 +1,33 @@
 """The FastAPI dependencies methods for Jams."""
 
 from typing import Annotated, TypeAlias
+from uuid import UUID
 
-from core.exceptions import ModelLookupError
+from core.db import GetAsyncSession
 from core.users import GetUser
 from fastapi import Depends, Query, Request
-from game.bouts.dependencies import GetBout
+from sqlalchemy import Result, Select, select
 
 from .models import Jam
 
 
-async def _get_jam(
+async def _get_jam(  # noqa: PLR0913
     request: Request,
     user: GetUser,
-    bout: GetBout,
+    session: GetAsyncSession,
+    bout_uuid: Annotated[UUID, Query(alias='boutUuid')],
     period_num: Annotated[int, Query(alias='periodNum')],
     jam_num: Annotated[int, Query(alias='jamNum')],
 ) -> Jam:
-    try:
-        jam: Jam = next(
-            jam for jam in bout.jams if jam.period == period_num and jam.num == jam_num
-        )
-    except StopIteration as e:
-        raise ModelLookupError(
-            f'Could not find Jam ({bout=} {period_num=} {jam_num=})'
-        ) from e
+    # TODO: polish this method
+    statement: Select[tuple[Jam]] = (
+        select(Jam)
+        .where(Jam.bout_uuid == bout_uuid)
+        .where(Jam.period == period_num)
+        .where(Jam.num == jam_num)
+    )
+    results: Result[tuple[Jam]] = await session.execute(statement)
+    jam: Jam = results.scalar_one()
 
     if request.method != 'GET':
         user.stage(jam.get_memento())
