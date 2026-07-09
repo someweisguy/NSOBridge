49a9bbd53fe2db3b7b6b33cc3aefc441efdf3283
Mitch Weisbrod <mitch@theweisbrods.com>
2026-07-09 11:15:41 -0700

    update docs

--- backend/src/core/__init__.py
+++ backend/src/core/__init__.py
@@ -3,12 +3,11 @@
 There are eight sub-modules with this module: `app`, `db`, `server`, `updates`, `users`,
 and `ws`.
 
-- `app` includes functionality for running the core FastAPI application.
+- `app` includes functionality for running the core FastAPI application and WebSockets.
 - `db' pertains to database operations.
 - `server` manages the network layer
 - `updates` is involved in checking for updates to this application.
 - `users` is for user management and related operations.
-- `ws` handles WebSockets.
 
 An additional `logging` file is used to configure information and error logging.
 """
