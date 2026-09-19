1f8a692f152441935d64639987ac29e80e2258bc
Mitch Weisbrod <mitch@theweisbrods.com>
2026-09-19 10:57:00 -0700

    add non-suspense query

--- frontend/hooks/use-ruleset.ts
+++ frontend/hooks/use-ruleset.ts
@@ -2,7 +2,11 @@ import { localAPI } from "@/lib/requests";
 import { AppQueryOptions, AppSuspenseQueryOptions } from "@/types/query";
 import { Ruleset } from "@/types/ruleset";
 import { rulesetKeys } from "@/utils/query-keys";
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import {
+  useQuery,
+  UseQueryResult,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 
 /**
  * Fetches the desired Ruleset from the server. This hook is a wrapper for call to
@@ -43,11 +47,11 @@ export const useSuspenseGetRuleset = ({
   });
 
 /**
- * Gets all the Bouts from the server. Each individual Bout is automatically cached
+ * Gets all the Rulesets from the server. Each individual Bout is automatically cached
  * after it is fetched. This hook is a wrapper for call to TanStack Query's
  * `useSuspenseQuery` function.
  *
- * @returns a Tanstack useSuspenseQuery object containing an array of all Bouts.
+ * @returns a Tanstack useSuspenseQuery object containing an array of all Rulesets.
  */
 export const useSuspenseGetAllRulesets = <T = Ruleset[]>(
   options?: AppSuspenseQueryOptions<Ruleset[], T>,
@@ -60,6 +64,28 @@ export const useSuspenseGetAllRulesets = <T = Ruleset[]>(
         .then((rulesets: Ruleset[]) => {
           // TODO: add all the rulesets to the cache individually
           return rulesets;
+        }),
+    ...options,
+  });
+
+/**
+ * Gets all the Rulesets from the server. Each individual Bout is automatically cached
+ * after it is fetched. This hook is a wrapper for call to TanStack Query's
+ * `useSuspenseQuery` function.
+ *
+ * @returns a Tanstack useeQuery object containing an array of all Rulesets.
+ */
+export const useGetAllRulesets = <T = Ruleset[]>(
+  options?: AppQueryOptions<Ruleset[], T>,
+): UseQueryResult<T, Error> =>
+  useQuery<Ruleset[], Error, T>({
+    queryKey: rulesetKeys.all,
+    queryFn: () =>
+      localAPI
+        .get<Ruleset[]>("bout/allRulesets")
+        .then((rulesets: Ruleset[]) => {
+          // TODO: add all the rulesets to the cache individually
+          return rulesets.filter((ruleset) => ruleset != null);
         }),
     ...options,
   });
