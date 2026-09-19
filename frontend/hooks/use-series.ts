20a012e2c749322163be9c78b1cac10287718817
Mitch Weisbrod <mitch@theweisbrods.com>
2026-09-18 22:11:31 -0700

    add non-suspense query

--- frontend/hooks/use-series.ts
+++ frontend/hooks/use-series.ts
@@ -1,9 +1,17 @@
 import queryClient from "@/lib/cache";
 import { localAPI } from "@/lib/requests";
-import { AppSuspenseQueryOptions, SeriesUri } from "@/types/query";
+import {
+  AppQueryOptions,
+  AppSuspenseQueryOptions,
+  SeriesUri,
+} from "@/types/query";
 import { Series } from "@/types/series";
 import { seriesKeys } from "@/utils/query-keys";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  useQuery,
+  UseQueryResult,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 
 /**
  * Gets all the Series from the server. Each individual Series is automatically cached
@@ -33,6 +41,32 @@ export const useSuspenseGetAllSeries = <T = Series[]>(
   options?: AppSuspenseQueryOptions<Series[], T>,
 ) =>
   useSuspenseQuery({
+    queryKey: seriesKeys.all,
+    queryFn: () =>
+      localAPI
+        .get<Series[]>("series/allSeries")
+        .then<Series[]>((allSeries: Series[]) => {
+          for (const series of allSeries) {
+            if (series.uuid != null) {
+              queryClient.setQueryData(seriesKeys.one(series.uuid), series);
+            }
+          }
+          return allSeries;
+        }),
+    ...options,
+  });
+
+/**
+ * Gets all the Series from the server. Each individual Series is automatically cached
+ * after it is fetched. This hook is a wrapper for call to TanStack Query's
+ * `useQuery` function.
+ *
+ * @returns a Tanstack useQuery object containing an array of all Series.
+ */
+export const useGetAllSeries = <T = Series[]>(
+  options?: AppQueryOptions<Series[], T>,
+): UseQueryResult<T, Error> =>
+  useQuery({
     queryKey: seriesKeys.all,
     queryFn: () =>
       localAPI
