0ab59a20aa9154c4a2fbb71876929044f4c9b376
Mitch Weisbrod <mitch@theweisbrods.com>
2026-09-24 20:13:22 -0700

    add endpoints

---
+++ frontend/configs/endpoints.ts
@@ -0,0 +1,16 @@
+/**
+ * API HTTP Endpoints.
+ *
+ * This file contains all of the constants for the backend API endpoints. They
+ * are used in both production code and test code. They are defined here so if
+ * the backend changes, only this file needs to be updated to update both
+ * production code and test code.
+ */
+
+export const ALL_SERIES_API = "allSeries"; // TODO: remove this API
+export const SERIES_API = "series";
+export const BOUT_API = "bout";
+export const RULESET_API = "bout/ruleset";
+export const ALL_RULESET_API = "bout/allRulesets"; // TODO: remove this API
+export const JAM_API = "jam";
+export const TIMEOUT_API = "timeout";
